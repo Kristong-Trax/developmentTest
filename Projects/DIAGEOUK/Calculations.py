0bba4062fe71a4dc235af2d54b9447ff6c8b0a81
yoav_a <yoava@traxretail.com>
2018-06-06 14:17:51 +0300

    profiling

--- Projects/DIAGEOUK/Calculations.py
+++ Projects/DIAGEOUK/Calculations.py
@@ -1,8 +1,8 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 from kpi_factory.Projects.DIAGEOUK.KPIGenerator import DIAGEOUKGenerator
 
@@ -16,12 +16,12 @@ class DIAGEOUKCalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('diageoau calculations')
-    Config.init()
-    project_name = 'diageouk'
-    data_provider = KEngineDataProvider(project_name)
-    session = '5E50365B-8C97-4CBF-A6F2-341D4E1DA466'
-    data_provider.load_session_data(session)
-    output = Output()
-    DIAGEOUKCalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('diageoau calculations')
+#     Config.init()
+#     project_name = 'diageouk'
+#     data_provider = KEngineDataProvider(project_name)
+#     session = '5E50365B-8C97-4CBF-A6F2-341D4E1DA466'
+#     data_provider.load_session_data(session)
+#     output = Output()
+#     DIAGEOUKCalculations(data_provider, output).run_project_calculations()
