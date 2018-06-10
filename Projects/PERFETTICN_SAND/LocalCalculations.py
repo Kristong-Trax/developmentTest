bc7a0b64c4826341e16ab9c9d11acf4da6c53ca5
limor_c <limorc@traxretail.com>
2018-06-10 12:21:44 +0300

    Merge branch 'master' of bitbucket.org:traxtechnology/sdk_factory
    # Conflicts:
    #	kpi_factory

--- Projects/PERFETTICN_SAND/LocalCalculations.py
+++ Projects/PERFETTICN_SAND/LocalCalculations.py
@@ -8,12 +8,12 @@ from Projects.PERFETTICN_SAND.Calculations import Calculations
 if __name__ == '__main__':
     Log.init('perfetticn-sand calculations')
     Config.init()
-    #project_name = 'diageomx-sand'
-    project_name = 'perfetticn-sand'
+    project_name = 'diageotw'
+    #project_name = 'perfetticn-sand'
     data_provider = KEngineDataProvider(project_name)
-    session = '86F603CB-11E1-4387-AC63-DD4C14F64F74'
+    #session = '86F603CB-11E1-4387-AC63-DD4C14F64F74'
     #session='69ea07bc-34c3-478f-88a9-e7f6859e61c1'
-    # session='9b0b74c8-c72c-420e-938c-3cd4375708c5'
+    session='3CEF75C7-1818-4969-A227-F51FF72B3C9F'
     data_provider.load_session_data(session)
     output = Output()
     Calculations(data_provider, output).run_project_calculations()
--- Projects/PERFETTICN_SAND/Utils/KPIToolBox.py
+++ Projects/PERFETTICN_SAND/Utils/KPIToolBox.py
@@ -46,36 +46,28 @@ class PERFETTICNToolBox:
         self.kpi_results_queries = []
         self.store_info = self.data_provider[Data.STORE_INFO]
         self.assortment = Assortment(self.data_provider, self.output)
-        self.template= self.data_provider.all_templates
+        self.template = self.data_provider.all_templates
+
     def main_calculation(self, *args, **kwargs):
 
         self.display_count()
-       # self.assortment_calculation()
+        self.assortment_calculation()
         self.common.commit_results_data_to_new_tables()
-
-
 
     def display_count (self):
 
         num_brands = {}
-        #display_info = self.scif['template_name']
-        display_info = self.scif['template_fk'].unique()
+        display_info = self.scif['template_fk']
         display_fks = display_info.unique()
-        template_seco= self.template[self.template['included_in_secondary_shelf_report']=='Y']['template_fk']
+        template_seco = self.template[self.template['included_in_secondary_shelf_report'] == 'Y']['template_fk']
         display_fks = list(filter(lambda x : x  in template_seco.values, display_fks))
-        count_fk = self.kpi_static_data[self.kpi_static_data['client_name']=='COUNT OF DISPLAY']['pk'].iloc[0]
+        count_fk = self.kpi_static_data[self.kpi_static_data['client_name'] == 'COUNT OF DISPLAY']['pk'].iloc[0]
         for value in display_fks:
             num_brands[value] = display_info[display_info == value].count()
-            score=num_brands[value]
-
-        # data=self.get_match_display(self.session_uid)
-        # store_num= self.store_info['store_number_1'][0] ??  what about the store_id?
+            score = num_brands[value]
             self.common.write_to_db_result_new_tables(count_fk,value , score,None, None, score, score)
-        # kpi_fk, self.session_id, numerator_id, numerator_result, denominator_id,
-        # denominator_result, result, score, score_after_actions,
-        # denominator_result_after_actions
 
-        return 0
+        return
 
     def assortment_calculation(self):
         """
@@ -84,39 +76,35 @@ class PERFETTICNToolBox:
         lvl3_result = self.assortment.calculate_lvl3_assortment()
 
         for result in lvl3_result.itertuples():
-            score = result.in_store * 100
+            score = result.in_store
             self.common.write_to_db_result_new_tables(result.kpi_fk_lvl3, result.product_fk, result.in_store,
                                                       score, result.assortment_group_fk, 1, score)
         if not lvl3_result.empty:
             lvl2_result = self.assortment.calculate_lvl2_assortment(lvl3_result)
             for result in lvl2_result.itertuples():
                 denominator_res = result.total
-                # if result.target and result.group_target_date <= self.current_date:
-                #     denominator_res = result.target
-                res = np.divide(float(result.passes), float(denominator_res)) * 100
-                if res >= 100:
-                    score = 100
+                res = np.divide(float(result.passes), float(denominator_res))
+                if result.passes >= 1:
+                    score = 1
                 else:
                     score = 0
                 self.common.write_to_db_result_new_tables(result.kpi_fk_lvl2, result.assortment_group_fk, result.passes,
                                                           res, result.assortment_super_group_fk, denominator_res, score)
-        if not lvl2_result.empty:
-            lvl1_result = self.assortment.calculate_lvl1_assortment(lvl2_result)
-            for result in lvl1_result.itertuples():
-                denominator_res = result.super_group_target
-                if not result.super_group_target:
-                    denominator_res = result.total
-                res = np.divide(float(result.passes), float(denominator_res)) * 100
-                if res >= 100:
-                    score = 100
-                else:
-                    score = 0
-                self.common.write_to_db_result_new_tables(fk=result.kpi_fk_lvl1,
-                                                          numerator_id=result.assortment_super_group_fk,
-                                                          numerator_result=result.passes,
-                                                          denominator_result=denominator_res,
-                                                          result=res, score=score)
 
+            if not lvl2_result.empty:
+                lvl1_result = self.assortment.calculate_lvl1_assortment(lvl2_result)
+                for result in lvl1_result.itertuples():
+                    denominator_res = result.total
+                    res = np.divide(float(result.passes), float(denominator_res))
+                    if res >= 0:
+                        score = 1
+                    else:
+                        score = 0
+                    self.common.write_to_db_result_new_tables(fk=result.kpi_fk_lvl1,
+                                                              numerator_id=result.assortment_super_group_fk,
+                                                              numerator_result=result.passes,
+                                                              denominator_result=denominator_res,
+                                                              result=res, score=score)
         return
 
     # def get_match_display(self,session_uid):
