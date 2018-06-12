b5830d122f8681ea9542fe2c65099c235ce0ee1e
Shani Meushar <shanim@traxretail.com>
2018-06-12 08:12:39 +0300

    PROS-4779

--- Projects/CCRUFIFA2018/Calculations.py
+++ Projects/CCRUFIFA2018/Calculations.py
@@ -1,12 +1,12 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 from Projects.CCRUFIFA2018.KPIGenerator import CCRUFIFA2018Generator
 
-__author__ = 'uri'
+__author__ = 'shani'
 
 
 class CCRUFIFACalculations(BaseCalculationsScript):
@@ -16,12 +16,12 @@ class CCRUFIFACalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('ccrufifa2018 calculations')
-    Config.init()
-    project_name = 'ccrufifa2018'
-    data_provider = KEngineDataProvider(project_name)
-    session = 'D3E3EAAD-B7AD-4466-B341-31390FAA3A79' # D3E3EAAD-B7AD-4466-B341-31390FAA3A79, B284DB41-84E1-4D72-9338-A12396D41B1A
-    data_provider.load_session_data(session)
-    output = Output()
-    CCRUFIFACalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('ccrufifa2018 calculations')
+#     Config.init()
+#     project_name = 'ccrufifa2018'
+#     data_provider = KEngineDataProvider(project_name)
+#     session = 'B284DB41-84E1-4D72-9338-A12396D41B1A' # D3E3EAAD-B7AD-4466-B341-31390FAA3A79, B284DB41-84E1-4D72-9338-A12396D41B1A
+#     data_provider.load_session_data(session)
+#     output = Output()
+#     CCRUFIFACalculations(data_provider, output).run_project_calculations()
