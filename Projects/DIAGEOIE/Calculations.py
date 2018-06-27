bdb0af85de46382cf25f023626f6c802bc036315
natalya_k <natalyak@traxretail.com>
2018-06-27 13:24:47 +0300

    changing code for new assortment calculations

--- Projects/DIAGEOIE/Calculations.py
+++ Projects/DIAGEOIE/Calculations.py
@@ -1,8 +1,8 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-# from Trax.Utils.Conf.Configuration import Config
-# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 from Projects.DIAGEOIE.KPIGenerator import DIAGEOIEGenerator
 from KPIUtils.GlobalProjects.DIAGEO.KPIGenerator import DIAGEOGenerator
@@ -17,15 +17,16 @@ class DIAGEOIECalculations(BaseCalculationsScript):
         common = Common(self.data_provider)
         DIAGEOIEGenerator(self.data_provider, self.output).main_function()
         DIAGEOGenerator(self.data_provider, self.output, common).diageo_global_assortment_function()
+        common.commit_results_data_to_new_tables()
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-# if __name__ == '__main__':
-#     LoggerInitializer.init('diageoie calculations')
-#     Config.init()
-#     project_name = 'diageoie'
-#     data_provider = KEngineDataProvider(project_name)
-#     session = 'e01e4ed9-7268-41b4-b0cb-0e808104eb17'
-#     data_provider.load_session_data(session)
-#     output = Output()
-#     DIAGEOIECalculations(data_provider, output).run_project_calculations()
+if __name__ == '__main__':
+    LoggerInitializer.init('diageoie calculations')
+    Config.init()
+    project_name = 'diageoie'
+    data_provider = KEngineDataProvider(project_name)
+    session = 'E2C8099C-F37E-4E1F-AA77-D5FBDE87C967'
+    data_provider.load_session_data(session)
+    output = Output()
+    DIAGEOIECalculations(data_provider, output).run_project_calculations()
