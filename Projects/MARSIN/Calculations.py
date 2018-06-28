17a1790bb8dbedcf7a085f05500bbe48d23f2492
limor_c <limorc@traxretail.com>
2018-06-28 15:14:28 +0300

    unchange commit

--- Projects/MARSIN/Calculations.py
+++ Projects/MARSIN/Calculations.py
@@ -1,21 +1,21 @@
-#
-# from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-# from Trax.Utils.Conf.Configuration import Config
-# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-#
-# from Projects.MARSIN.KPIGenerator import MARSINGenerator
-#
-# __author__ = 'Nimrod'
-#
-#
-# class MARSINCalculations(BaseCalculationsScript):
-#     def run_project_calculations(self):
-#         self.timer.start()
-#         MARSINGenerator(self.data_provider, self.output).main_function()
-#         self.timer.stop('KPIGenerator.run_project_calculations')
-#
-#
+
+from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+
+from Projects.MARSIN.KPIGenerator import MARSINGenerator
+
+__author__ = 'Nimrod'
+
+
+class MARSINCalculations(BaseCalculationsScript):
+    def run_project_calculations(self):
+        self.timer.start()
+        MARSINGenerator(self.data_provider, self.output).main_function()
+        self.timer.stop('KPIGenerator.run_project_calculations')
+
+
 # if __name__ == '__main__':
 #     LoggerInitializer.init('marsin calculations')
 #     Config.init()
