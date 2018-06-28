f1276278dea711656867950a981d9301008e7bd9
limor_c <limorc@traxretail.com>
2018-06-28 14:36:12 +0300

    Merge branch 'master' of bitbucket.org:traxtechnology/sdk_factory
    # Conflicts:
    #	kpi_factory

--- Projects/PERFETTICN/LocalCalculations.py
+++ Projects/PERFETTICN/LocalCalculations.py
@@ -1,16 +1,16 @@
-
+#
 # from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
 # from Trax.Utils.Conf.Configuration import Config
 # from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 # from Projects.PERFETTICN.Calculations import Calculations
-
-
+#
+#
 # if __name__ == '__main__':
 #     LoggerInitializer.init('perfetticn calculations')
 #     Config.init()
 #     project_name = 'perfetticn'
 #     data_provider = KEngineDataProvider(project_name)
-#     session = ''
+#     session = 'AC3ED6D6-19E5-4B1A-A215-E4B28A659118'
 #     data_provider.load_session_data(session)
 #     output = Output()
 #     Calculations(data_provider, output).run_project_calculations()
--- Projects/PERFETTICN_SAND/LocalCalculations.py
+++ Projects/PERFETTICN_SAND/LocalCalculations.py
@@ -1,19 +1,19 @@
-
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import Log
-from Projects.PERFETTICN_SAND.Calculations import Calculations
-
-
-if __name__ == '__main__':
-    Log.init('perfetticn-sand calculations')
-    Config.init()
-    #project_name = 'diageotw'
-    project_name = 'perfetticn-sand'
-    data_provider = KEngineDataProvider(project_name)
-    session = 'B57B5139-3D03-4ABB-8043-0DCE260A11E1'
-    session2 =   '250CD4BC-5CDD-4DA5-9D6B-67022B4BDE8B'
-    #session='3CEF75C7-1818-4969-A227-F51FF72B3C9F'
-    data_provider.load_session_data(session)
-    output = Output()
-    Calculations(data_provider, output).run_project_calculations()
+#
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import Log
+# from Projects.PERFETTICN_SAND.Calculations import Calculations
+#
+#
+# if __name__ == '__main__':
+#     Log.init('perfetticn-sand calculations')
+#     Config.init()
+#     #project_name = 'diageotw'
+#     project_name = 'perfetticn-sand'
+#     data_provider = KEngineDataProvider(project_name)
+#     session = 'B57B5139-3D03-4ABB-8043-0DCE260A11E1'
+#     session2 =   '250CD4BC-5CDD-4DA5-9D6B-67022B4BDE8B'
+#     #session='3CEF75C7-1818-4969-A227-F51FF72B3C9F'
+#     data_provider.load_session_data(session)
+#     output = Output()
+#     Calculations(data_provider, output).run_project_calculations()
