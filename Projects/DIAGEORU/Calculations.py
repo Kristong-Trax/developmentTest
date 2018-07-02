c0cb54f4297f97e17ddca8feb70366fd11136d26
yoava <yoava@traxretail.com>
2018-07-02 13:24:22 +0300

    removed failed projects

--- Projects/DIAGEORU/Calculations.py
+++ Projects/DIAGEORU/Calculations.py
@@ -1,8 +1,8 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-# from Trax.Utils.Conf.Configuration import Config
-# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 from Projects.DIAGEORU.KPIGenerator import DIAGEORUGenerator
 from KPIUtils.GlobalProjects.DIAGEO.KPIGenerator import DIAGEOGenerator
@@ -21,12 +21,12 @@ class DIAGEORUDIAGEORUCalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-# if __name__ == '__main__':
-#     LoggerInitializer.init('diageoru-sand calculations')
-#     Config.init()
-#     project_name = 'diageoru-sand'
-#     data_provider = KEngineDataProvider(project_name)
-#     session = ''
-#     data_provider.load_session_data(session)
-#     output = Output()
-#     DIAGEORUDIAGEORUCalculations(data_provider, output).run_project_calculations()
+if __name__ == '__main__':
+    LoggerInitializer.init('diageoru calculations')
+    Config.init()
+    project_name = 'diageoru'
+    data_provider = KEngineDataProvider(project_name)
+    session = '68680f5f-0499-4fab-ab74-291e4c4890d7'
+    data_provider.load_session_data(session)
+    output = Output()
+    DIAGEORUDIAGEORUCalculations(data_provider, output).run_project_calculations()
