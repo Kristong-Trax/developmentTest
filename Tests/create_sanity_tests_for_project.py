914fc78cbdabe605c4055cc8f9b73592b46b8e92
yoava <yoava@traxretail.com>
2018-07-03 16:16:47 +0300

    removed diageoar sanity tests

--- Tests/create_sanity_tests_for_project.py
+++ Tests/create_sanity_tests_for_project.py
@@ -203,7 +203,7 @@ class ProjectsSanityData(BaseSeedData):
 if __name__ == '__main__':
     LoggerInitializer.init('')
     Config.init()
-    project_to_test = 'sanofihk'
+    project_to_test = 'americas-demo'
     creator = SeedCreator(project_to_test)
     creator.activate_exporter()
     creator.rds_conn.disconnect_rds()
