71391f98dcb612cab37ce455ddcdd965e7235247
yoava <yoava@traxretail.com>
2018-07-04 10:57:35 +0300

    added sanofis

--- Projects/SANOFIAE/Calculations.py
+++ Projects/SANOFIAE/Calculations.py
@@ -1,8 +1,8 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-# from Trax.Utils.Conf.Configuration import Config
-# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 import os
 from KPIUtils.GlobalProjects.SANOFI.KPIGenerator import SANOFIGenerator
 
@@ -18,12 +18,12 @@ class SANOFIAECalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-# if __name__ == '__main__':
-#     LoggerInitializer.init('sanofiae calculations')
-#     Config.init()
-#     project_name = 'sanofiae'
-#     data_provider = KEngineDataProvider(project_name)
-#     session = '8C9908A9-942D-425F-A943-C9550CB124B5'
-#     data_provider.load_session_data(session)
-#     output = Output()
-#     SANOFIAECalculations(data_provider, output).run_project_calculations()
+if __name__ == '__main__':
+    LoggerInitializer.init('sanofiae calculations')
+    Config.init()
+    project_name = 'sanofiae'
+    data_provider = KEngineDataProvider(project_name)
+    session = '49AEE8EA-1075-4EEF-8DD5-3EDA0E2217C7'
+    data_provider.load_session_data(session)
+    output = Output()
+    SANOFIAECalculations(data_provider, output).run_project_calculations()
