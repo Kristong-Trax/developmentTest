87455947e581ae310289fb7122ed19545efbff77
sergeypol <sergey@traxretail.com>
2018-07-07 11:43:20 +0300

    Merge branches 'master' and 'PROS-5326-ccru-premium-spirits-picos' of bitbucket.org:traxtechnology/kpi_factory into PROS-5326-ccru-premium-spirits-picos
    # Conflicts:
    #	Projects/CCRU/Utils/TopSKU.py

--- Projects/CCRU/Sets/HoReCaCoffee2018.py
+++ Projects/CCRU/Sets/HoReCaCoffee2018.py
@@ -70,7 +70,7 @@ class CCRUHRCCoffee2018Calculations:
                                                                                         'kpi_set_fk'])
         self.tool_box.write_to_db_result(attributes_for_table1, 'level1', HRC_COFFEE_2018)
 # Sergey
-        self.tool_box.prepare_hidden_set(jg.project_kpi_dict.get('kpi_data')[0])
+#        self.tool_box.prepare_hidden_set(jg.project_kpi_dict.get('kpi_data')[0])
 # Sergey
         jg.create_gaps_json('gaps_guide_2018.xlsx', sheet_name=HRC_COFFEE_2018_GAPS)
         self.tool_box.calculate_gaps(jg.project_kpi_dict.get('gaps'))
