abea5c3bbb9f045c81ff61dbf026baf7d1c5e6de
ilay_s <ilays@traxretail.com>
2018-07-26 15:16:44 +0300

    move inbevbr sand to prod

--- Projects/INBEVBR/KPIGenerator.py
+++ Projects/INBEVBR/KPIGenerator.py
@@ -1,12 +1,11 @@
-import os
+
 from Trax.Utils.Logging.Logger import Log
-from Projects.INBEVBR.Utils.ParseTemplates import parse_template
+
 from Projects.INBEVBR.Utils.KPIToolBox import INBEVBRToolBox, log_runtime
 
-__author__ = 'Yasmin'
+__author__ = 'ilays'
 
-TEMPLATE_PATH = os.path.join('/home/Yasmin/dev/trax_ace_factory/Projects/INBEVBR/', 'Data',
-                             'Setupfile_current.xlsx')
+
 class INBEVBRGenerator:
 
     def __init__(self, data_provider, output):
@@ -24,13 +23,4 @@ class INBEVBRGenerator:
         """
         if self.tool_box.scif.empty:
             Log.warning('Scene item facts is empty for this session')
-        # for kpi_set_fk in self.tool_box.kpi_static_data['kpi_set_fk'].unique().tolist():
-
-        kpi_template = parse_template(TEMPLATE_PATH, sheet_name='KPIs')
-        kpi_sets = filter(None, kpi_template['Tested KPI Group'].unique().tolist())
-        for kpi_set_name in kpi_sets:
-            kpi_set_fk = self.tool_box.kpi_static_data.loc[
-                self.tool_box.kpi_static_data['kpi_set_name'] == kpi_set_name]['kpi_set_fk'].iloc[0]
-            score = self.tool_box.main_calculation(kpi_set_fk=kpi_set_fk)
-            self.tool_box.write_to_db_result(kpi_set_fk, score, self.tool_box.LEVEL1)
-        self.tool_box.commit_results_data()
+        self.tool_box.main_calculation()
