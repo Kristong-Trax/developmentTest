c6e88312a8f32d6cfcb3d917cfe6d072e5689341
Shani Meushar <shanim@traxretail.com>
2018-07-31 11:05:39 +0300

    PROS-5550

--- Projects/CUBAU/Calculations.py
+++ Projects/CUBAU/Calculations.py
@@ -1,9 +1,9 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-from Trax.Utils.Logging.Logger import Log
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from Trax.Utils.Logging.Logger import Log
 from Projects.CUBAU.KPIGenerator import CUBAUGenerator
 
 __author__ = 'Shani'
@@ -16,14 +16,14 @@ class CUBAUCalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-if __name__ == '__main__':
-    Log.init('cubau calculations')
-    Config.init()
-    project_name = 'CUB'
-    data_provider = KEngineDataProvider(project_name)
-    sessions = ['52407D63-C856-45AC-949A-C5AAF8C84494']
-    for session in sessions:
-        data_provider = KEngineDataProvider(project_name)
-        data_provider.load_session_data(session)
-        output = Output()
-        CUBAUCalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     Log.init('cubau calculations')
+#     Config.init()
+#     project_name = 'CUB-2'
+#     data_provider = KEngineDataProvider(project_name)
+#     sessions = ['52407D63-C856-45AC-949A-C5AAF8C84494']
+#     for session in sessions:
+#         data_provider = KEngineDataProvider(project_name)
+#         data_provider.load_session_data(session)
+#         output = Output()
+#         CUBAUCalculations(data_provider, output).run_project_calculations()
