4c286ebd1e3a96af60ff7a24d084f9f2a4534cda
limor_c <limorc@traxretail.com>
2018-08-06 15:46:10 +0300

    Heinken tw calculation

--- Projects/HEINEKENTW/Tests/test_unit_heinekentw.py
+++ Projects/HEINEKENTW/Tests/test_unit_heinekentw.py
@@ -12,7 +12,8 @@ __author__ = 'limorc'
 class TestHEINEKENTW(TestCase):
 
     @mock.patch('Projects.HEINEKENTW.Utils.KPIToolBox.ProjectConnector')
-    def setUp(self, x):
+    @mock.patch('Projects.HEINEKENTW.Utils.KPIToolBox.Common')
+    def setUp(self,x,y):
         Config.init('')
         self.data_provider_mock = MagicMock()
         self.data_provider_mock.project_name = 'heinekentw'
@@ -20,3 +21,9 @@ class TestHEINEKENTW(TestCase):
         self.output = MagicMock()
         self.tool_box = HEINEKENTWToolBox(self.data_provider_mock, MagicMock())
 
+
+    def test_mock(self):
+        self.assertEquals("true",3,"not good")
+
+if __name__=="__main__":
+    TestHEINEKENTW()
