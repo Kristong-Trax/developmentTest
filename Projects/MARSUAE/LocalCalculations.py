9eeb70be397a3c10ea87b8d4e0b065f6c9040bf4
israelshok <israels@traxretail.com>
2018-08-21 15:42:22 +0300

    uae

--- Projects/MARSUAE/LocalCalculations.py
+++ Projects/MARSUAE/LocalCalculations.py
@@ -1,16 +1,16 @@
 
-# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-# from Trax.Utils.Conf.Configuration import Config
-# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-# from Projects.MARSUAE.Calculations import Calculations
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+from Projects.MARSUAE.Calculations import Calculations
 
 
-# if __name__ == '__main__':
-#     LoggerInitializer.init('marsuae calculations')
-#     Config.init()
-#     project_name = 'marsuae'
-#     data_provider = KEngineDataProvider(project_name)
-#     session = ''
-#     data_provider.load_session_data(session)
-#     output = Output()
-#     Calculations(data_provider, output).run_project_calculations()
+if __name__ == '__main__':
+    LoggerInitializer.init('marsuae calculations')
+    Config.init()
+    project_name = 'ccru-sand'
+    data_provider = KEngineDataProvider(project_name)
+    session = '00003802-9b4d-47c7-89b3-30e6cd1a39ad'
+    data_provider.load_session_data(session)
+    output = Output()
+    Calculations(data_provider, output).run_project_calculations()
