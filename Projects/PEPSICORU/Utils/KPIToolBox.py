cc0bb42dc96d9bb1af5c16232f6d6619d35213e0
idanre <idanr@traxretail.com>
2018-08-21 17:13:34 +0300

    pepsicoRU on the go

--- Projects/PEPSICORU/Utils/KPIToolBox.py
+++ Projects/PEPSICORU/Utils/KPIToolBox.py
@@ -384,7 +384,7 @@ class PEPSICORUToolBox:
         """
         self.calculate_share_of_shelf()
         self.calculate_count_of_display()
-        Assortment(self.data_provider, self.output, common=self.common).main_assortment_calculation()
+        # Assortment(self.data_provider, self.output, common=self.common).main_assortment_calculation()
 
     ###################################### Plaster ######################################
 
