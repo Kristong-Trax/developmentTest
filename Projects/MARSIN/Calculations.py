1c9e2862561b0bb825cb1aae3220f0b8f81bc125
limor_c <limorc@traxretail.com>
2018-08-23 15:56:43 +0300

    changing template

--- Projects/MARSIN/Calculations.py
+++ Projects/MARSIN/Calculations.py
@@ -1,8 +1,8 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-# from Trax.Utils.Conf.Configuration import Config
-# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 from Projects.MARSIN.KPIGenerator import MARSINGenerator
 
@@ -16,12 +16,12 @@ class MARSINCalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-# if __name__ == '__main__':
-#     LoggerInitializer.init('marsin calculations')
-#     Config.init()
-#     project_name = 'marsin'
-#     data_provider = KEngineDataProvider(project_name)
-#     session = 'e0e03fd4-f1cb-4924-b122-d6671aa81d18'
-#     data_provider.load_session_data(session)
-#     output = Output()
-#     MARSINCalculations(data_provider, output).run_project_calculations()
+if __name__ == '__main__':
+    LoggerInitializer.init('marsin calculations')
+    Config.init()
+    project_name = 'marsin'
+    data_provider = KEngineDataProvider(project_name)
+    session = 'e85cfeb5-3508-4e7e-85f8-60e5ac53cb71'
+    data_provider.load_session_data(session)
+    output = Output()
+    MARSINCalculations(data_provider, output).run_project_calculations()
