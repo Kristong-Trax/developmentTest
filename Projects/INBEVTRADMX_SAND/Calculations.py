e5ffc61a074a014f82a648084d08dd93a32b9ae7
samkowalski <samk@traxretail.com>
2018-09-02 19:38:02 +0300

    reverted to original

--- Projects/INBEVTRADMX_SAND/Calculations.py
+++ Projects/INBEVTRADMX_SAND/Calculations.py
@@ -6,7 +6,7 @@ from Projects.INBEVTRADMX_SAND.KPIGenerator import Generator
 __author__ = 'yoava'
 
 
-class Calculations(BaseCalculationsScript):
+class INBEVTRADMX_SANDCalculations(BaseCalculationsScript):
     def run_project_calculations(self):
         self.timer.start()
         Generator(self.data_provider, self.output).main_function()
