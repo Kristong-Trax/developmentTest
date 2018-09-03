242fbe7ea8ce72f0eb7d8d9ff05b6849b6a78fd0
yasmingr <yasmin@traxretail.com>
2018-09-03 15:54:19 +0300

    bug fix

--- Projects/INBEVTRADMX_SAND/LocalCalculations.py
+++ Projects/INBEVTRADMX_SAND/LocalCalculations.py
@@ -1,4 +1,4 @@
-#
+
 # from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
 # from Trax.Utils.Conf.Configuration import Config
 # from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
@@ -10,7 +10,7 @@
 #     Config.init()
 #     project_name = 'inbevtradmx-sand'
 #     data_provider = KEngineDataProvider(project_name)
-#     session = '10aee05a-0d9a-4f93-ba8e-90efd4d5c1dc'
+#     session = 'fffff255-d0f6-4cdd-bf1f-81c1df465e8f'
 #     data_provider.load_session_data(session)
 #     output = Output()
 #     Calculations(data_provider, output).run_project_calculations()
