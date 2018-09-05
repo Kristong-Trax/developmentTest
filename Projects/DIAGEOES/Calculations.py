d317c56a521da1d04084f4405b5322e219af46ac
israelshok <israels@traxretail.com>
2018-09-05 20:42:57 +0300

    new diageo es

--- Projects/DIAGEOES/Calculations.py
+++ Projects/DIAGEOES/Calculations.py
@@ -15,18 +15,18 @@ class DIAGEOESCalculations(BaseCalculationsScript):
     def run_project_calculations(self):
         self.timer.start()
         common = Common(self.data_provider)
-        DIAGEOESGenerator(self.data_provider, self.output).main_function()
+        # DIAGEOESGenerator(self.data_provider, self.output).main_function()
         DIAGEOGenerator(self.data_provider, self.output, common).diageo_global_assortment_function()
         common.commit_results_data_to_new_tables()
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-# if __name__ == '__main__':
-#     LoggerInitializer.init('diageoes calculations')
-#     Config.init()
-#     project_name = 'diageoes'
-#     data_provider = KEngineDataProvider(project_name)
-#     session = '065F07D9-B669-45E2-82D5-38C69C2AC9D6'
-#     data_provider.load_session_data(session)
-#     output = Output()
-#     DIAGEOESCalculations(data_provider, output).run_project_calculations()
+if __name__ == '__main__':
+    LoggerInitializer.init('diageoes calculations')
+    Config.init()
+    project_name = 'diageoes'
+    data_provider = KEngineDataProvider(project_name)
+    session = 'F9E5B557-84D2-4334-9814-6B972FA950AF'
+    data_provider.load_session_data(session)
+    output = Output()
+    DIAGEOESCalculations(data_provider, output).run_project_calculations()
