d9e16823f1d06417d5230e6de2e7248bb7ef3550
Shani Meushar <shanim@traxretail.com>
2018-09-05 10:19:08 +0300

    PROS-5958

--- Projects/PNGJP/Calculations.py
+++ Projects/PNGJP/Calculations.py
@@ -1,8 +1,8 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 from Projects.PNGJP.KPIGenerator import PNGJPGenerator
 
@@ -14,13 +14,13 @@ class PNGJPCalculations(BaseCalculationsScript):
         self.timer.start()
         PNGJPGenerator(self.data_provider, self.output).main_function()
         self.timer.stop('KPIGenerator.run_project_calculations')
-
-if __name__ == '__main__':
-    LoggerInitializer.init('pngjp calculations')
-    Config.init()
-    project_name = 'pngjp'
-    data_provider = KEngineDataProvider(project_name)
-    session = '6F1E801F-4994-42B3-808C-6409AEEAECCA'
-    data_provider.load_session_data(session)
-    output = Output()
-    PNGJPCalculations(data_provider, output).run_project_calculations()
+#
+# if __name__ == '__main__':
+#     LoggerInitializer.init('pngjp calculations')
+#     Config.init()
+#     project_name = 'pngjp'
+#     data_provider = KEngineDataProvider(project_name)
+#     session = '6F1E801F-4994-42B3-808C-6409AEEAECCA'
+#     data_provider.load_session_data(session)
+#     output = Output()
+#     PNGJPCalculations(data_provider, output).run_project_calculations()
