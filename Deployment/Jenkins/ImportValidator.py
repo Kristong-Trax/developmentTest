66d352a1df57a2969462fd01134334698e6c3995
elisuiza <elis@traxretail.com>
2018-09-13 10:14:39 +0300

    Tem Fix import validator

--- Deployment/Jenkins/ImportValidator.py
+++ Deployment/Jenkins/ImportValidator.py
@@ -80,12 +80,11 @@ class ValidateRelations(object):
             projects.sort()
             for project in projects:
                 project_x = os.path.join(self.projects_path, project.upper())
-                # if not os.path.exists(project_x):
-                #     self.log_errors(project, ['ERROR: The project does not exists'])
                 if os.path.isdir(project_x):
-                    print "***Validating Imports for: {}".format(project_x)
-                    p = subprocess.Popen([self.sfood_home, project_x], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     if not project in['CCUS', 'CCKH']:
+                        print "***Validating Imports for: {}*****".format(project_x)
+                        p = subprocess.Popen([self.sfood_home, project_x], stdout=subprocess.PIPE,
+                                             stderr=subprocess.PIPE)
                         self.log_errors(project, p.stderr.readlines())
                         result = p.communicate()[0]
                         res_df = ValidateRelations.validate_relations(project,
