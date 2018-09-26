82a3878cd4fa5541e7fbdccb4820d417e4608134
samkowalski <samk@traxretail.com>
2018-09-26 17:11:59 -0400

    re-enabled other kpi sets

--- Projects/CCBOTTLERSUS/KPIGenerator.py
+++ Projects/CCBOTTLERSUS/KPIGenerator.py
@@ -27,10 +27,10 @@ class CCBOTTLERSUSGenerator:
         It calculates the score for every KPI set and saves it to the DB.
         """
         Common(self.data_provider).commit_results_data()
-        # self.calculate_red_score()
+        self.calculate_red_score()
         # self.calculate_bci()
-        # self.calculate_manufacturer_displays()
-        # self.calculate_cma_compliance()
+        self.calculate_manufacturer_displays()
+        self.calculate_cma_compliance()
         self.calculate_cma_compliance_sw()
 
     @log_runtime('Manufacturer Displays CCBOTTLERSUSCalculations')
