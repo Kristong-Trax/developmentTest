b86a0b8abffd9a26e0ac8a8c0e78c38d51b27f2a
sergeypol <sergey@traxretail.com>
2018-10-03 15:39:27 +0300

    CCRU - util improvement

--- Projects/CCRU/Utils/run_sql.py
+++ Projects/CCRU/Utils/run_sql.py
@@ -14,9 +14,14 @@ class CCRURunSql:
         self.aws_conn = AwsProjectConnector(self.project, DbUsers.CalculationEng)
 
     def run_it(self):
-        statement_1 = \
+        statements = \
 """
-DELETE FROM `static`.`atomic_kpi` WHERE `pk`>=4821;
+UPDATE `static`.`atomic_kpi` SET `name`='Juice Shelf: Moya Semya - Apple Mix - 0.95L', `description`='Juice Shelf: Moya Semya - Apple Mix - 0.95L', `display_text`='Juice Shelf: Moya Semya - Apple Mix - 0.95L' WHERE `pk`='3362';
+UPDATE `static`.`atomic_kpi` SET `name`='Moya Semya - Apple Mix - 0.95L', `description`='Moya Semya - Apple Mix - 0.95L', `display_text`='Moya Semya - Apple Mix - 0.95L' WHERE `pk`='4235';
+UPDATE `static`.`atomic_kpi` SET `name`='Moya Semya - Apple Mix - 0.95L', `description`='Moya Semya - Apple Mix - 0.95L', `display_text`='Moya Semya - Apple Mix - 0.95L' WHERE `pk`='4281';
+UPDATE `static`.`atomic_kpi` SET `name`='Moya Semya - Pineapple-Mango - 0.95L', `description`='Moya Semya - Pineapple-Mango - 0.95L', `display_text`='Moya Semya - Pineapple-Mango - 0.95L' WHERE `pk`='4236';
+UPDATE `static`.`atomic_kpi` SET `name`='Moya Semya - Pineapple-Mango - 0.95L', `description`='Moya Semya - Pineapple-Mango - 0.95L', `display_text`='Moya Semya - Pineapple-Mango - 0.95L' WHERE `pk`='4282';
+UPDATE `static`.`atomic_kpi` SET `name`='Moya Semya - Apple-Strawberry - 0.95L', `description`='Moya Semya - Apple-Strawberry - 0.95L', `display_text`='Moya Semya - Apple-Strawberry - 0.95L' WHERE `pk`='4248';
 """
 # """
 # DELETE FROM `static`.`kpi_set` WHERE `pk`>='64';
@@ -28,9 +33,13 @@ DELETE FROM `static`.`atomic_kpi` WHERE `pk`>=4821;
 # UPDATE `static`.`kpi_set` SET `name`='Contract Execution 2018 X' WHERE `pk`='56';
 # """
 
-        cur = self.aws_conn.db.cursor()
-        cur.execute(statement_1)
-        self.aws_conn.db.commit()
+        statements = statements.split(';\n')
+        for statement in statements:
+            if len(statement) > 0:
+                cur = self.aws_conn.db.cursor()
+                print statement
+                cur.execute(statement)
+                self.aws_conn.db.commit()
 
 
 if __name__ == '__main__':
