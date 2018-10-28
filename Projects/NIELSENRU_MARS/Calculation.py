333c0924ed86e2f2021f71713f8146eaabf910e8
elyashivm <elyashivm@traxretail.com>
2018-10-28 10:31:22 +0200

    nielsenmarsrupos-sand to prod 2

--- Projects/NIELSENRU_MARS/Calculation.py
+++ Projects/NIELSENRU_MARS/Calculation.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import os
 
-# from Trax.Utils.Conf.Configuration import Config
-# # from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-# from Trax.Algo.Calculations.Core.DataProvider import ACEDataProvider, Output, KEngineDataProvider
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+from Trax.Algo.Calculations.Core.DataProvider import ACEDataProvider, Output, KEngineDataProvider
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
 from KPIUtils.GlobalProjects.MARSRU.Utils.ToolBox import GLOBAL_MARSRU_KPIToolBox
 from KPIUtils.GlobalProjects.MARSRU.Utils.JSON import GLOBAL_MARSRU_JsonGenerator
@@ -11,14 +11,14 @@ from KPIUtils.GlobalProjects.MARSRU.Utils.JSON import GLOBAL_MARSRU_JsonGenerato
 __author__ = 'urid'
 
 
-class NIELSENMARSRUCalculations(BaseCalculationsScript):
+class NIELSENRUMARSCalculations(BaseCalculationsScript):
     def run_project_calculations(self):
         self.timer.start()  # use log.time_message
         base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Data')
         tool_box = GLOBAL_MARSRU_KPIToolBox(self.data_provider, self.output,
                                             set_name='NIELSENMARSRU KPIs 2018', base_path=base_path)
         tool_box.hadle_update_custom_scif()
-        jg = GLOBAL_MARSRU_JsonGenerator('nielsenmarsrupos-sand', base_path)
+        jg = GLOBAL_MARSRU_JsonGenerator('nielsenru-mars', base_path)
         jg.create_json('template.xlsx')
         tool_box.check_availability(jg.project_kpi_dict.get('kpi_data')[0])
         tool_box.custom_linear_sos(jg.project_kpi_dict.get('kpi_data')[0])
@@ -42,24 +42,24 @@ class NIELSENMARSRUCalculations(BaseCalculationsScript):
                                                                        'kpi_set_fk'])
 
         tool_box.write_to_db_result(attributes_for_table1, 'level1', tool_box.set_name)
-        # tool_box.commit_results_data()
+        tool_box.commit_results_data()
         self.timer.stop('NIELSENMARSRUCalculations.run_project_calculations')
 
 
-# if __name__ == '__main__':
-#     LoggerInitializer.init('nielsenmarsrupos-sand calculations')
-#     Config.init()
-#     project_name = 'rinielsenru-mars'
-#     session_uids = ['555e59e6-8cc3-11e7-8bba-12ae84a7e6ca']
-#     data_provider = KEngineDataProvider(project_name)
-#     output = Output()
-#     for session in session_uids:
-#         data_provider.load_session_data(session)
-#         NIELSENMARSRUCalculations(data_provider, output).run_project_calculations()
-#
-#     # jg = NIELSENMARSRUPOS_SANDJsonGenerator('nielsenmarsrupos-sand')
-#     # jg.create_json('template.xlsx')
-#     # # tb = NIELSENMARSRUPOS_SANDMARSRUKPIToolBox(data_provider, self.output, 'Hypermarket')
-#     # tb = NIELSENMARSRUPOS_SANDKPIToolBox(data_provider, output, 'NIELSENMARSRU KPIs 2018')
-#     # tb.insert_new_kpis_old(project_name, jg.project_kpi_dict.get('kpi_data')[0])
-#     # tb.insert_new_kpis_old(project_name)
+if __name__ == '__main__':
+    LoggerInitializer.init('rinielsenru-mars calculations')
+    Config.init()
+    project_name = 'rinielsenru-mars'
+    session_uids = ['555e59e6-8cc3-11e7-8bba-12ae84a7e6ca']
+    data_provider = KEngineDataProvider(project_name)
+    output = Output()
+    for session in session_uids:
+        data_provider.load_session_data(session)
+        NIELSENRUMARSCalculations(data_provider, output).run_project_calculations()
+
+    # jg = NIELSENMARSRUPOS_SANDJsonGenerator('nielsenmarsrupos-sand')
+    # jg.create_json('template.xlsx')
+    # # tb = NIELSENMARSRUPOS_SANDMARSRUKPIToolBox(data_provider, self.output, 'Hypermarket')
+    # tb = NIELSENMARSRUPOS_SANDKPIToolBox(data_provider, output, 'NIELSENRU_MARS KPIs 2018')
+    # tb.insert_new_kpis_old(project_name, jg.project_kpi_dict.get('kpi_data')[0])
+    # tb.insert_new_kpis_old(project_name)
