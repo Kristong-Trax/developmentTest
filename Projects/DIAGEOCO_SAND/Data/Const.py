1e188366a897e55c2f56145be91b5c6d44904c66
Hunter Young <huntery@traxretail.com>
2018-10-30 15:48:34 -0400

    Refactored to be more in line with other Diageo projects

--- Projects/DIAGEOCO_SAND/Data/Const.py
+++ Projects/DIAGEOCO_SAND/Data/Const.py
@@ -7,12 +7,6 @@ class Const(object):
 
     TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), '..', 'DIAGEOCO_SAND',
                                  'Data', 'Diageo Colombia - Kpi Bank - 29102018.xlsx')
-    RELATIVE_POSITIONING_SHEET_NAME = '2.1 Relative Positioning'
-    RELATIVE_POSITIONING_HEADER_ROW = 4
-    BRAND_BLOCKING_SHEET_NAME = '2.2 Brand Blocking'
-    BRAND_BLOCKING_HEADER_ROW = 6
-    BRAND_POURING_SHEET_NAME = '5.1 Brand Pouring Status '
-    BRAND_POURING_HEADER_ROW = 3
     TOUCH_POINT_SHEET_NAME = 'Touch Point'
     TOUCH_POINT_HEADER_ROW = 2
 
@@ -21,3 +15,4 @@ class Const(object):
     BRAND_BLOCKING_BRAND_FROM_CATEGORY = 'Brand Blocking'
     SECONDARY_DISPLAY = 'Secondary display'
     RELATIVE_POSITION = 'Relative Position'
+    BRAND_POURING = 'Brand Pouring'
