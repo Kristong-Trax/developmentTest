5da124df08697f7a57e8748eaf006ee7cfcbe047
samkowalski <samk@traxretail.com>
2018-11-01 15:53:26 -0400

    fixed index layer issue after groupby

--- Projects/GOOGLEKR_SAND/Utils/SceneKPIToolBox.py
+++ Projects/GOOGLEKR_SAND/Utils/SceneKPIToolBox.py
@@ -28,7 +28,7 @@ class SceneGOOGLEToolBox:
             return
 
         brand_totals = brands_scif.set_index([Const.BRAND, 'brand_fk']) \
-                                  .groupby(level=[0, 1], axis=0)[Const.FACINGS].sum()
+                                  .groupby(level=[0, 1])[Const.FACINGS].sum()
 
         total_facings = scif[Const.FACINGS].sum()
         google_fk, google_facings = brand_totals.reset_index(drop=False, level=1).loc[Const.GOOGLE_BRAND]
