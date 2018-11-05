b0405f12bd8bb072a0343cd8225a7bc2587f2764
idanre <idanr@traxretail.com>
2018-11-05 14:05:26 +0200

    diageoGR assortment migration

--- Projects/DIAGEOGR_SAND/Calculations.py
+++ Projects/DIAGEOGR_SAND/Calculations.py
@@ -5,6 +5,8 @@ from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScrip
 # from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 from Projects.DIAGEOGR_SAND.KPIGenerator import DIAGEOGR_SANDGenerator
+from KPIUtils.GlobalProjects.DIAGEO.KPIGenerator import DIAGEOGenerator
+from KPIUtils.DB.Common import Common
 
 __author__ = 'Nimrod'
 
@@ -12,7 +14,10 @@ __author__ = 'Nimrod'
 class DIAGEOGR_SANDCalculations(BaseCalculationsScript):
     def run_project_calculations(self):
         self.timer.start()
+        common = Common(self.data_provider)
         DIAGEOGR_SANDGenerator(self.data_provider, self.output).main_function()
+        DIAGEOGenerator(self.data_provider, self.output, common).diageo_global_assortment_function()
+        common.commit_results_data_to_new_tables()
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
