f3c0036c9d8461266ee0628a00f0c08ace412633
ilay_s <ilays@traxretail.com>
2018-11-07 16:12:30 +0200

    working on tsingtaobeercn project

--- Projects/TSINGTAOBEERCN_SAND/LocalCalculations.py
+++ Projects/TSINGTAOBEERCN_SAND/LocalCalculations.py
@@ -4,6 +4,22 @@ from Trax.Utils.Conf.Configuration import Config
 from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 from Projects.TSINGTAOBEERCN_SAND.Calculations import Calculations
 
+from Trax.Algo.Calculations.Core.Constants import Keys, Fields, SCENE_ITEM_FACTS_COLUMNS
+from Trax.Algo.Calculations.Core.Vanilla.Calculations import SceneVanillaCalculations
+from Trax.Algo.Calculations.Core.Vanilla.Output import VanillaOutput
+import pandas as pd
+from Projects.TSINGTAOBEERCN_SAND.SceneKpis.SceneCalculations import SceneCalculations
+
+
+def save_scene_item_facts_to_data_provider(data_provider, output):
+    scene_item_facts_obj = output.get_facts()
+    if scene_item_facts_obj:
+        scene_item_facts = scene_item_facts_obj[Keys.SCENE_ITEM_FACTS][Keys.SCENE_ITEM_FACTS].fact_df
+    else:
+        scene_item_facts = pd.DataFrame(columns=SCENE_ITEM_FACTS_COLUMNS)
+    scene_item_facts.rename(columns={Fields.PRODUCT_FK: 'item_id', Fields.SCENE_FK: 'scene_id'}, inplace=True)
+    data_provider.set_scene_item_facts(scene_item_facts)
+
 
 if __name__ == '__main__':
     LoggerInitializer.init('tsingtaobeercn-sand calculations')
@@ -11,6 +27,14 @@ if __name__ == '__main__':
     project_name = 'tsingtaobeercn-sand'
     data_provider = KEngineDataProvider(project_name)
     session = '635ED507-A0CE-4DCC-8F71-1D4D39F2271C'
+    for scene in [12, 13]:
+        print('Calculating scene id: ' + str(scene))
+        data_provider = KEngineDataProvider(project_name)
+        data_provider.load_scene_data(session, scene)
+        output = VanillaOutput()
+        SceneVanillaCalculations(data_provider, output).run_project_calculations()
+        save_scene_item_facts_to_data_provider(data_provider, output)
+        SceneCalculations(data_provider).calculate_kpis()
     data_provider.load_session_data(session)
     output = Output()
     Calculations(data_provider, output).run_project_calculations()
--- Projects/TSINGTAOBEERCN_SAND/Utils/SceneKPIToolBox.py
+++ Projects/TSINGTAOBEERCN_SAND/Utils/SceneKPIToolBox.py
@@ -1,16 +1,16 @@
 from Trax.Algo.Calculations.Core.DataProvider import Data
 from KPIUtils_v2.Calculations.CalculationsUtils.GENERALToolBoxCalculations import GENERALToolBox
 
-
-import pandas as pd
-
 __author__ = 'ilays'
 
+FACINGS_PER_SKU_SCENE_LOCATION_1_KPI = 'FACINGS_PER_SKU_SCENE_LOCATION_1'
+FACINGS_PER_SKU_SCENE_LOCATION_OTHER_KPI = 'FACINGS_PER_SKU_SCENE_LOCATION_OTHER'
 
 class SceneToolBox:
 
     def __init__(self, data_provider, common):
         self.data_provider = data_provider
+        self.common = common
         self.project_name = self.data_provider.project_name
         self.products = self.data_provider[Data.PRODUCTS]
         self.all_products = self.data_provider[Data.ALL_PRODUCTS]
@@ -26,23 +26,25 @@ class SceneToolBox:
         This function calculates the KPI results.
         """
 
-        self.calculate_facings_per_sku(location_type_1=True)
-        self.calculate_facings_per_sku(location_type_1=False)
+        self.calculate_facings_per_sku(location_type_1=True, kpi_name=FACINGS_PER_SKU_SCENE_LOCATION_1_KPI)
+        self.calculate_facings_per_sku(location_type_1=False, kpi_name=FACINGS_PER_SKU_SCENE_LOCATION_OTHER_KPI)
 
 
-    def calculate_facings_per_sku(self, location_type_1):
+    def calculate_facings_per_sku(self, location_type_1, kpi_name):
         if location_type_1:
-            self.count_facings_by_scenes(self.scif, {'location_type':(self.tools.INCLUDE_FILTER), "product_type":"SKU"})
+            result_df = self.count_facings_by_scenes(self.scif, {'location_type':(self.tools.INCLUDE_FILTER),
+                                                                "product_type":"SKU"})[['product_fk', 'facings']]
         else:
-            self.count_facings_by_scenes(self.scif, {'location_type':(self.tools.EXCLUDE_FILTER), "product_type":"SKU"})
-
-        # self.common.write_to_db_result(fk=atomic_pk, numerator_id=1, identifier_parent=parent_kpi,
-        #                                   numerator_result=numerator_number_of_facings, denominator_id=self.store_fk,
-        #                                   denominator_result_after_actions=3, should_enter=True,
-        #                                   denominator_result=denominator_number_of_total_facings, result=count_result)
+            result_df = self.count_facings_by_scenes(self.scif, {'location_type':(self.tools.EXCLUDE_FILTER),
+                                                                "product_type":"SKU"})[['product_fk', 'facings']]
+        kpi_fk = self.common.get_kpi_fk_by_kpi_name(kpi_name=kpi_name)
+        for index, row in result_df.iterrows():
+            result = row['facings']
+            self.common.write_to_db_result(fk=kpi_fk, numerator_id=row['product_fk'], result=result, by_scene=True,
+                                                                                        denominator_id=self.store_id)
 
     def count_facings_by_scenes(self, df, filters):
         facing_data = df[self.tools.get_filter_condition(df, **filters)]
         # filter by scene_id and by template_name (scene type)
-        scene_types_groupby = facing_data.groupby(['scene_id'])['facings'].sum().reset_index()
-        return scene_types_groupby
+        # scene_types_groupby = facing_data.groupby(['scene_id'])['facings'].sum().reset_index()
+        return facing_data
