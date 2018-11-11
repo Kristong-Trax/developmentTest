7050a6f59d961d0399869541b4f047737957c3e4
ilay_s <ilays@traxretail.com>
2018-11-11 16:54:12 +0200

    add 'touch point' exception

--- Projects/DIAGEOMX_SAND/Utils/KPIToolBox.py
+++ Projects/DIAGEOMX_SAND/Utils/KPIToolBox.py
@@ -129,7 +129,8 @@ class DIAGEOMX_SANDToolBox:
 
         set_score=0
         for set_name in set_names:
-            if set_name not in self.tools.KPI_SETS_WITHOUT_A_TEMPLATE and set_name not in self.set_templates_data.keys():
+            if set_name not in self.tools.KPI_SETS_WITHOUT_A_TEMPLATE and set_name not in \
+                                    self.set_templates_data.keys() and set_name not in ('TOUCH POINT'):
                 self.set_templates_data[set_name] = self.tools.download_template(set_name)
 
             if set_name in ('Relative Position'):
