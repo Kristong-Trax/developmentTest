b1ee989f1ca1ad4efe48c3853db7ee8016847f01
sathiyanarayanand <sathiyanarayanan@traxretail.com>
2018-11-15 20:03:14 +0800

    BATAU - kpis

--- Projects/BATAU/LocalCalculations.py
+++ Projects/BATAU/LocalCalculations.py
@@ -1,4 +1,4 @@
-
+import sys
 from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
 from Trax.Utils.Conf.Configuration import Config
 from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     LoggerInitializer.init('batau custom kpi calculations')
     Config.init()
     project_name = 'batau'
+    print(sys.path)
     data_provider = KEngineDataProvider(project_name)
     session = '1707F91D-E4A6-401B-86C8-E04AA942BCA8'
     #session ='EF7A4F7A-4C99-4179-8886-BA95DCF22F0C'
