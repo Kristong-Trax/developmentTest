53f40b157a757880be1f5682a7f6c90569102fb6
sathiyanarayanand <sathiyanarayanan@traxretail.com>
2018-11-24 19:48:29 +0800

    BATAU - kpis

--- Projects/BATAU_SAND/LocalCalculations.py
+++ Projects/BATAU_SAND/LocalCalculations.py
@@ -10,8 +10,11 @@ if __name__ == '__main__':
     Config.init()
     project_name = 'batau-sand'
     data_provider = KEngineDataProvider(project_name)
+    #session = '84751c4f-cb52-4863-b528-5531c7d9b4f0'
     #session = 'f90ad7a0-23c5-46d8-9abb-f9ba04806ff9'
-    session = 'a59be007-5c89-4f52-8389-086afa802dc8'
+    #session = '0DB58E6A-E7AB-48B7-A99B-536882F40E61'
+    session = '8D338894-6618-4516-9DC9-36745F5553CF'
+    #session = 'a59be007-5c89-4f52-8389-086afa802dc8'
     data_provider.load_session_data(session)
     output = Output()
     Calculations(data_provider, output).run_project_calculations()
