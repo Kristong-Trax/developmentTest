45388468251b92f0bea2d99f063b6984cacde51c
sathiyanarayanand <sathiyanarayanan@traxretail.com>
2018-11-27 17:02:48 +0800

    BATAU - kpis

--- Projects/BATAU/LocalCalculations.py
+++ Projects/BATAU/LocalCalculations.py
@@ -11,7 +11,9 @@ from Projects.BATAU_SAND.Calculations import Calculations
 #     project_name = 'batau'
 #     data_provider = KEngineDataProvider(project_name)
 #     #session = '1707F91D-E4A6-401B-86C8-E04AA942BCA8'
-#     session = '557077e4-b393-4284-91a7-340cc07c6db0'
+#     #session = '557077e4-b393-4284-91a7-340cc07c6db0'
+#     #session = '734f6c43-99e0-4d43-982e-075ad967badb'
+#     session = '6fea70ab-ff49-4730-aaca-ece17e8f6674'
 #     data_provider.load_session_data(session)
 #     output = Output()
 #     Calculations(data_provider, output).run_project_calculations()
