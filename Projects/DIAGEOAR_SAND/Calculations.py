7a25752ff9a6c3062c5a77fe6e532e09d2dfdb28
ilay_s <ilays@traxretail.com>
2018-11-28 10:11:03 +0200

    migrating diageoar-sand

--- Projects/DIAGEOAR_SAND/Calculations.py
+++ Projects/DIAGEOAR_SAND/Calculations.py
@@ -16,12 +16,12 @@ class DIAGEOARCalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('diageoar-sand calculations')
-    Config.init()
-    project_name = 'diageoar-sand'
-    data_provider = KEngineDataProvider(project_name)
-    session = 'ca8a3f2e-693a-11e8-b53f-127c7ae559da'
-    data_provider.load_session_data(session)
-    output = Output()
-    DIAGEOARCalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('diageoar-sand calculations')
+#     Config.init()
+#     project_name = 'diageoar-sand'
+#     data_provider = KEngineDataProvider(project_name)
+#     session = 'ca8a3f2e-693a-11e8-b53f-127c7ae559da'
+#     data_provider.load_session_data(session)
+#     output = Output()
+#     DIAGEOARCalculations(data_provider, output).run_project_calculations()
