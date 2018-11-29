241ced4b098590f422dfc68450bb6e3ef727413a
idanre <idanr@traxretail.com>
2018-11-29 10:27:38 +0200

    diageoke-sand

--- Projects/DIAGEONORDICS_SAND/Utils/KPIToolBox.py
+++ Projects/DIAGEONORDICS_SAND/Utils/KPIToolBox.py
@@ -101,8 +101,7 @@ class DIAGEONORDICS_SANDToolBox:
         This function calculates the KPI results.
         """
         # Global assortment kpis
-        assortment_res_dict = DIAGEOGenerator(self.data_provider, self.output,
-                                              self.common).diageo_global_assortment_function_v2()
+        assortment_res_dict = self.diageo_generator.diageo_global_assortment_function_v2()
         self.commonV2.save_json_to_new_tables(assortment_res_dict)
 
         for set_name in set_names:
