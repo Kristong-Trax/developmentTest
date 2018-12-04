ad7609a514d995f6946d7ac2affae89d6f18c3e2
ilay_s <ilays@traxretail.com>
2018-12-04 16:57:44 +0200

    Migrating diageoau

--- Projects/DIAGEOAU/Calculations.py
+++ Projects/DIAGEOAU/Calculations.py
@@ -1,8 +1,8 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-# from Trax.Utils.Conf.Configuration import Config
-# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 from KPIUtils.GlobalProjects.DIAGEO.KPIGenerator import DIAGEOGenerator
 from KPIUtils.DB.Common import Common
 from Projects.DIAGEOAU.KPIGenerator import DIAGEOAUGenerator
@@ -13,10 +13,7 @@ __author__ = 'Nimrod'
 class DIAGEOAUCalculations(BaseCalculationsScript):
     def run_project_calculations(self):
         self.timer.start()
-        common = Common(self.data_provider)
         DIAGEOAUGenerator(self.data_provider, self.output).main_function()
-        DIAGEOGenerator(self.data_provider, self.output, common).diageo_global_assortment_function()
-        common.commit_results_data_to_new_tables()
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
