e6e441ab282f56edd47ec1500c529796dbe13ab6
idanre <idanr@traxretail.com>
2018-12-10 14:16:10 +0200

    diageoru fix

--- Projects/DIAGEORU/Utils/KPIToolBox.py
+++ Projects/DIAGEORU/Utils/KPIToolBox.py
@@ -116,20 +116,6 @@ class DIAGEORUToolBox:
                 set_score = self.tools.calculate_number_of_scenes(location_type='Secondary')
                 self.save_level2_and_level3(set_name, set_name, set_score)
 
-            elif set_name in ('Visible to Customer', 'Visible to Consumer %'):
-                # Global function
-                sku_list = filter(None, self.scif[self.scif['product_type'] == 'SKU'].product_ean_code.tolist())
-                res_dict = self.diageo_generator.diageo_global_visible_percentage(sku_list)
-
-                if res_dict:
-                    # Saving to new tables
-                    parent_res = res_dict[-1]
-                    self.commonV2.save_json_to_new_tables(res_dict)
-
-                    # Saving to old tables
-                    set_score = result = parent_res['result']
-                    self.save_level2_and_level3(set_name=set_name, kpi_name=set_name, score=result)
-
             if set_score == 0:
                 pass
             elif set_score is False:
