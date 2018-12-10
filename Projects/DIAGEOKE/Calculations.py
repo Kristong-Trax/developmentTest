b49ebcd4c6d86e3b0f07a459beb32fdaed44573f
idanre <idanr@traxretail.com>
2018-12-10 14:08:57 +0200

    diageoke-migration-prod

--- Projects/DIAGEOKE/Calculations.py
+++ Projects/DIAGEOKE/Calculations.py
@@ -1,8 +1,8 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-from Trax.Algo.Calculations.Core.DataProvider import ACEDataProvider, Output, KEngineDataProvider
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from Trax.Algo.Calculations.Core.DataProvider import ACEDataProvider, Output, KEngineDataProvider
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 from Projects.DIAGEOKE.KPIGenerator import DIAGEOKEGenerator
 
@@ -16,12 +16,12 @@ class DIAGEOKECalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('diageoke calculations')
-    Config.init()
-    project_name = 'diageoke'
-    data_provider = KEngineDataProvider(project_name)
-    session = '28051307-a521-4a61-9fba-4c78ae9969f9'
-    data_provider.load_session_data(session)
-    output = Output()
-    DIAGEOKECalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('diageoke calculations')
+#     Config.init()
+#     project_name = 'diageoke'
+#     data_provider = KEngineDataProvider(project_name)
+#     session = '28051307-a521-4a61-9fba-4c78ae9969f9'
+#     data_provider.load_session_data(session)
+#     output = Output()
+#     DIAGEOKECalculations(data_provider, output).run_project_calculations()
