c52caf238bd32716486fcb09613946600d589340
idanre <idanr@traxretail.com>
2018-12-10 14:02:43 +0200

    diageoke-migration-prod

--- Projects/DIAGEOKE/Calculations.py
+++ Projects/DIAGEOKE/Calculations.py
@@ -1,12 +1,10 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-# from Trax.Algo.Calculations.Core.DataProvider import ACEDataProvider, Output, KEngineDataProvider
-# from Trax.Utils.Conf.Configuration import Config
-# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+from Trax.Algo.Calculations.Core.DataProvider import ACEDataProvider, Output, KEngineDataProvider
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 from Projects.DIAGEOKE.KPIGenerator import DIAGEOKEGenerator
-from KPIUtils.GlobalProjects.DIAGEO.KPIGenerator import DIAGEOGenerator
-from KPIUtils.DB.Common import Common
 
 __author__ = 'Nimrod'
 
@@ -14,19 +12,16 @@ __author__ = 'Nimrod'
 class DIAGEOKECalculations(BaseCalculationsScript):
     def run_project_calculations(self):
         self.timer.start()
-        common = Common(self.data_provider)
         DIAGEOKEGenerator(self.data_provider, self.output).main_function()
-        DIAGEOGenerator(self.data_provider, self.output, common).diageo_global_assortment_function()
-        common.commit_results_data_to_new_tables()
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-# if __name__ == '__main__':
-#     LoggerInitializer.init('diageoke calculations')
-#     Config.init()
-#     project_name = 'diageoke'
-#     data_provider = KEngineDataProvider(project_name)
-#     session = '41dd9f2c-4a92-4a70-bb64-a4d24e349fbd'
-#     data_provider.load_session_data(session)
-#     output = Output()
-#     DIAGEOKECalculations(data_provider, output).run_project_calculations()
+if __name__ == '__main__':
+    LoggerInitializer.init('diageoke calculations')
+    Config.init()
+    project_name = 'diageoke'
+    data_provider = KEngineDataProvider(project_name)
+    session = '28051307-a521-4a61-9fba-4c78ae9969f9'
+    data_provider.load_session_data(session)
+    output = Output()
+    DIAGEOKECalculations(data_provider, output).run_project_calculations()
