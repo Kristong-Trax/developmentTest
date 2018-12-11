278a738b7cc5bbeb807a7b397318a33cbf1bf231
limorc <limorc@traxretail.com>
2018-12-11 11:34:56 +0200

    taking results from dictionary , manufacturer fk not hardcoded

--- Projects/DIAGEOCO_SAND/Utils/KPIToolBox.py
+++ Projects/DIAGEOCO_SAND/Utils/KPIToolBox.py
@@ -25,7 +25,7 @@ class DIAGEOCO_SANDToolBox:
     LEVEL1 = 1
     LEVEL2 = 2
     LEVEL3 = 3
-    diageo_manufacturer_fk = 74
+    diageo_manufacturer = 'DIAGEO'
 
     def __init__(self, data_provider, output):
         self.output = output
@@ -105,13 +105,15 @@ class DIAGEOCO_SANDToolBox:
                 set_score = self.calculate_relative_position_sets(set_name)
 
             elif set_name == 'Activation Standard':
-                manufacturer_fk = self.diageo_manufacturer_fk
+
+                manufacturer_fk = self.all_products[self.all_products['manufacturer_name'] == self.diageo_manufacturer]['manufacturer_fk'].values[0]
+
                 results_list = self.global_gen.diageo_global_activation_standard_function(total_scores_dict,
                                                                                           self.set_templates_data[set_name], self.store_id,
                                                                                           manufacturer_fk)
                 for result in results_list[1]:
                     self.save_level2_and_level3(result[0], result[1], result[2])
-                self.write_to_db_result(results_list[2][0], results_list[2][1], results_list[2][2])
+                self.write_to_db_result(results_list[2]['fk'], results_list[2]['score'], results_list[2]['level'])
                 self.save_results_to_db(results_list[0])
 
             elif set_name == 'TOUCH POINT':
