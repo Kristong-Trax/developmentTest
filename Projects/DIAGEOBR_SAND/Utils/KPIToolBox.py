bcec2362b28ac6ff77d047003d327c42cd873084
ilay_s <ilays@traxretail.com>
2018-12-13 10:15:15 +0200

    migrating all projects to new assortment

--- Projects/DIAGEOBR_SAND/Utils/KPIToolBox.py
+++ Projects/DIAGEOBR_SAND/Utils/KPIToolBox.py
@@ -111,9 +111,6 @@ class DIAGEOBR_SANDToolBox:
         """
         log_runtime('Updating templates')(self.tools.update_templates)()
 
-        # old assortment
-        self.diageo_generator.diageo_global_assortment_function()
-
         # Global assortment kpis
         assortment_res_dict = self.diageo_generator.diageo_global_assortment_function_v2()
         self.commonV2.save_json_to_new_tables(assortment_res_dict)
