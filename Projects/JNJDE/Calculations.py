5197c6e743fadc3acf95618079919f4706333b65
israelshok <israels@traxretail.com>
2018-12-17 16:01:59 +0200

    jnjde

--- Projects/JNJDE/Calculations.py
+++ Projects/JNJDE/Calculations.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 from KPIUtils.GlobalProjects.JNJ.KPIGenerator import JNJGenerator
 from KPIUtils.DB.Common import Common
+from KPIUtils_v2.DB.CommonV2 import Common as Common_V2
+
 from KPIUtils_v2.Utils.Decorators.Decorators import log_runtime
 
 __author__ = 'israels'
@@ -22,11 +24,23 @@ class JNJDECalculations(BaseCalculationsScript):
         survey_template = pd.read_excel(survey_template_path, sheetname='Sheet1')
         eye_hand_lvl_template = pd.read_excel(eye_hand_lvl_template_path)
         common = Common(self.data_provider)
-        jnj_generator = JNJGenerator(self.data_provider, self.output, common)
+        common_v2 = Common_V2(self.data_provider)
+        jnj_generator = JNJGenerator(self.data_provider, self.output, common_v2)
         # jnj_generator.secondary_placement_location_quality(survey_template)
         # jnj_generator.secondary_placement_location_visibility_quality(survey_template)
+
+        # KPI 2 - Share of Shelf
         jnj_generator.calculate_auto_assortment()
-        jnj_generator.promo_calc(sales_reps_date='2018-09-30')
+        # KPI 3 - OOS
+        jnj_generator.calculate_auto_assortment()
+        # KPI 4 - Share of shelf - Hand & Eye
         jnj_generator.eye_hand_level_sos_calculation(eye_hand_lvl_template)
+        # KPI 5 IR - Activation compliance vs plans
+        jnj_generator.promo_calc(sales_reps_date='2018-09-30')
+        # KPI 9 - MSL
+        jnj_generator.assortment_calculation()
+        # KPI 10 - New Display compliance
+        jnj_generator.display_compliance_calculation()
+
         common.commit_results_data_to_new_tables()
         self.timer.stop('KPIGenerator.run_project_calculations')
