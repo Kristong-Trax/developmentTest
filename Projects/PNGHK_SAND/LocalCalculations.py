c81437c32583016098070b7d30c699e06892401e
ilay_s <ilays@traxretail.com>
2018-12-23 16:32:41 +0200

    Initiate a new project - pnghk-sand

---
+++ Projects/PNGHK_SAND/LocalCalculations.py
@@ -0,0 +1,18 @@
+
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+from Projects.PNGHK_SAND.Calculations import Calculations
+
+
+if __name__ == '__main__':
+    LoggerInitializer.init('pnghk-sand calculations')
+    Config.init()
+    project_name = 'pnghk-sand'
+    data_provider = KEngineDataProvider(project_name)
+    data_provider = KEngineDataProvider(project_name)
+    sessions = ['69426997-1E74-4DB2-9695-09DCE1EF58E3','e9d4bf08-8aa1-4840-871f-a15643dda368']
+    for session in sessions:
+        data_provider.load_session_data(session)
+        output = Output()
+        Calculations(data_provider, output).run_project_calculations()
