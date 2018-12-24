1adc603f793721238578593734e588b58baa516e
elisuiza <elis@traxretail.com>
2018-12-24 16:11:04 +0200

    GFKDE

--- Projects/GFKDE/SOS_Category.py
+++ Projects/GFKDE/SOS_Category.py
@@ -1,12 +1,25 @@
-from Trax.Algo.Calculations.Core.KPI.UnifiedKPICalculation import UnifiedCalculationsScript
+from KPIUtils.GlobalProjects.GFK.Base.BaseCalculationScript import GfkBaseCalculation
+from Trax.Algo.Calculations.Core.Constants import ProductTypes
 from Trax.Utils.DesignPatterns.Decorators import classproperty
 
 
-class SOSCategory_KPI(UnifiedCalculationsScript):
+class SOSCategory_KPI(GfkBaseCalculation):
 
     @classproperty
     def kpi_type(self):
         return "SOS_CATEGORY"
+
+    def kpi_policy(self):
+        return {'scenes': {
+            'included': [],
+            'excluded': []
+        },
+            'product_type': [ProductTypes.P_TYPE_SKU, ProductTypes.P_TYPE_OTHER],
+            'numerator': '',
+            'denominator': 'Category',
+            'include_stacking': True
+
+        }
 
     def calculate(self):
         pass
--- Projects/GFKDE/SoSOnGondolaEndCategory.py
+++ Projects/GFKDE/SoSOnGondolaEndCategory.py
@@ -1,12 +1,23 @@
-from Trax.Algo.Calculations.Core.KPI.UnifiedKPICalculation import UnifiedCalculationsScript
+from KPIUtils.GlobalProjects.GFK.Base.BaseCalculationScript import GfkBaseCalculation
+from Trax.Algo.Calculations.Core.Constants import ProductTypes
 from Trax.Utils.DesignPatterns.Decorators import classproperty
 
 
-class SosOnGondolaEndCategory_KPI(UnifiedCalculationsScript):
+class SosOnGondolaEndCategory_KPI(GfkBaseCalculation):
 
     @classproperty
     def kpi_type(self):
         return "SOS_ON_GONDOLA_END_CATEGORY"
+
+    def kpi_policy(self):
+        return {'scenes': {
+            'included': [{'name': 'Washing Machines - Gondola End'}],
+            'excluded': []
+        },
+            'product_type': [ProductTypes.P_TYPE_SKU, ProductTypes.P_TYPE_OTHER],
+            'numerator': '',
+            'denominator': 'Category'
+        }
 
     def calculate(self):
         pass
