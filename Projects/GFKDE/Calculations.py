cc5f8a7aafe991b23cfdf7f985e39ae2d915f1fe
elisuiza <elis@traxretail.com>
2019-01-06 15:38:47 +0200

    GFKDE

--- Projects/GFKDE/Calculations.py
+++ Projects/GFKDE/Calculations.py
@@ -49,6 +49,7 @@ DISTRIBUTION = [UniqueDistributionPerBrand_KPI, UniqueDistributionPerManufacture
 
 KPIs = SOS + SOS_SUB + GONDOLA_END + GONDOLA_END_SUB + SOS_BRANDED + SOS_BRANDED_SUB + SHARE_BRANDED + SHARE_BRANDED_SUB + PRESENCE + DISTRIBUTION
 
+
 class GFKDECalculations(BaseCalculationsScript):
     """
     https://confluence.trax-cloud.com/pages/resumedraft.action?draftId=174198555&draftShareId=feac8c7a-ec57-4b36-b380-190d3668debc
@@ -70,36 +71,36 @@ class GFKDECalculations(BaseCalculationsScript):
             GFKDataManager.reset()
 
 
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-
-if __name__ == '__main__':
-    LoggerInitializer.init('gfkde calculations')
-    Config.init()
-    project_name = 'gfkde'
-    data_provider = KEngineDataProvider(project_name)
-    sessions = ['ad9cfb70-cdef-42e7-bfed-2bc9c4012ce1',
-                '492a6090-9e75-43f7-9461-4315cf92f9e0',
-                '498668b8-0644-4dd2-9391-2702b7d4cffb',
-                '56ec7fae-a00d-4b2f-b62f-94d5a0a6a172',
-                '9d48cf56-6f9a-4ac6-ba29-d4d587aeaa2d',
-                '29d8ecb9-d335-403b-bbd5-411ff1d352a2',
-                'b84269ac-774e-4e9b-a2f3-9984fefca213',
-                'fae5cf3a-16ea-40e3-9016-ff4bb2730291',
-                '77f12245-8b0c-414e-b160-2f60d7cb61ec',
-                '20d2c696-bd96-40cb-a6e0-dc731fcca34a',
-                'a707419d-3a76-4d66-a45b-5be7762baa89',
-                '4ad2199b-02c2-4dbb-a8a2-204b9d872ca1',
-                '6a1c63e4-58e1-45e7-a6b0-1125a35b944d',
-                '0c2d4138-fa35-4328-b156-281ad72bf8a9',
-                'd7ced1ad-0dd5-4bee-9061-51d2fde8c99c',
-                '5f39c5b0-9a13-408c-bcca-6d4e1df72d25',
-                '9e6fc5ea-62fe-45c2-be69-a80f297c0103',
-                '5c96586a-6c8e-4283-855c-f9e527696f61',
-                '9c579504-defe-4504-a15e-925264a6408f']
-    for session in sessions:
-        data_provider.load_session_data(session)
-        GFKDECalculations(data_provider=data_provider, output=None).run_project_calculations()
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+#
+# if __name__ == '__main__':
+#     LoggerInitializer.init('gfkde calculations')
+#     Config.init()
+#     project_name = 'gfkde'
+#     data_provider = KEngineDataProvider(project_name)
+#     sessions = ['ad9cfb70-cdef-42e7-bfed-2bc9c4012ce1',
+#                 '492a6090-9e75-43f7-9461-4315cf92f9e0',
+#                 '498668b8-0644-4dd2-9391-2702b7d4cffb',
+#                 '56ec7fae-a00d-4b2f-b62f-94d5a0a6a172',
+#                 '9d48cf56-6f9a-4ac6-ba29-d4d587aeaa2d',
+#                 '29d8ecb9-d335-403b-bbd5-411ff1d352a2',
+#                 'b84269ac-774e-4e9b-a2f3-9984fefca213',
+#                 'fae5cf3a-16ea-40e3-9016-ff4bb2730291',
+#                 '77f12245-8b0c-414e-b160-2f60d7cb61ec',
+#                 '20d2c696-bd96-40cb-a6e0-dc731fcca34a',
+#                 'a707419d-3a76-4d66-a45b-5be7762baa89',
+#                 '4ad2199b-02c2-4dbb-a8a2-204b9d872ca1',
+#                 '6a1c63e4-58e1-45e7-a6b0-1125a35b944d',
+#                 '0c2d4138-fa35-4328-b156-281ad72bf8a9',
+#                 'd7ced1ad-0dd5-4bee-9061-51d2fde8c99c',
+#                 '5f39c5b0-9a13-408c-bcca-6d4e1df72d25',
+#                 '9e6fc5ea-62fe-45c2-be69-a80f297c0103',
+#                 '5c96586a-6c8e-4283-855c-f9e527696f61',
+#                 '9c579504-defe-4504-a15e-925264a6408f']
+#     for session in sessions:
+#         data_provider.load_session_data(session)
+#         GFKDECalculations(data_provider=data_provider, output=None).run_project_calculations()
 
 
