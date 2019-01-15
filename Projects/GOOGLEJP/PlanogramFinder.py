e4f050cc41eb0ca6b0ae77fcd10935bc964c7606
elyashivm <elyashivm@traxretail.com>
2019-01-15 08:50:09 +0200

    Merge branch 'master' of bitbucket.org:traxtechnology/kpi_factory into PROS-6829-set-up-google-globals
    # Conflicts:
    #	Projects/GOOGLEJP/KPIGenerator.py

--- Projects/GOOGLEJP/PlanogramFinder.py
+++ Projects/GOOGLEJP/PlanogramFinder.py
@@ -1,58 +1,20 @@
-from Trax.Apps.Services.KEngine.Handlers.Utils.Scripts import PlanogramComplianceBaseClass
-from Trax.Apps.Services.KEngine.PRE.Algo.PlanogramCompliance import get_tag_planogram_compliance
-import itertools
+from Trax.Apps.Services.KEngine.Handlers.Utils.Scripts import PlanogramFinderBaseClass
 import pandas as pd
 from Trax.Apps.Services.KEngine.PRE.Resources.Constants import Keys
-from Trax.Algo.Calculations.Core.Constants import ProductTypes, PlanogramTagCompliance
+from Trax.Utils.Logging.Logger import Log
 
 __author__ = 'Shivi'
 
 
-class PlanogramCompliance(PlanogramComplianceBaseClass):
+class PlanogramCompliance(PlanogramFinderBaseClass):
 
-    def get_compliance(self):
-        planogram_data = self._data_provider.planogram_data
-        scene_data = self._data_provider.matches
-        self.get_iterated_position(scene_data, planogram_data)
+    def get_planogram_id(self):
+        project_name = self._data_provider._project_name
 
-    @staticmethod
-    def _get_df_of_bay(df, bay_number):
-        df_answer = df[df['bay_number'] == bay_number]
-        df_answer['bay_number'] = 1
-        return df_answer
-
-    def get_iterated_position(self, scene_data, planogram_data):
-        """
-        For multi-sided POGs. It iterates about all the possible permutations of the scene bays and the POG bays,
-        and returns the tag_compliance of the permutation with the best score (most of "in position" products).
-        """
-        scene_bays = scene_data['bay_number'].unique().tolist()
-        pog_bays = planogram_data['bay_number'].unique().tolist()
-        all_combinations = pd.DataFrame()
-        combinations = list(itertools.product(pog_bays, scene_bays))
-        for combination in combinations:
-            scene_bay, pog_bay = combination[0], combination[1]
-            scene_bay_data = self._get_df_of_bay(scene_data, scene_bay)
-            pog_bay_data = self._get_df_of_bay(planogram_data, pog_bay)
-            tag_compliance = get_tag_planogram_compliance(scene_bay_data, pog_bay_data)
-            score = float(len(
-                tag_compliance[tag_compliance[Keys.COMPLIANCE_STATUS_FK] == PlanogramTagCompliance.CORRECTLY_POSITIONED]
-            )) / len(tag_compliance)
-            all_combinations = all_combinations.append(
-                {"scene_bay": scene_bay, "pog_bay": pog_bay, "score": score, "tag_compliance": tag_compliance},
-                ignore_index=True)
-        permutations = [zip(x, pog_bays) for x in itertools.permutations(scene_bays, len(pog_bays))]
-        final_tag_compliance, highest_score = None, 0
-        for permutation in permutations:
-            score = 0
-            temp_final_tag_compliance = pd.DataFrame()
-            for combination in permutation:
-                combination_data = all_combinations[(all_combinations['scene_bay'] == combination[0]) &
-                                                    (all_combinations['pog_bay'] == combination[1])]
-                score += combination_data['score'].sum()
-                temp_final_tag_compliance = temp_final_tag_compliance.append(combination_data['tag_compliance'][0],
-                                                                             ignore_index=True)
-            if score > highest_score:
-                highest_score = score
-                final_tag_compliance = temp_final_tag_compliance
-        return final_tag_compliance
+    def get_planogram_id_by_policies(self, project_name, data_provider):
+        policies_table = pd.DataFrame()
+        if policies_table.empty:
+            Log.error("There is no Planogram that matches this scene.")
+            return None
+        else:
+            return policies_table.iloc[0][Keys.PLANOGRAM_FK]
--- Projects/GOOGLEKR/PlanogramFinder/PlanogramFinder.py
+++ Projects/GOOGLEKR/PlanogramFinder/PlanogramFinder.py
@@ -1,20 +1,82 @@
 from Trax.Apps.Services.KEngine.Handlers.Utils.Scripts import PlanogramFinderBaseClass
 import pandas as pd
-from Trax.Apps.Services.KEngine.PRE.Resources.Constants import Keys
 from Trax.Utils.Logging.Logger import Log
+from Trax.Data.Projects.Connector import ProjectConnector
+from Trax.Cloud.Services.Connector.Keys import DbUsers
+import json
 
 __author__ = 'Shivi'
+
+SCENE_INFO_QUERY = """  SELECT 
+                            stores.store_type,
+                            stores.store_number_1,
+                            templates.pk AS 'template_fk',
+                            templates.template_group
+                        FROM
+                            (SELECT 
+                                *
+                            FROM
+                                probedata.scene
+                            WHERE
+                                pk = {}) scenes
+                                JOIN
+                            probedata.session sessions ON scenes.session_uid = sessions.session_uid
+                                JOIN
+                            static.stores stores ON stores.pk = sessions.store_fk
+                                JOIN
+                            static.template templates ON scenes.template_fk = templates.pk;"""
+PLANOGRAMS_QUERY = "SELECT * FROM pservice.planogram_custom_policy;"
+STORE_TYPE = "store_type"
+STORE_NUMBER_1 = "store_number_1"
+TEMPLATE_FK = "template_fk"
+TEMPLATE_GROUP = "template_group"
+PLANOGRAM_FK = "planogram_fk"
+NECESSARY_COLUMNS = [STORE_TYPE, STORE_NUMBER_1, TEMPLATE_FK, TEMPLATE_GROUP]
 
 
 class PlanogramCompliance(PlanogramFinderBaseClass):
 
     def get_planogram_id(self):
-        project_name = self._data_provider.project_name
+        self.project_name = self._data_provider._project_name
+        self.scene_id = self._data_provider._scene_id
+        self.rds_conn = self.rds_connection()
+        return self.get_scene_info_and_planograms()
 
-    def get_planogram_id_by_policies(self, project_name, data_provider):
-        policies_table = pd.DataFrame()
-        if policies_table.empty:
+    def get_planogram_id_by_policies(self):
+        if self.planogram_policies.empty:
             Log.error("There is no Planogram that matches this scene.")
             return None
-        else:
-            return policies_table.iloc[0][Keys.PLANOGRAM_FK]
+        filtered_planograms = self.planogram_policies
+        for column in NECESSARY_COLUMNS:
+            filtered_planograms = filtered_planograms[filtered_planograms[column] == self.scene_info[column] |
+                                                      filtered_planograms[column].isnan]
+        return filtered_planograms.iloc[0][PLANOGRAM_FK]
+
+    def get_scene_info_and_planograms(self):
+        self.rds_conn.connect_rds()
+        self.scene_info = pd.read_sql_query(SCENE_INFO_QUERY.format(self.scene_id), self.rds_conn.db).iloc[0]
+        self.planograms_info = pd.read_sql_query(PLANOGRAMS_QUERY, self.rds_conn.db)
+        self.get_planograms_details()
+
+    def get_planograms_details(self):
+        if self.planograms_info.empty:
+            return
+        policies_df = self.planograms_info.policy.apply(json.loads).apply(pd.io.json.json_normalize).pipe(
+            lambda x: pd.concat(x.values))
+        pogs_df = self.planograms_info.drop(['policy'], axis=1)
+        self.planogram_policies = pd.concat([pogs_df.reset_index(drop=True), policies_df.reset_index(drop=True)],
+                                            axis=1)
+        for column in NECESSARY_COLUMNS:
+            if not self.planogram_policies.has_key(column):
+                self.planogram_policies[column] = None
+
+    def rds_connection(self):
+        if not hasattr(self, '_rds_conn'):
+            self._rds_conn = ProjectConnector(self.project_name, DbUsers.CalculationEng)
+        try:
+            pd.read_sql_query('select pk from probedata.session limit 1', self._rds_conn.db)
+        except Exception as e:
+            self._rds_conn.disconnect_rds()
+            self._rds_conn = ProjectConnector(self.project_name, DbUsers.CalculationEng)
+            Log.error(e.message)
+        return self._rds_conn
