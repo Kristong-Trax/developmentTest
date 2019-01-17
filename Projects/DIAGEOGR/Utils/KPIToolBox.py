a01c82dff19baf52d3da834af0cb81c7d1bae1c2
elyashivm <elyashivm@traxretail.com>
2019-01-17 16:00:01 +0200

    menu diageogr

--- Projects/DIAGEOGR/Utils/KPIToolBox.py
+++ Projects/DIAGEOGR/Utils/KPIToolBox.py
@@ -70,7 +70,7 @@ class DIAGEOGRToolBox:
 
         self.commonV2 = CommonV2(self.data_provider)
         self.tools = DIAGEOToolBox(self.data_provider, output, match_display_in_scene=self.match_display_in_scene)
-        self.diageo_generator = DIAGEOGenerator(self.data_provider, self.output, self.common)
+        self.diageo_generator = DIAGEOGenerator(self.data_provider, self.output, self.common, menu=True)
 
     def get_kpi_static_data(self):
         """
