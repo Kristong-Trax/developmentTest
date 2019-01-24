89d36f6d82d37d190034df5e704ae642f0fb5108
nidhinb <nidhinb@traxretail.com>
2019-01-24 10:52:44 +0800

    initial commit

---
+++ Projects/DIAGEOIN_SAND/Utils/KPIToolBox.py
@@ -0,0 +1,75 @@
+
+import pandas as pd
+from datetime import datetime
+from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
+from Trax.Cloud.Services.Connector.Keys import DbUsers
+from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
+from Trax.Utils.Logging.Logger import Log
+from KPIUtils.DIAGEO.ToolBox import DIAGEOToolBox
+from KPIUtils.GlobalProjects.DIAGEO.Utils.Fetcher import DIAGEOQueries
+from KPIUtils.GlobalProjects.DIAGEO.KPIGenerator import DIAGEOGenerator
+from KPIUtils.DB.Common import Common
+from KPIUtils_v2.DB.CommonV2 import Common as CommonV2
+
+__author__ = 'Nidhin'
+
+def log_runtime(description, log_start=False):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            calc_start_time = datetime.utcnow()
+            if log_start:
+                Log.info('{} started at {}'.format(description, calc_start_time))
+            result = func(*args, **kwargs)
+            calc_end_time = datetime.utcnow()
+            Log.info('{} took {}'.format(description, calc_end_time - calc_start_time))
+            return result
+        return wrapper
+    return decorator
+
+
+class DIAGEOIN_SANDToolBox:
+
+    def __init__(self, data_provider, output):
+        self.k_engine = BaseCalculationsScript(data_provider, output)
+        self.data_provider = data_provider
+        self.project_name = self.data_provider.project_name
+        self.session_uid = self.data_provider.session_uid
+        self.rds_conn = PSProjectConnector(self.project_name, DbUsers.CalculationEng)
+        self.match_display_in_scene = self.get_match_display()
+        self.output = output
+        self.common = Common(self.data_provider)
+        self.commonV2 = CommonV2(self.data_provider)
+        self.tools = DIAGEOToolBox(self.data_provider, output, match_display_in_scene=self.match_display_in_scene)
+        self.diageo_generator = DIAGEOGenerator(self.data_provider, self.output, self.common)
+
+    def get_match_display(self):
+        """
+        This function extracts the display matches data and saves it into one global data frame.
+        The data is taken from probedata.match_display_in_scene.
+        """
+        query = DIAGEOQueries.get_match_display(self.session_uid)
+        match_display = pd.read_sql_query(query, self.rds_conn.db)
+        return match_display
+
+    def main_calculation(self):
+        """
+        This function calculates the KPI results.
+        """
+        # Global assortment kpis
+        assortment_res_dict = self.diageo_generator.diageo_global_assortment_function_v2()
+        self.commonV2.save_json_to_new_tables(assortment_res_dict)
+
+        # # global SOS kpi
+        # res_dict = self.diageo_generator.diageo_global_share_of_shelf_function()
+        # self.commonV2.save_json_to_new_tables(res_dict)
+
+        # for set_name in []:
+        #     # if set_name not in self.tools.KPI_SETS_WITHOUT_A_TEMPLATE and set_name not in self.set_templates_data.keys():
+        #     #     try:
+        #     #         self.set_templates_data[set_name] = self.tools.download_template(set_name)
+        #     #     except:
+        #     #         Log.warning("Couldn't find a template for set name: " + str(set_name))
+        #     #         continue
+
+        # commiting to new tables
+        self.commonV2.commit_results_data()
