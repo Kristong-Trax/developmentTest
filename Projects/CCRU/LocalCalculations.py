964b2edafa97439a25ba96a9b484a50bf7391479
sergeypol <sergey@traxretail.com>
2019-01-30 15:05:58 +0300

    PROS-6581 - CCRU - new KPI tables and POS 2019

--- Projects/CCRU/LocalCalculations.py
+++ Projects/CCRU/LocalCalculations.py
@@ -11,8 +11,7 @@ if __name__ == '__main__':
     project_name = 'ccru'
     data_provider = KEngineDataProvider(project_name)
     session_uids = [
-        'e3f3c101-1d36-41c2-9ff1-f0961da3bec2',
-        'aaea8e23-08cf-48f1-81dd-00a6b950b8b7'
+        '834FD53D-8A51-4CE5-AA22-A9237DB783EC'
 
         # 'ebe286b9-4200-4f50-8261-21a30c31ea7d',
         # '6626e7a7-05ce-4463-89ee-d94533a5c2d4',
