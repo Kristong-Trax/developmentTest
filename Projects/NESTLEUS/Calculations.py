e13736eb3c8fb3b4ed80e6adabef7c3407c1aa2d
limorc <limorc@traxretail.com>
2019-02-04 16:21:47 +0200

    pass calculation for project

--- Projects/NESTLEUS/Calculations.py
+++ Projects/NESTLEUS/Calculations.py
@@ -8,9 +8,10 @@ __author__ = 'nicolaske'
 
 class Calculations(BaseCalculationsScript):
     def run_project_calculations(self):
-        self.timer.start()
-        Generator(self.data_provider, self.output).main_function()
-        self.timer.stop('KPIGenerator.run_project_calculations')
+        pass
+        # self.timer.start()
+        # Generator(self.data_provider, self.output).main_function()
+        # self.timer.stop('KPIGenerator.run_project_calculations')
 
 
 
