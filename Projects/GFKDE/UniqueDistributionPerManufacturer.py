dbdcbb288b5b353658742fa0496560f9756b18f7
elisuiza <elis@traxretail.com>
2019-02-10 11:22:20 +0200

    GFK

--- Projects/GFKDE/UniqueDistributionPerManufacturer.py
+++ Projects/GFKDE/UniqueDistributionPerManufacturer.py
@@ -20,5 +20,5 @@ class UniqueDistributionPerManufacturer_KPI(GfkDistributionBaseCalculationScript
                 "include_operator": "and"
             },
             "numerator": Fields.MANUFACTURER_FK,
-            "denominator": Keys.STORE_ID,
+            "denominator": Keys.CATEGORY_FK,
         }
