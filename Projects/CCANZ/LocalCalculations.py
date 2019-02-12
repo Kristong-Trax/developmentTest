09bb70413f7cc5ef57680dc0817f1ca5061ee5c9
sathiyanarayanand <sathiyanarayanan@traxretail.com>
2019-02-12 14:40:11 +0800

    PROS-7427 CCANZ Inclusion and Exclusion

--- Projects/CCANZ/LocalCalculations.py
+++ Projects/CCANZ/LocalCalculations.py
@@ -1,16 +1,18 @@
-
+#
 # from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
 # from Trax.Utils.Conf.Configuration import Config
 # from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 # from Projects.CCANZ.Calculations import Calculations
-
-
+#
+#
 # if __name__ == '__main__':
 #     LoggerInitializer.init('ccanz calculations')
 #     Config.init()
 #     project_name = 'ccanz'
 #     data_provider = KEngineDataProvider(project_name)
+#
 #     session = ''
+#
 #     data_provider.load_session_data(session)
 #     output = Output()
 #     Calculations(data_provider, output).run_project_calculations()
