54db34a19d738f92644df50ea6c8fb8c7b97bd9d
natalya_k <natalyak@traxretail.com>
2019-02-17 13:07:20 +0200

    adding stateless auto ass function

--- Projects/JNJUKTRIAL_SAND/LocalCalculations.py
+++ Projects/JNJUKTRIAL_SAND/LocalCalculations.py
@@ -10,12 +10,15 @@ if __name__ == '__main__':
     Config.init()
     project_name = 'jnjuktrial-sand'
     data_provider = KEngineDataProvider(project_name)
-    sessions = ['b741be7a-8cf4-4cb5-8413-097544396535',
-                '21bdd793-b6bd-4d51-8706-30d22f914513',
-                'fac30e59-0fe7-4133-8678-8801c5796df5',
-                '19a87af2-a24c-4f41-b4a0-93d62b354190',
-                '234569b1-2ac9-4fcb-9dbf-b0742aadb8b8',
-                '61f76f46-0290-47c8-bc29-ef7477010caa',
+    sessions = [
+        '1c4b132b-7637-4b57-9adb-d7aebe744460'
+        # 'faa35677-1473-47cd-a5fb-ca1c0e979d16'
+        # 'b741be7a-8cf4-4cb5-8413-097544396535',
+        #         '21bdd793-b6bd-4d51-8706-30d22f914513',
+        #         'fac30e59-0fe7-4133-8678-8801c5796df5',
+        #         '19a87af2-a24c-4f41-b4a0-93d62b354190',
+        #         '234569b1-2ac9-4fcb-9dbf-b0742aadb8b8',
+        #         '61f76f46-0290-47c8-bc29-ef7477010caa',
                 ]
     for session in sessions:
         data_provider.load_session_data(session)
