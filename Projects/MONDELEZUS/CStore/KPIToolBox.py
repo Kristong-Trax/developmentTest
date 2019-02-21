b15ba0b736a3234516ac2a33696debb111892dec
samkowalski <samk@traxretail.com>
2019-02-21 15:46:53 -0500

    displays running

--- Projects/MONDELEZUS/CStore/KPIToolBox.py
+++ Projects/MONDELEZUS/CStore/KPIToolBox.py
@@ -4,7 +4,7 @@ from Trax.Cloud.Services.Connector.Keys import DbUsers
 from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
 from KPIUtils_v2.Calculations.AssortmentCalculations import Assortment
 from Trax.Utils.Logging.Logger import Log
-from Projects.GPUS.Utils.Const import Const
+from Projects.MONDELEZUS.CStore.Const import Const
 
 __author__ = 'sam'
 
@@ -30,9 +30,10 @@ class CSTOREToolBox:
         self.store_id = self.data_provider[Data.STORE_FK]
         self.scif = self.data_provider[Data.SCENE_ITEM_FACTS]
         self.scif['store_fk'] = self.store_id
-        self.scif = self.scif[~(self.scif['product_type'] == Const.IRRELEVANT)]
+        self.scif = self.scif[~(self.scif['product_type'].isin([Const.IRRELEVANT, Const.EMPTY]))]
         self.rds_conn = PSProjectConnector(self.project_name, DbUsers.CalculationEng)
         self.kpis = self.load_kpis()
+        self.results_values = self.load_results_values()
         self.kpi_static_data = self.common.get_kpi_static_data()
         self.manufacturer_fk = int(self.data_provider[Data.OWN_MANUFACTURER].iloc[0, 1])
 
@@ -46,7 +47,7 @@ class CSTOREToolBox:
         This function calculates the KPI results.
         """
         # if not self.filter_df(self.scif, self.brand_filter).empty:
-        # self.calculate_facings_sos()
+        self.calculate_displays()
         self.calculate_assortment()
 
         for result in self.kpi_results:
@@ -64,54 +65,51 @@ class CSTOREToolBox:
             Log.error('ERROR {} KPIs Failed to Calculate'.format(group))
             Log.error(e)
 
-
-    def calculate_sos(self, kpi_family, kpi_filter):
-        relevant_kpis = self.kpis[self.kpis[Const.KPI_FAMILY] == kpi_family]
-        sum_col = Const.SUM_COLS[kpi_family]
+    def calculate_displays(self):
+        relevant_kpis = self.kpis[self.kpis[Const.KPI_FAMILY] == Const.DISPLAY]
         relevant_kpis['num_types'] = self.name_to_col_name(relevant_kpis[Const.NUMERATOR])
-        relevant_kpis['den_types'] = self.name_to_col_name(relevant_kpis[Const.DENOMINATOR])
-
+        df_base = self.scif[self.scif['template_name'].str.contains('Secondary Location')]
+        df_base = df_base[df_base['manufacturer_fk'] == self.manufacturer_fk]
+        df_base['numerator_result'], df_base['result'] = 1, 1
         for i, kpi in relevant_kpis.iterrows():
             parent = relevant_kpis[relevant_kpis['type'] == Const.SOS_HIERARCHY[kpi['type']]] #  Note, parent is df, kpi is a series
-            df = self.scif.copy()
-            df = self.transform_new_col(df, [kpi['num_types'], kpi['den_types']], sum_col, 'numerator_result')
-            df = self.transform_new_col(df, kpi['den_types'], sum_col, 'denominator_result')
-            df['result'] = df['numerator_result'].astype(float) / df['denominator_result']
+            df = df_base.copy()
             df = self.update_and_rename_df(df, kpi, parent)
-            self.update_results(df, kpi_filter)
+            df = self.transform_new_col(df, 'numerator_id', 'numerator_result')
+            df['result'] = df['numerator_result']
+            self.update_results(df)
             if parent.empty:
                 df['denominator_id'] = self.store_id
                 df.drop('ident_result', axis=1, inplace=True)
                 df.rename(columns={'ident_parent': 'ident_result'}, inplace=True)
                 df['kpi_name'] = Const.SOS_HIERARCHY[kpi['type']]
-                self.update_results(df, {'numerator_id': self.manufacturer_fk})
+                df['numerator_id'] = self.manufacturer_fk
+                self.update_results(df)
 
-    def update_results(self, df, filter):
-        df = self.filter_df(df, filter)
+    def update_results(self, df):
         results = [val for key, val in df.to_dict('index').items()]
         self.kpi_results += results
 
     def update_and_rename_df(self, df, kpi, parent):
-        df['ident_result'] = ['{}_{}_{}'.format(row[kpi['num_types']], row[kpi['den_types']], kpi['type'])
+        df['ident_result'] = ['{}_{}'.format(row[kpi['num_types']], kpi['type'])
                               for i, row in df.iterrows()]
-        df['ident_parent'] = ['{}_{}_{}_{}'.format(row[kpi['num_types']], row[kpi['den_types']], 'Parent', kpi['type'])
+        df['ident_parent'] = ['{}_{}_{}'.format(row[kpi['num_types']], 'Parent', kpi['type'])
                               for i, row in df.iterrows()]
         parent_col = ['ident_parent']
         if not parent.empty:
-            df['ident_parent'] = ['{}_{}_{}'.format(row[parent['num_types'].iloc[0]], row[parent['den_types'].iloc[0]],
-                                                    parent['type'].iloc[0]) for i, row in df.iterrows()]  #parent is a df, hence the iloc
-        df = df[['numerator_result', 'denominator_result', 'result', kpi['den_types'], kpi['num_types'],
-                 'ident_result'] + parent_col]
+            df['ident_parent'] = ['{}_{}'.format(row[parent['num_types'].iloc[0]], parent['type'].iloc[0])
+                                  for i, row in df.iterrows()]  #parent is a df, hence the iloc
+        df = df[['numerator_result', 'result', kpi['num_types'], 'ident_result'] + parent_col]
         df.drop_duplicates(inplace=True)
-        df.rename(columns={kpi['num_types']: 'numerator_id', kpi['den_types']: 'denominator_id'}, inplace=True)
+        df.rename(columns={kpi['num_types']: 'numerator_id'}, inplace=True)
         df['kpi_name'] = kpi['type']
         return df
 
     def name_to_col_name(self, col):
         return ['{}_fk'.format(num) for num in col]
 
-    def transform_new_col(self, df, group_col, sum_col, name):
-        df[name] = df.groupby(group_col)[sum_col].transform('sum')
+    def transform_new_col(self, df, group_col, sum_col):
+        df[sum_col] = df.groupby(group_col)[sum_col].transform('sum')
         return df
 
     def grouper(self, filter, df):
@@ -130,8 +128,6 @@ class CSTOREToolBox:
             lvl3_result = lvl3_result.set_index('product_fk').join(self.scif.set_index('product_fk')['category_fk']).reset_index()
             lvl3_result = lvl3_result.rename(columns={'assortment_group_fk': 'ass_grp_fk',
                                                       'category_fk': 'assortment_group_fk'})
-
-
             lvl2_result = self.assortment.calculate_lvl2_assortment(lvl3_result)
             lvl1_result = self.assortment.calculate_lvl1_assortment(lvl2_result)
             lvl1_result['total'] = lvl2_result['total'].sum()
@@ -139,7 +135,13 @@ class CSTOREToolBox:
             lvl1_result['num_id'] = self.manufacturer_fk
             lvl1_result['den_id'] = self.store_id
 
+            lvlx_result = self.assortment_additional(lvl3_result)
+            lvl3_result['in_store'] = lvl3_result['in_store'].apply(lambda x:
+                                                                    self.results_values[Const.RESULTS_TYPE_DICT[x]])
+
             self.parse_assortment_results(lvl3_result, 'kpi_fk_lvl3', 'product_fk', 'in_store', 'assortment_group_fk',
+                                          'target', None, 'assortment_group_fk')
+            self.parse_assortment_results(lvlx_result, 'kpi_fk_lvl3', 'product_fk', 'in_store', 'assortment_group_fk',
                                           'target', None, 'assortment_group_fk')
             self.parse_assortment_results(lvl2_result, 'kpi_fk_lvl2', 'assortment_group_fk', 'passes', 'assortment_fk',
                                           'total', 'assortment_group_fk', 'assortment_super_group_fk')
@@ -154,14 +156,28 @@ class CSTOREToolBox:
                        'numerator_result': row[num_col],
                        'denominator_id': row[den_id_col],
                        'denominator_result': row[den_col],
-                       'score': 1,
+                       'score': self.safe_divide(row[num_col], row[den_col]),
                        'result': self.safe_divide(row[num_col], row[den_col]),
                        'ident_result': row[self_id] if self_id else None,
                        'ident_parent': row[parent] if parent else None}
             self.kpi_results.append(kpi_res)
 
+    def assortment_additional(self, lvl3_result):
+        assort = set(lvl3_result['product_fk'])
+        remaining = self.scif[~self.scif['product_fk'].isin(assort)]
+        remaining = remaining[remaining['manufacturer_fk'] == self.manufacturer_fk]
+        remaining = remaining[remaining['facings'] != 0][['product_fk', 'category_fk']]
+        remaining['kpi_fk_lvl3'] = lvl3_result['kpi_fk_lvl3'].values[0]
+        remaining['in_store'] = self.results_values['Additional']
+        remaining['target'] = 0
+        remaining = remaining.rename(columns={'category_fk': 'assortment_group_fk'})
+        return remaining
+
     def safe_divide(self, num, den):
-        return (float(num) / den) * 100 if num else 0
+        res = num
+        if num <= den:
+            res = (float(num) / den) * 100 if num and den else 0
+        return res
 
     @staticmethod
     def filter_df(df, filters, exclude=0):
@@ -179,6 +195,10 @@ class CSTOREToolBox:
 
     def load_kpis(self):
         return pd.read_sql_query(Const.KPI_QUERY, self.rds_conn.db)
+
+    def load_results_values(self):
+        return pd.read_sql_query(Const.RESULT_TYPE_QUERY, self.rds_conn.db).set_index('value')['pk'].to_dict()
+
 
     def write_to_db(self, kpi_name=None, score=0, result=None, target=None, numerator_result=None, scene_result_fk=None,
                     denominator_result=None, numerator_id=999, denominator_id=999, ident_result=None, ident_parent=None,
