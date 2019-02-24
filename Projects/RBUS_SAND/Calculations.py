43364e8fda8f15922d76b61a59b342828dc569c1
yasmingr <yaming@traxretail.com>
2019-02-24 18:53:07 +0200

    migrate to new tables

--- Projects/RBUS_SAND/Calculations.py
+++ Projects/RBUS_SAND/Calculations.py
@@ -1,9 +1,5 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-# from Trax.Utils.Conf.Configuration import Config
-
 from Projects.RBUS_SAND.KPIGenerator import RBUSGenerator
 
 __author__ = 'yoava'
@@ -16,13 +12,15 @@ class RBUSCalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from Trax.Utils.Conf.Configuration import Config
 # if __name__ == '__main__':
 #     LoggerInitializer.init('rbus-sand calculations')
 #     Config.init()
 #     project_name = 'rbus-sand'
 #     data_provider = KEngineDataProvider(project_name)
-#     # session = '537C4629-9347-473D-8162-8BBB47E9482D'
-#     session = 'b6ce555b-763d-404a-b1d7-b514f3bb9bae'
+#     session = 'b596a3fb-541b-11e8-b95f-0a0d2137d3d2'
 #     data_provider.load_session_data(session)
 #     output = Output()
 #     RBUSCalculations(data_provider, output).run_project_calculations()
