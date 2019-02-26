1b8dd0f2ab3261211207bb05ea3f8c7a589a3668
limorc <limorc@traxretail.com>
2019-02-26 15:31:28 +0200

    Diageo migration

--- Projects/DIAGEOGTR_SAND/Calculations.py
+++ Projects/DIAGEOGTR_SAND/Calculations.py
@@ -1,8 +1,8 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-# from Trax.Utils.Conf.Configuration import Config
-# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 from Projects.DIAGEOGTR_SAND.KPIGenerator import DIAGEOGTRGenerator
 
@@ -16,12 +16,12 @@ class DIAGEOGTRCalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-# if __name__ == '__main__':
-#     LoggerInitializer.init('diageogtr-sand calculations')
-#     Config.init()
-#     project_name = 'diageogtr-sand'
-#     data_provider = KEngineDataProvider(project_name)
-#     session = ''
-#     data_provider.load_session_data(session)
-#     output = Output()
-#     DIAGEOGTRCalculations(data_provider, output).run_project_calculations()
+if __name__ == '__main__':
+    LoggerInitializer.init('diageogtr-sand calculations')
+    Config.init()
+    project_name = 'diageogtr-sand'
+    data_provider = KEngineDataProvider(project_name)
+    session = '932CA562-BAA5-4682-81EC-B3667D8B0979'
+    data_provider.load_session_data(session)
+    output = Output()
+    DIAGEOGTRCalculations(data_provider, output).run_project_calculations()
