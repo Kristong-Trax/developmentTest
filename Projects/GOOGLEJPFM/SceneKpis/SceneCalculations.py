7fc833b3dcb3d52aa9cc7e361e2cdec803b7f15a
elyashivm <elyashivm@traxretail.com>
2019-02-27 16:01:23 +0200

    googlejpfm creation

---
+++ Projects/GOOGLEJPFM/SceneKpis/SceneCalculations.py
@@ -0,0 +1,13 @@
+from KPIUtils.GlobalProjects.GOOGLE.SceneKPIGenerator import SceneGenerator
+from Trax.Apps.Services.KEngine.Handlers.Utils.Scripts import SceneBaseClass
+
+__author__ = 'Eli_Shivi_Sam'
+
+
+class SceneCalculations(SceneBaseClass):
+    def __init__(self, data_provider):
+        super(SceneCalculations, self).__init__(data_provider)
+        self.scene_generator = SceneGenerator(self._data_provider)
+
+    def calculate_kpis(self):
+        self.scene_generator.main_function()
