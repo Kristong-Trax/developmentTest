6be93f38f41c62541de3387a987407ea1dcc4ce4
nicolaske <nicolaske@traxretail.com>
2019-02-28 17:35:31 -0500

    template change in kpi toolboc

--- Projects/CCBR_PROD/Utils/KPIToolBox.py
+++ Projects/CCBR_PROD/Utils/KPIToolBox.py
@@ -24,7 +24,7 @@ KPI_RESULT = 'report.kpi_results'
 KPK_RESULT = 'report.kpk_results'
 KPS_RESULT = 'report.kps_results'
 KPI_NEW_TABLE = 'report.kpi_level_2_results'
-PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'Data', 'Femsa template 2019 - KENGINE_DCH v6.4_Feb.xlsx')
+PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'Data', 'Femsa template 2019 - KENGINE_DCH v6.5_Feb.xlsx')
 
 def log_runtime(description, log_start=False):
     def decorator(func):
@@ -158,7 +158,7 @@ class CCBRToolBox:
             if store_type_filter not in store_types:
                 return
 
-        # find the answer to the survey in session
+        # find the answer to the  in session
         question_id = row[Const.SURVEY_QUESTION_ID].values[0]
         question_answer_template = row[Const.TARGET_ANSWER].values[0]
 
