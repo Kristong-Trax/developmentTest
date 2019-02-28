3b2fccdc02e047c6665e91119fd06dfbe6a9f1d8
elisuiza <elis@traxretail.com>
2019-02-28 11:22:22 +0200

    Comment main function

--- Projects/GMIUS/Calculations.py
+++ Projects/GMIUS/Calculations.py
@@ -7,5 +7,5 @@ __author__ = 'Sam'
 class Calculations(BaseCalculationsScript):
     def run_project_calculations(self):
         self.timer.start()
-        Generator(self.data_provider, self.output).main_function()
+        # Generator(self.data_provider, self.output).main_function()
         self.timer.stop('KPIGMIUSGenerator.run_project_calculations')
