a035435e50b10cad4fae0c7015927d4e6899e3c6
elyashivm <elyashivm@traxretail.com>
2019-03-03 13:36:25 +0200

    googlejpfm creation

--- Projects/GOOGLEJPFM/LocalCalculations.py
+++ Projects/GOOGLEJPFM/LocalCalculations.py
@@ -23,23 +23,12 @@ if __name__ == '__main__':
     LoggerInitializer.init('googlejpfm calculations')
     Config.init()
     project_name = 'googlejpfm'
-    sessions = [
-        '68ac2242-d6d5-4a08-ba5c-ba5418c69852',
-        'dd7452bb-134c-4b5a-a695-be6355695b48',
-        '33203324-796f-42f5-af71-9acb0e48b2f4',
-        'ba74b587-901d-437d-ab34-2ffc33c49aaa',
-        '9b33b4d0-cb67-4e9b-82d9-4d4c3c505491'
-    ]
-    sessions = ['6da55dc7-440d-4e86-966d-e5fa803e78c8']
-
+    sessions = ['fca88900-e56f-4b79-be99-3e38a3308a52']
     for session in sessions:
         data_provider = KEngineDataProvider(project_name)
         data_provider.load_session_data(session)
         scif = data_provider['scene_item_facts']
         scenes = scif['scene_id'].unique().tolist()
-
-        # scenes = [392]
-
         for scene in scenes:
             print('scene')
             data_provider = KEngineDataProvider(project_name)
