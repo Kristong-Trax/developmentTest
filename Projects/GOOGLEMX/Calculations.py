3c5d19c3c6422f6d2562c5c7112e90c321c3f79f
samkowalski <samk@traxretail.com>
2019-03-08 15:12:11 -0500

    copied googlekr updated names & paths

---
+++ Projects/GOOGLEMX/Calculations.py
@@ -0,0 +1,15 @@
+from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
+from KPIUtils.GlobalProjects.GOOGLE.KPIGenerator import Generator
+import os
+
+__author__ = 'Eli_Shivi_Sam'
+
+FIXTURE_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Data',
+                                     'KR - Google Fixture Targets v.6 Feb Visits.xlsx')
+
+
+class Calculations(BaseCalculationsScript):
+    def run_project_calculations(self):
+        self.timer.start()
+        Generator(self.data_provider, self.output, FIXTURE_TEMPLATE_PATH).main_function()
+        self.timer.stop('KPIGOOGLEMXGenerator.run_project_calculations')
