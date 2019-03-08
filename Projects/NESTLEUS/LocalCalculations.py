952060ab822400c4acdb62ca4df27bf5a45d72cc
samkowalski <samk@traxretail.com>
2019-03-08 15:42:43 -0500

    oy vey

--- Projects/NESTLEUS/LocalCalculations.py
+++ Projects/NESTLEUS/LocalCalculations.py
@@ -10,7 +10,7 @@
 #     Config.init()
 #     project_name = 'nestleus'
 #     data_provider = KEngineDataProvider(project_name)
-#     session = 'a27d1d20-a9ac-4bdf-9b68-ee5e4195224a'
+#     session = '9bffe5d5-ee61-4246-b545-560c2acfbb1b'
 #     data_provider.load_session_data(session)
 #     output = Output()
 #     Calculations(data_provider, output).run_project_calculations()
