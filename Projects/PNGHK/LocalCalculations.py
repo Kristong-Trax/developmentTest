82ce77e79897539727f5d2cf537b1878697a0693
ilay_s <ilays@traxretail.com>
2019-03-10 17:24:10 +0200

    update pnghk template

--- Projects/PNGHK/LocalCalculations.py
+++ Projects/PNGHK/LocalCalculations.py
@@ -4,6 +4,8 @@ from Trax.Utils.Conf.Configuration import Config
 from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 from Projects.PNGHK.Calculations import Calculations
 
+__author__ = 'ilays'
+
 
 if __name__ == '__main__':
     LoggerInitializer.init('pnghk calculations')
@@ -12,10 +14,10 @@ if __name__ == '__main__':
     data_provider = KEngineDataProvider(project_name)
     sessions = [
         '6dc13c96-5943-42f8-ad26-48f9e5617f0f',
-        'ed5bc391-ce81-4d7e-b16d-1de5a573cbe0',  # has smart probe/69066 has hanger probe/69044 has stock
-        'ad052993-9609-4aba-a927-dbaaff036280',  # has smart
-        '6c56a073-c8db-42aa-a491-c38b0c1c4086',  # has smart
-        'dca92352-a549-4956-88b0-0811b35137c3',
+        # 'ed5bc391-ce81-4d7e-b16d-1de5a573cbe0',  # has smart probe/69066 has hanger probe/69044 has stock
+        # 'ad052993-9609-4aba-a927-dbaaff036280',  # has smart
+        # '6c56a073-c8db-42aa-a491-c38b0c1c4086',  # has smart
+        # 'dca92352-a549-4956-88b0-0811b35137c3',
     ]
     for session in sessions:
         print "Running for {}".format(session)
