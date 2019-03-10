7f2a59917e0264bc305bd3f766556888a297e8e9
yasmingr <yaming@traxretail.com>
2019-03-10 15:08:18 +0200

    revereting prod migration

---
+++ Projects/DIAGEOIT_SAND/Utils/KPIToolBox.py
@@ -0,0 +1,116 @@
+
+import pandas as pd
+from datetime import datetime
+
+from Trax.Algo.Calculations.Core.DataProvider import Data
+from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
+from Trax.Cloud.Services.Connector.Keys import DbUsers
+from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
+from Trax.Utils.Logging.Logger import Log
+from Trax.Data.Utils.MySQLservices import get_table_insertion_query as insert
+
+from KPIUtils.DIAGEO.ToolBox import DIAGEOToolBox
+from KPIUtils.GlobalProjects.DIAGEO.Utils.Fetcher import DIAGEOQueries
+from KPIUtils.GlobalProjects.DIAGEO.KPIGenerator import DIAGEOGenerator
+from KPIUtils.DB.Common import Common
+from KPIUtils_v2.DB.CommonV2 import Common as CommonV2
+
+__author__ = 'Nimrod'
+
+KPI_RESULT = 'report.kpi_results'
+KPK_RESULT = 'report.kpk_results'
+KPS_RESULT = 'report.kps_results'
+
+
+def log_runtime(description, log_start=False):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            calc_start_time = datetime.utcnow()
+            if log_start:
+                Log.info('{} started at {}'.format(description, calc_start_time))
+            result = func(*args, **kwargs)
+            calc_end_time = datetime.utcnow()
+            Log.info('{} took {}'.format(description, calc_end_time - calc_start_time))
+            return result
+        return wrapper
+    return decorator
+
+
+class DIAGEOITToolBox:
+
+    LEVEL1 = 1
+    LEVEL2 = 2
+    LEVEL3 = 3
+
+    def __init__(self, data_provider, output):
+        self.k_engine = BaseCalculationsScript(data_provider, output)
+        self.data_provider = data_provider
+        self.project_name = self.data_provider.project_name
+        self.session_uid = self.data_provider.session_uid
+        self.products = self.data_provider[Data.PRODUCTS]
+        self.all_products = self.data_provider[Data.ALL_PRODUCTS]
+        self.match_product_in_scene = self.data_provider[Data.MATCHES]
+        self.visit_date = self.data_provider[Data.VISIT_DATE]
+        self.session_info = self.data_provider[Data.SESSION_INFO]
+        self.rds_conn = PSProjectConnector(self.project_name, DbUsers.CalculationEng)
+        self.store_info = self.data_provider[Data.STORE_INFO]
+        self.store_id = self.data_provider[Data.STORE_FK]
+        self.store_channel = self.store_info['store_type'].values[0]
+        if self.store_channel:
+            self.store_channel = self.store_channel.upper()
+        self.store_type = self.store_info['additional_attribute_1'].values[0]
+        self.segment = self.get_business_unit_name()
+        self.scene_info = self.data_provider[Data.SCENES_INFO]
+        self.scif = self.data_provider[Data.SCENE_ITEM_FACTS]
+        self.match_display_in_scene = self.get_match_display()
+        self.set_templates_data = {}
+        self.kpi_static_data = self.get_kpi_static_data()
+        self.kpi_results_queries = []
+        self.output = output
+        self.common = Common(self.data_provider)
+        self.commonV2 = CommonV2(self.data_provider)
+        self.tools = DIAGEOToolBox(self.data_provider, output, match_display_in_scene=self.match_display_in_scene)
+        self.kpi_results_queries = []
+        self.diageo_generator = DIAGEOGenerator(self.data_provider, self.output, self.common)
+
+
+    def get_business_unit_name(self):
+        """
+        This function extracts the static KPI data and saves it into one global data frame.
+        The data is taken from static.kpi / static.atomic_kpi / static.kpi_set.
+        """
+        query = DIAGEOQueries.get_business_unit_name(self.store_id)
+        business_unit_name = pd.read_sql_query(query, self.rds_conn.db)
+        if business_unit_name['business_unit_name'].empty:
+            return ""
+        else:
+            return business_unit_name['business_unit_name'].values[0]
+
+    def get_kpi_static_data(self):
+        """
+        This function extracts the static KPI data and saves it into one global data frame.
+        The data is taken from static.kpi / static.atomic_kpi / static.kpi_set.
+        """
+        query = DIAGEOQueries.get_all_kpi_data()
+        kpi_static_data = pd.read_sql_query(query, self.rds_conn.db)
+        return kpi_static_data
+
+    def get_match_display(self):
+        """
+        This function extracts the display matches data and saves it into one global data frame.
+        The data is taken from probedata.match_display_in_scene.
+        """
+        query = DIAGEOQueries.get_match_display(self.session_uid)
+        match_display = pd.read_sql_query(query, self.rds_conn.db)
+        return match_display
+
+    def main_calculation(self, set_names):
+        """
+        This function calculates the KPI results.
+        """
+        # Global assortment kpis
+        assortment_res_dict = self.diageo_generator.diageo_global_assortment_function_v2()
+        self.commonV2.save_json_to_new_tables(assortment_res_dict)
+
+        # commiting to new tables
+        self.commonV2.commit_results_data()
