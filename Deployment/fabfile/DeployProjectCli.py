c24e081ed2028d7b8089d116d6a747be7457bef7
ilanpinto <ilanp@traxretail.com>
2019-03-11 10:18:39 +0200

    jenkins deploy

--- Deployment/fabfile/DeployProjectCli.py
+++ Deployment/fabfile/DeployProjectCli.py
@@ -10,6 +10,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='deploys projects ')
     parser.add_argument('--project', type=str, required=True, help='Project name to be deployed')
     parser.add_argument('-e', '--env', type=str, help='The environment - dev/int/prod')
+    parser.add_argument('-am', default='local' , type=str, help='The environment - dev/int/prod')
     return parser.parse_args()
 
 
