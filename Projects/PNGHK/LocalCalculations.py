860fb86426ec8e9be951e4beed7294e969360d03
ilay_s <ilays@traxretail.com>
2019-03-11 11:02:50 +0200

    fix bug in osd filtering

--- Projects/PNGHK/LocalCalculations.py
+++ Projects/PNGHK/LocalCalculations.py
@@ -13,7 +13,9 @@ if __name__ == '__main__':
     project_name = 'pnghk'
     data_provider = KEngineDataProvider(project_name)
     sessions = [
-        '6dc13c96-5943-42f8-ad26-48f9e5617f0f',
+        '41a4abd4-0ee7-490a-868b-9043f162d252',
+        '8e687c04-ac58-47a4-8000-bd8afd9382d0',
+        # '6dc13c96-5943-42f8-ad26-48f9e5617f0f',
         # 'ed5bc391-ce81-4d7e-b16d-1de5a573cbe0',  # has smart probe/69066 has hanger probe/69044 has stock
         # 'ad052993-9609-4aba-a927-dbaaff036280',  # has smart
         # '6c56a073-c8db-42aa-a491-c38b0c1c4086',  # has smart
