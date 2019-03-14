c2589a8164dd75448d97d9f8aae301e5527bcc43
sergeypol <sergey@traxretail.com>
2019-03-14 09:19:23 +0200

    PROS-8045 - BATRU - Additional filers and POSM in API

--- Projects/BATRU/Calculations.py
+++ Projects/BATRU/Calculations.py
@@ -1,12 +1,7 @@
-
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-
-# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-# from Trax.Utils.Conf.Configuration import Config
-# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-
-from Projects.BATRU.KPIGenerator import BATRUGenerator
 from KPIUtils_v2.Utils.Decorators.Decorators import log_runtime
+from Projects.BATRU.KPIGenerator import BATRUGenerator
+
 
 __author__ = 'uri'
 
@@ -19,15 +14,3 @@ class BATRUCalculations(BaseCalculationsScript):
         BATRUGenerator(self.data_provider, self.output).main_function()
         self.timer.stop('KPIGenerator.run_project_calculations')
 
-
-# if __name__ == '__main__':
-#     LoggerInitializer.init('batru calculations')
-#     Config.init()
-#     project_name = 'batru'
-#     sessions = [
-#          '4d345fea-5725-43d0-b1d9-4a97ea940a50']
-#     for session in sessions:
-#         data_provider = KEngineDataProvider(project_name)
-#         data_provider.load_session_data(session)
-#         output = Output()
-#         BATRUCalculations(data_provider, output).run_project_calculations()
