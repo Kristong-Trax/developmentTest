6efa40f54055f669d02057cb1ad9ea67f90f8e7f
natalya_k <natalyak@traxretail.com>
2019-03-14 09:48:26 +0200

    add exclusion parameters

---
+++ Projects/PEPSICOUK/Utils/Fetcher.py
@@ -0,0 +1,74 @@
+
+__author__ = 'natalyak'
+
+class PEPSICOUK_Queries(object):
+    @staticmethod
+    def get_all_kpi_data():
+        return """
+                select api.name as atomic_kpi_name, api.pk as atomic_kpi_fk,
+                       kpi.display_text as kpi_name, kpi.pk as kpi_fk,
+                       kps.name as kpi_set_name, kps.pk as kpi_set_fk
+                from static.atomic_kpi api
+                left join static.kpi kpi on kpi.pk = api.kpi_fk
+                join static.kpi_set kps on kps.pk = kpi.kpi_set_fk
+            """
+
+    @staticmethod
+    def get_store_data_by_store_id(store_fk):
+        return """
+                select s.store_type, s.additional_attribute_1, s.additional_attribute_2
+                from static.stores s
+                where s.pk = {}
+            """.format(store_fk)
+
+    @staticmethod
+    def get_new_kpi_data():
+        return """
+                select *
+                from static.kpi_level_2
+            """
+
+    @staticmethod
+    def get_kpi_result_values():
+        return """
+                SELECT *
+                FROM static.kpi_result_value
+                """
+
+    @staticmethod
+    def get_kpi_score_values():
+        return """
+                    SELECT *
+                    FROM static.kpi_score_value
+                    """
+    @staticmethod
+    def get_manufacturer_pk_by_name(manufacturer_name):
+        return """
+                    SELECT pk
+                    FROM static_new.manufacturer 
+                    WHERE name = '{}'
+                    """.format(manufacturer_name)
+
+    @staticmethod
+    def on_display_products_query(probe_match_list):
+        if len(probe_match_list) == 1:
+            query = """
+                    SELECT statevalue.match_product_in_probe_fk as probe_match_fk, 
+                    sstate.name as smart_attribute
+                    FROM probedata.match_product_in_probe_state_value statevalue
+                    join static.match_product_in_probe_state sstate 
+                    on statevalue.match_product_in_probe_state_fk=sstate.pk
+                    where sstate.name = 'additional display'
+                    and statevalue.match_product_in_probe_fk in ({});
+                    """.format(probe_match_list[0])
+        else:
+            query = """
+                    SELECT statevalue.match_product_in_probe_fk as probe_match_fk, 
+                    sstate.name as smart_attribute
+                    FROM probedata.match_product_in_probe_state_value statevalue
+                    join static.match_product_in_probe_state sstate 
+                    on statevalue.match_product_in_probe_state_fk=sstate.pk
+                    where sstate.name = 'additional display'
+                    and statevalue.match_product_in_probe_fk in {};
+                    """.format(tuple(probe_match_list))
+        return query
--- Projects/PEPSICOUK/Utils/KPIToolBox.py
+++ Projects/PEPSICOUK/Utils/KPIToolBox.py
@@ -5,10 +5,12 @@ from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
 # from Trax.Utils.Logging.Logger import Log
 import pandas as pd
 import os
+import numpy as np
 
 from KPIUtils_v2.DB.Common import Common as CommonV1
 from KPIUtils_v2.DB.CommonV2 import Common
 from KPIUtils_v2.Calculations.AssortmentCalculations import Assortment
+from Projects.PEPSICOUK.Utils.Fetcher import PEPSICOUK_Queries
 # from KPIUtils_v2.Calculations.AvailabilityCalculations import Availability
 # from KPIUtils_v2.Calculations.NumberOfScenesCalculations import NumberOfScenes
 # from KPIUtils_v2.Calculations.PositionGraphsCalculations import PositionGraphs
@@ -16,7 +18,7 @@ from KPIUtils_v2.Calculations.AssortmentCalculations import Assortment
 # from KPIUtils_v2.Calculations.SequenceCalculations import Sequence
 # from KPIUtils_v2.Calculations.SurveyCalculations import Survey
 
-# from KPIUtils_v2.Calculations.CalculationsUtils import GENERALToolBoxCalculations
+from KPIUtils_v2.Calculations.CalculationsUtils.GENERALToolBoxCalculations import GENERALToolBox
 
 __author__ = 'natalyak'
 
@@ -32,6 +34,7 @@ class PEPSICOUKToolBox:
 
     EXCLUSION_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'Data',
                                       'Incl_Excl_Template.xlsx')
+    ADDITIONAL_DISPLAY = 'additional display'
 
     def __init__(self, data_provider, output):
         self.output = output
@@ -51,16 +54,116 @@ class PEPSICOUKToolBox:
         self.rds_conn = PSProjectConnector(self.project_name, DbUsers.CalculationEng)
         self.kpi_static_data = self.common.get_kpi_static_data()
         self.kpi_results_queries = []
-        self.incl_excl_template = self.get_exclusion_template_data()
+
+        self.toolbox = GENERALToolBox(data_provider)
+        self.on_display_products = self.get_on_display_products()
+        self.exclusion_template = self.get_exclusion_template_data()
+        self.set_scif_and_matches_for_all_kpis(self.scif, self.match_product_in_scene)
 
         self.assortment = Assortment(self.data_provider, self.output, common=self.common_v1)
         self.lvl3_ass_result = self.assortment.calculate_lvl3_assortment()
 
 #------------------init functions-----------------
+    def get_on_display_products(self):
+        probe_match_list = self.match_product_in_scene['probe_match_fk'].values.tolist()
+        query = PEPSICOUK_Queries.on_display_products_query(probe_match_list)
+        on_display_products = pd.read_sql_query(query, self.rds_conn.db)
+        return on_display_products
+
     def get_exclusion_template_data(self):
-        return pd.read_excel(self.EXCLUSION_TEMPLATE_PATH)
+        excl_templ = pd.read_excel(self.EXCLUSION_TEMPLATE_PATH)
+        excl_templ = excl_templ.fillna('')
+        return excl_templ
+
+    def set_scif_and_matches_for_all_kpis(self, scif, matches):
+        excl_template_all_kpis = self.exclusion_template[self.exclusion_template['KPI'].str.upper() == 'ALL']
+        if not excl_template_all_kpis.empty:
+            template_filters = self.get_filters_dictionary(excl_template_all_kpis)
+            scif, matches = self.filter_scif_and_matches_for_scene_and_product_filters(template_filters, scif, matches)
+            scif, matches = self.update_scif_and_matches_for_smart_attributes(scif, matches)
+            self.data_provider._set_scene_item_facts(scif)
+            self.data_provider._set_matches(matches)
 
 #------------------utility functions--------------
+    def update_scif_and_matches_for_smart_attributes(self, scif, matches):
+        matches = self.filter_matches_for_products_with_smart_attributes(matches)
+        aggregated_matches = self.aggregate_matches(matches)
+        # remove relevant products from scif
+        scif = self.update_scif_for_products_with_smart_attributes(scif, aggregated_matches)
+        return scif, matches
+
+    def aggregate_matches(self, matches):
+        matches = matches[~(matches['bay_number'] == -1)]
+        # ask about oos equivalent in matches - should not exist for the project
+        matches['facings_matches'] = 1
+        aggregated_df = matches.groupby(['scene_fk', 'product_fk'], as_index=False).agg({'width_mm_advance': np.sum,
+                                                                                         'facings_matches': np.sum})
+        return aggregated_df
+
+    def filter_matches_for_products_with_smart_attributes(self, matches):
+        matches = matches.merge(self.on_display_products, on='probe_match_fk', how='left')
+        matches = matches[~(matches['smart_attribute'] == self.ADDITIONAL_DISPLAY)]
+        return matches
+
+    @staticmethod
+    def update_scif_for_products_with_smart_attributes(scif, agg_matches):
+        scif = scif.merge(agg_matches, on=['scene_fk', 'product_fk'], how='left')
+        scif = scif[~scif['facings_matches'].isnull()]
+        scif.rename(columns={'width_mm_advance': 'updated_gross_length', 'facings_matches': 'updated_facings'},
+                    inplace=True)
+        return scif
+
+    def get_filters_dictionary(self, excl_template_all_kpis):
+        filters = {}
+        for i, row in excl_template_all_kpis.iterrows():
+            if row['Action'].upper() == 'INCLUDE':
+                filters.update({row['Type']: self.split_and_strip(row['Value'])})
+            if row['Action'].upper() == 'EXCLUDE':
+                filters.update({row['Type']: (self.split_and_strip(row['Value']), 0)})
+        return filters
+
+    @staticmethod
+    def split_and_strip(value):
+        return map(lambda x: x.strip(), str(value).split(','))
+
+    def get_filters_for_scif_and_matches(self, template_filters):
+        product_keys = filter(lambda x: x in self.data_provider[Data.ALL_PRODUCTS].columns.values.tolist(),
+                              template_filters.keys())
+        scene_keys = filter(lambda x: x in self.data_provider[Data.ALL_TEMPLATES].columns.values.tolist(),
+                            template_filters.keys())
+        product_filters = {}
+        scene_filters = {}
+        for key in product_keys:
+            product_filters.update({key: template_filters[key]})
+        for key in scene_keys:
+            scene_filters.update({key: template_filters[key]})
+
+        filters_all = {}
+        if product_filters:
+            product_fks = self.get_product_fk_from_filters(product_filters)
+            filters_all.update({'product_fk': product_fks})
+        if scene_filters:
+            scene_fks = self.get_scene_fk_from_filters(scene_filters)
+            filters_all.update({'scene_fk': scene_fks})
+        return filters_all
+
+    def get_product_fk_from_filters(self, filters):
+        all_products = self.data_provider.all_products
+        product_fk_list = all_products[self.toolbox.get_filter_condition(all_products, **filters)]
+        product_fk_list = product_fk_list['product_fk'].unique().tolist()
+        return product_fk_list
+
+    def get_scene_fk_from_filters(self, filters):
+        scif_data = self.data_provider[Data.SCENE_ITEM_FACTS]
+        scene_fk_list = scif_data[self.toolbox.get_filter_condition(scif_data, **filters)]
+        scene_fk_list = scene_fk_list['scene_fk'].unique().tolist()
+        return scene_fk_list
+
+    def filter_scif_and_matches_for_scene_and_product_filters(self, template_filters, scif, matches):
+        filters = self.get_filters_for_scif_and_matches(template_filters)
+        scif = scif[self.toolbox.get_filter_condition(scif, **filters)]
+        matches = matches[self.toolbox.get_filter_condition(matches, **filters)]
+        return scif, matches
 
 #------------------main project calculations------
 
