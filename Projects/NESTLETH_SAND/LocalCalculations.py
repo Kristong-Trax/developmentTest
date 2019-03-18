3dd3cd80ca5d1d0a5aabebf4e9a9cb43c49909a4
limorc <limorc@traxretail.com>
2019-03-18 17:30:15 +0200

    calculation in scene level

--- Projects/NESTLETH_SAND/LocalCalculations.py
+++ Projects/NESTLETH_SAND/LocalCalculations.py
@@ -3,7 +3,7 @@ from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
 from Trax.Utils.Conf.Configuration import Config
 from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 from Projects.NESTLETH_SAND.Calculations import Calculations
-
+from Projects.NESTLETH_SAND.SceneKpis.SceneCalculations import SceneCalculations
 
 if __name__ == '__main__':
     LoggerInitializer.init('nestleth-sand calculations')
@@ -13,4 +13,9 @@ if __name__ == '__main__':
     session = 'b2c89c48-26c0-4a8a-818a-f5b1cf7d62dd'
     data_provider.load_session_data(session)
     output = Output()
-    Calculations(data_provider, output).run_project_calculations()
+    #Calculations(data_provider, output).run_project_calculations()
+#scenes calculation
+    scenes = [2, 32, 49]
+    for scene in scenes:
+        data_provider.load_scene_data(session, scene)
+        SceneCalculations(data_provider).calculate_kpis()
--- Projects/NESTLETH_SAND/Utils/KPISceneToolBox.py
+++ Projects/NESTLETH_SAND/Utils/KPISceneToolBox.py
@@ -1,36 +1,21 @@
-
+import pandas as pd
+import os
 from Trax.Algo.Calculations.Core.DataProvider import Data
 from Trax.Cloud.Services.Connector.Keys import DbUsers
 from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
-# from Trax.Utils.Logging.Logger import Log
-import pandas as pd
-import os
-
 from KPIUtils_v2.DB.CommonV2 import Common
-# from KPIUtils_v2.Calculations.AssortmentCalculations import Assortment
-# from KPIUtils_v2.Calculations.AvailabilityCalculations import Availability
-# from KPIUtils_v2.Calculations.NumberOfScenesCalculations import NumberOfScenes
-# from KPIUtils_v2.Calculations.PositionGraphsCalculations import PositionGraphs
-# from KPIUtils_v2.Calculations.SOSCalculations import SOS
-# from KPIUtils_v2.Calculations.SequenceCalculations import Sequence
-# from KPIUtils_v2.Calculations.SurveyCalculations import Survey
-
-# from KPIUtils_v2.Calculations.CalculationsUtils import GENERALToolBoxCalculations
-from KPIUtils.GlobalProjects.NESTLE.KPIGenerator import NESTLEGenerator
+from KPIUtils.GlobalProjects.NESTLE.SceneKPIGenerator import SceneGenerator
+from Trax.Utils.Logging.Logger import Log
 
 __author__ = 'limorc'
 
 KPI_RESULT = 'report.kpi_results'
 KPK_RESULT = 'report.kpk_results'
 KPS_RESULT = 'report.kps_results'
-#TEMPLATE_PATH = os.path.join(os.path.dirname(
-#    os.path.realpath(__file__)), '..', 'Data', 'Template.xlsx')
 
-class NESTLETHToolBox:
-    LEVEL1 = 1
-    LEVEL2 = 2
-    LEVEL3 = 3
 
+class NESTLETHSceneToolBox:
+    ERROR_LOC = -1
     def __init__(self, data_provider, output):
         self.output = output
         self.data_provider = data_provider
@@ -38,27 +23,27 @@ class NESTLETHToolBox:
         self.project_name = self.data_provider.project_name
         self.session_uid = self.data_provider.session_uid
         self.products = self.data_provider[Data.PRODUCTS]
+        self.templates = self.data_provider[Data.TEMPLATES]
         self.all_products = self.data_provider[Data.ALL_PRODUCTS]
         self.match_product_in_scene = self.data_provider[Data.MATCHES]
         self.visit_date = self.data_provider[Data.VISIT_DATE]
         self.session_info = self.data_provider[Data.SESSION_INFO]
         self.scene_info = self.data_provider[Data.SCENES_INFO]
         self.store_id = self.data_provider[Data.STORE_FK]
-        self.scif = self.data_provider[Data.SCENE_ITEM_FACTS]
+        self.store_type = self.data_provider.store_type
         self.rds_conn = PSProjectConnector(self.project_name, DbUsers.CalculationEng)
         self.kpi_static_data = self.common.get_kpi_static_data()
         self.kpi_results_queries = []
-        self.nestle_generator = NESTLEGenerator(self.data_provider, self.output, self.common)
+        self.nestle_generator = SceneGenerator(self.data_provider, self.output, self.common)
 
-    def main_calculation(self, *args, **kwargs):
-        """
-        This function calculates the KPI results.
-        """
-
+    def main_function(self):
         shelf_placement_template = pd.read_excel(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'Data',
                                                               'Placement.xlsx'), sheetname='Minimum Shelf',
                                                  keep_default_na=False)
-        self.nestle_generator.nestle_global_shelf_placement_function(shelf_placement_template)
-        score = 0
-        return score
-
+        shelf_placement_dict = self.nestle_generator.nestle_global_shelf_placement_function(shelf_placement_template)
+        if shelf_placement_dict == self.ERROR_LOC:
+            Log.warning("Shelf placement failed for scene - {0} "
+                        "Match product in scene doesnt contain Nestle product in layer 1 ".format(self.scene_info['scene_fk'].iloc[0]))
+            return 0
+        self.common.save_json_to_new_tables(shelf_placement_dict)
+        return 0
