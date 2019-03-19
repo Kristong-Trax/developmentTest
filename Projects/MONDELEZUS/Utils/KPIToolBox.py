a9fd6af2252b5756ac30c023d9ce5c3ff7e895ef
nicolaske <nicolaske@traxretail.com>
2019-03-19 12:22:30 -0400

    template update to match db change

--- Projects/MONDELEZUS/Utils/KPIToolBox.py
+++ Projects/MONDELEZUS/Utils/KPIToolBox.py
@@ -173,6 +173,7 @@ class MONDELEZUSToolBox:
 
                     shelf_values.append(tested_group_linear_value)
                 # max_shelf_mm = max(shelf_values)
+                # TEST_MAX_SHELF_space_length = (3 * math.ceil((max_shelf_mm * self.MM_TO_FEET_CONVERSION) / 3.))
                 total_space_length_mm = sum(shelf_values)
                 space_length = (3 * math.ceil((total_space_length_mm * self.MM_TO_FEET_CONVERSION) / 3.))
 
