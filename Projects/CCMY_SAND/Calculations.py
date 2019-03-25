3cbfb1965ea993be7f701f0f96d53289bbeece7a
sathiyanarayanand <sathiyanarayanan@traxretail.com>
2019-03-25 12:22:25 +0800

    PROS-7314 CCMY New KPI developments. Number of Pure shelves and Total Number of shelves

--- Projects/CCMY_SAND/Calculations.py
+++ Projects/CCMY_SAND/Calculations.py
@@ -15,16 +15,12 @@ class CCMY_SANDCalculations(BaseCalculationsScript):
         CCMY_SANDGenerator(self.data_provider, self.output).main_function()
         self.timer.stop('KPIGenerator.run_project_calculations')
 
-if __name__ == '__main__':
-    LoggerInitializer.init('ccmy-sand calculations')
-    Config.init()
-    project_name = 'ccmy-sand'
-    data_provider = KEngineDataProvider(project_name)
-    # session = '0030b950-c998-4fe5-a579-eb9263caf683'
-    # session = '01a7d79c-8062-4709-bd8c-5910c7e93aed'
-    # session = '531a5188-88e8-4160-9391-aba4bb342466'
-    # session ='b5e13d4f-4a67-4bc4-b7db-7dc867f043b4'
-    session='4689b9e7-baf5-4ceb-961e-ab4b2bc61bd5'
-    data_provider.load_session_data(session)
-    output = Output()
-    CCMY_SANDCalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('ccmy-sand calculations')
+#     Config.init()
+#     project_name = 'ccmy-sand'
+#     data_provider = KEngineDataProvider(project_name)
+#     session='4689b9e7-baf5-4ceb-961e-ab4b2bc61bd5'
+#     data_provider.load_session_data(session)
+#     output = Output()
+#     CCMY_SANDCalculations(data_provider, output).run_project_calculations()
