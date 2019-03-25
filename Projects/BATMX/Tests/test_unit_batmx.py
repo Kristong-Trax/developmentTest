b76bd3e21cb9dca478276705a12ce45f60a24aa7
elyashivm <elyashivm@traxretail.com>
2019-03-25 10:28:49 +0200

    batmx 1

--- Projects/BATMX/Tests/test_unit_batmx.py
+++ Projects/BATMX/Tests/test_unit_batmx.py
@@ -3,20 +3,19 @@ from Trax.Utils.Conf.Configuration import Config
 from Trax.Utils.Testing.Case import TestCase
 from mock import MagicMock, mock
 import pandas as pd
-from Projects.BATMX_SAND.Utils.KPIToolBox import BATMXToolBox
+from Projects.BATMX.Utils.KPIToolBox import BATMXToolBox
 
 
 __author__ = 'elyashiv'
 
 
-class TestBATMX_SAND(TestCase):
+class TestBATMX(TestCase):
 
-    @mock.patch('Projects.BATMX_SAND.Utils.KPIToolBox.ProjectConnector')
+    @mock.patch('Projects.BATMX.Utils.KPIToolBox.ProjectConnector')
     def setUp(self, x):
         Config.init('')
         self.data_provider_mock = MagicMock()
-        self.data_provider_mock.project_name = 'batmx-sand'
+        self.data_provider_mock.project_name = 'batmx'
         self.data_provider_mock.rds_conn = MagicMock()
         self.output = MagicMock()
         self.tool_box = BATMXToolBox(self.data_provider_mock, MagicMock())
-
