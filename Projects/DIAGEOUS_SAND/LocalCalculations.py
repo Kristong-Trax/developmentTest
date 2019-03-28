4a875c4be4148138571a995370f7f89895423342
Hunter Young <huntery@traxretail.com>
2019-03-28 11:01:52 -0400

    Updated SAND

--- Projects/DIAGEOUS_SAND/LocalCalculations.py
+++ Projects/DIAGEOUS_SAND/LocalCalculations.py
@@ -1,20 +1,19 @@
-
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-from Projects.DIAGEOUS_SAND.Calculations import DIAGEOUS_SANDCalculations
-
-
-if __name__ == '__main__':
-    LoggerInitializer.init('diageous calculations')
-    Config.init()
-    project_name = 'diageous-sand'
-    sessions = [
-        "004F1CF3-7135-44FD-9651-5F1E9E4C0BB6",
-        "CFF663F6-D2C8-453C-B901-95BE44368809",
-    ]
-    for session in sessions:
-        data_provider = KEngineDataProvider(project_name)
-        data_provider.load_session_data(session)
-        output = Output()
-        DIAGEOUS_SANDCalculations(data_provider, output).run_project_calculations()
+#
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from Projects.DIAGEOUS_SAND.Calculations import DIAGEOUS_SANDCalculations
+#
+#
+# if __name__ == '__main__':
+#     LoggerInitializer.init('diageous calculations')
+#     Config.init()
+#     project_name = 'diageous-sand'
+#     sessions = [
+#         "A27A5DEB-E598-45A3-ADAE-F56A2F0FA320",
+#     ]
+#     for session in sessions:
+#         data_provider = KEngineDataProvider(project_name)
+#         data_provider.load_session_data(session)
+#         output = Output()
+#         DIAGEOUS_SANDCalculations(data_provider, output).run_project_calculations()
