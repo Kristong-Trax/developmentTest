9c7cd84694ae0bcbe136c978d9b53f2c02db6ffd
idanre <idanr@traxretail.com>
2019-04-03 23:45:50 +0300

    fix in encoding marsru

--- Projects/MARSRU_PROD/LocalCalculations.py
+++ Projects/MARSRU_PROD/LocalCalculations.py
@@ -1,18 +1,16 @@
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-from Projects.MARSRU_PROD.Calculations import MARSRU_PRODCalculations
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from Projects.MARSRU_PROD.Calculations import MARSRU_PRODCalculations
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('MARSRU calculations')
-    Config.init()
-    project_name = 'marsru-prod'
-    session_uids = [
-        'b6677800-673d-43f8-90e2-967c57d36bd6',
-    ]
-    data_provider = KEngineDataProvider(project_name)
-    output = Output()
-    for session in session_uids:
-        data_provider.load_session_data(session)
-        MARSRU_PRODCalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('MARSRU calculations')
+#     Config.init()
+#     project_name = 'marsru-prod'
+#     session_uids = ['2178daba-8dc4-4aa1-936a-fc5cae7c08b9']
+#     data_provider = KEngineDataProvider(project_name)
+#     output = Output()
+#     for session in session_uids:
+#         data_provider.load_session_data(session)
+#         MARSRU_PRODCalculations(data_provider, output).run_project_calculations()
