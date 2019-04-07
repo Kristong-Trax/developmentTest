8c7d3ef2b5d21a47383866106e8fca8775286687
ilay_s <ilays@traxretail.com>
2019-04-07 17:16:45 +0300

    added sos custom functions

--- Projects/DIAGEOGH/Calculations.py
+++ Projects/DIAGEOGH/Calculations.py
@@ -17,13 +17,13 @@ class DIAGEOGHCalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('diageogh calculations')
-    Config.init()
-    project_name = 'diageogh'
-    data_provider = KEngineDataProvider(project_name)
-    session = 'ff653a3e-2a29-42cc-a54b-8670f746c8ab'
-    data_provider.load_session_data(session)
-    output = Output()
-    DIAGEOGHCalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('diageogh calculations')
+#     Config.init()
+#     project_name = 'diageogh'
+#     data_provider = KEngineDataProvider(project_name)
+#     session = 'ff653a3e-2a29-42cc-a54b-8670f746c8ab'
+#     data_provider.load_session_data(session)
+#     output = Output()
+#     DIAGEOGHCalculations(data_provider, output).run_project_calculations()
 
