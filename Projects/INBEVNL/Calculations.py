86474bba6b9e4e7616eb73e4cd5ef5e34f34f7e5
idanre <idanr@traxretail.com>
2019-04-07 15:59:02 +0300

    inbevnl-fix

--- Projects/INBEVNL/Calculations.py
+++ Projects/INBEVNL/Calculations.py
@@ -1,9 +1,9 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-from Trax.Algo.Calculations.Core.DataProvider import ACEDataProvider, Output, KEngineDataProvider
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-from mock import MagicMock
+# from Trax.Algo.Calculations.Core.DataProvider import ACEDataProvider, Output, KEngineDataProvider
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from mock import MagicMock
 
 from Projects.INBEVNL.KPIGenerator import INBEVNLINBEVBEGenerator
 
@@ -17,12 +17,12 @@ class INBEVNLINBEVBECalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('inbevnl calculations')
-    Config.init()
-    project_name = 'inbevnl'
-    data_provider = KEngineDataProvider(project_name, monitor=MagicMock())
-    session = '231e2358-824e-4bf9-bd9d-281fc64dd093'
-    data_provider.load_session_data(session)
-    output = Output()
-    INBEVNLINBEVBECalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('inbevnl calculations')
+#     Config.init()
+#     project_name = 'inbevnl'
+#     data_provider = KEngineDataProvider(project_name, monitor=MagicMock())
+#     session = '231e2358-824e-4bf9-bd9d-281fc64dd093'
+#     data_provider.load_session_data(session)
+#     output = Output()
+#     INBEVNLINBEVBECalculations(data_provider, output).run_project_calculations()
