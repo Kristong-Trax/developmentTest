317ae90555c215d99435d529e92c9499509c8892
yasmingr <yaming@traxretail.com>
2019-04-07 14:01:04 +0300

    migrate project

--- Projects/SANOFIUA/Calculations.py
+++ Projects/SANOFIUA/Calculations.py
@@ -1,8 +1,8 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 import os
 from KPIUtils.GlobalProjects.SANOFI_2.KPIGenerator import SANOFIGenerator
 
@@ -21,12 +21,12 @@ class SANOFIUACalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('sanofiua calculations')
-    Config.init()
-    project_name = 'sanofiua'
-    data_provider = KEngineDataProvider(project_name)
-    session = '5694050B-8310-4A9C-87AB-D9A0D796DB28'
-    data_provider.load_session_data(session)
-    output = Output()
-    SANOFIUACalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('sanofiua calculations')
+#     Config.init()
+#     project_name = 'sanofiua'
+#     data_provider = KEngineDataProvider(project_name)
+#     session = '5694050B-8310-4A9C-87AB-D9A0D796DB28'
+#     data_provider.load_session_data(session)
+#     output = Output()
+#     SANOFIUACalculations(data_provider, output).run_project_calculations()
