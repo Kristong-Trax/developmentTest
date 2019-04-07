6e21b29e02f655b0dc2103d72d898a9ba995fdd9
yasmingr <yaming@traxretail.com>
2019-04-07 14:36:09 +0300

    migrate project

--- Projects/SANOFIRU/Calculations.py
+++ Projects/SANOFIRU/Calculations.py
@@ -1,9 +1,9 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-from Trax.Utils.Logging.Logger import Log
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from Trax.Utils.Logging.Logger import Log
 import os
 from KPIUtils.GlobalProjects.SANOFI_2.KPIGenerator import SANOFIGenerator
 
@@ -19,12 +19,12 @@ class SANOFIRUCalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('sanofiru calculations')
-    Config.init()
-    project_name = 'sanofiru'
-    data_provider = KEngineDataProvider(project_name)
-    session = 'C18C862E-ECCA-4181-98CF-DB595FBFE1FD'
-    data_provider.load_session_data(session)
-    output = Output()
-    SANOFIRUCalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('sanofiru calculations')
+#     Config.init()
+#     project_name = 'sanofiru'
+#     data_provider = KEngineDataProvider(project_name)
+#     session = 'C18C862E-ECCA-4181-98CF-DB595FBFE1FD'
+#     data_provider.load_session_data(session)
+#     output = Output()
+#     SANOFIRUCalculations(data_provider, output).run_project_calculations()
