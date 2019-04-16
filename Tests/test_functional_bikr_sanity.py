58a96c522b3408ab91b3c12ac749959c036413c1
sathiyanarayanand <sathiyanarayanan@traxretail.com>
2019-04-16 18:37:10 +0800

    PROS-8638 BIKR Sanity Check update seed file

--- Tests/test_functional_bikr_sanity.py
+++ Tests/test_functional_bikr_sanity.py
@@ -1,10 +1,9 @@
 
 import os
+import MySQLdb
 
-from Trax.Apps.Core.Testing.BaseCase import TestFunctionalCase
 from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
 from Trax.Data.Testing.SeedNew import Seeder
-import MySQLdb
 from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
 from Trax.Cloud.Services.Connector.Keys import DbUsers
 from Trax.Data.Testing.TestProjects import TestProjectsNames
@@ -14,16 +13,11 @@ from Tests.Data.TestData.test_data_bikr_sanity import ProjectsSanityData
 from Projects.BIKR.Calculations import BIKRCalculations
 from Trax.Apps.Core.Testing.BaseCase import TestMockingFunctionalCase
 
-from Tests.TestUtils import remove_cache_and_storage
 
-__author__ = 'yoava'
+__author__ = 'sathiyanarayanan'
 
 
-class TestKEngineOutOfTheBox(TestFunctionalCase):
-
-    def set_up(self):
-        super(TestKEngineOutOfTheBox, self).set_up()
-        remove_cache_and_storage()
+class TestKEngineOutOfTheBox(TestMockingFunctionalCase):
 
     @property
     def import_path(self):
