b8ac02f754dce25fb77fd8175165c4ab5b1594b7
Hunter Young <huntery@traxretail.com>
2019-05-02 14:53:37 -0400

    Updated retailers for the second time

--- Projects/CCBOTTLERSUS/WAREHOUSE_JUICE/Const.py
+++ Projects/CCBOTTLERSUS/WAREHOUSE_JUICE/Const.py
@@ -15,7 +15,8 @@ class Const(object):
 
     # template stuff
     TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Data', 'Must Have SKUs v3.xlsx')
-    RETAILERS = ['C&S NE Key Foods', 'Krasdale', 'General Trading', 'C&S Foodtown', 'C&S Heritage']
+    RETAILERS = ['C&S NE Key Foods', 'Krasdale', 'General Trading', 'C&S Foodtown', 'C&S Heritage', 'C&S NE Ind',
+                 'C&S Associated Group', 'Bozzutos Ind']
     UPC = 'UPC'
     PRODUCT_NAME = 'Product/No'
     SET_SIZE = 'Set Size'
