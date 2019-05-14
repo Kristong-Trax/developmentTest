025212d48b99df649865811bfdb52c7f2bec929e
limor_c <limorc@traxretail.com>
2019-05-14 15:11:52 +0300

    gsk global kpis

---
+++ Projects/GSKJP_SAND/KPIGenerator.py
@@ -0,0 +1,30 @@
+
+from Trax.Utils.Logging.Logger import Log
+
+from Projects.GSKJP_SAND.Utils.KPIToolBox import GSKJPToolBox
+
+from KPIUtils_v2.DB.Common import Common
+
+from KPIUtils_v2.Utils.Decorators.Decorators import log_runtime
+
+__author__ = 'limorc'
+
+
+class Generator:
+
+    def __init__(self, data_provider, output):
+        self.data_provider = data_provider
+        self.output = output
+        self.project_name = data_provider.project_name
+        self.session_uid = self.data_provider.session_uid
+        self.tool_box = GSKJPToolBox(self.data_provider, self.output)
+        self.common = Common(data_provider)
+
+    @log_runtime('Total Calculations', log_start=True)
+    def main_function(self):
+        """
+        This is the main KPI calculation function.
+        It calculates the score for every KPI set and saves it to the DB.
+        """
+
+        self.tool_box.main_calculation()
