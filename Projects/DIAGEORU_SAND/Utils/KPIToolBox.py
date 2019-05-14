83010e016040eea61b3392e86e9f8cf123be7b85
ilay_s <ilays@traxretail.com>
2019-05-14 15:53:08 +0300

    add another assortment function

--- Projects/DIAGEORU_SAND/Utils/KPIToolBox.py
+++ Projects/DIAGEORU_SAND/Utils/KPIToolBox.py
@@ -489,6 +489,8 @@ class DIAGEORUToolBox:
         """
         This function writes all KPI results to the DB, and commits the changes.
         """
+        self.rds_conn.disconnect_rds()
+        self.rds_conn.connect_rds()
         cur = self.rds_conn.db.cursor()
         delete_queries = DIAGEOQueries.get_delete_session_results_query_old_tables(self.session_uid)
         for query in delete_queries:
