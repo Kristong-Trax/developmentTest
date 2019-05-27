206b241ff329edb0cc1a8d7789ce51aedc36f191
idanre <idanr@traxretail.com>
2019-05-27 16:34:24 +0300

    Tara SAND - Initialize

---
+++ Projects/CBCDAIRYIL_SAND/Calculations.py
@@ -0,0 +1,16 @@
+
+from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
+
+from Projects.CBCDAIRYIL_SAND.KPIGenerator import Generator
+
+__author__ = 'idanr'
+
+
+class Calculations(BaseCalculationsScript):
+    def run_project_calculations(self):
+        self.timer.start()
+        Generator(self.data_provider, self.output).main_function()
+        self.timer.stop('KPIGenerator.run_project_calculations')
+
+
+
