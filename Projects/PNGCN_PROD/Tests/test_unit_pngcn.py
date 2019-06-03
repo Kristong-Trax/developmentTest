2268e4c91b90dfd5102ec7aa5afe44549715362e
avrahamA <avrahama@traxretail.com>
2019-06-03 12:28:40 +0300

    add test on presize_linear_length

--- Projects/PNGCN_PROD/Tests/test_unit_pngcn.py
+++ Projects/PNGCN_PROD/Tests/test_unit_pngcn.py
@@ -1,5 +1,6 @@
 from Trax.Utils.Testing.Case import MockingTestCase
-# from Projects.PNGCN_PROD.SceneKpis.KPISceneToolBox import PngcnSceneKpis
+from mock import MagicMock
+from Projects.PNGCN_PROD.SceneKpis.KPISceneToolBox import PngcnSceneKpis
 
 __author__ = 'avrahama'
 
@@ -12,6 +13,19 @@ class Test_PNGCN(MockingTestCase):
     def set_up(self):
         super(Test_PNGCN, self).set_up()
 
-    def test_calculate_presize_linear_length(self):
+        # mock PSProjectConnector
+        self.ProjectConnector_mock = self.mock_object('ProjectConnector', path='KPIUtils_v2.DB.PsProjectConnector')
 
-        pass
+        # mock 'Common' object used in toolbox
+        self.common_mock = self.mock_object('Common', path='KPIUtils_v2.DB.CommonV2')
+
+        # mock 'data provider' object giving to the toolbox
+        self.data_provider_mock = MagicMock()
+
+
+    def test_calculate_presize_linear_length(self):
+        # mock: project_connector, common, scene_id
+        scene_tool_box = PngcnSceneKpis(self.ProjectConnector_mock, self.common_mock, 132487, self.data_provider_mock)
+
+        print type(scene_tool_box)
+
