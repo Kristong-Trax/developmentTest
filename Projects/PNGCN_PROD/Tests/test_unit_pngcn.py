2a1bd994b055e908f045f2a327222ff9cd4baa24
avrahamA <avrahama@traxretail.com>
2019-06-03 13:59:38 +0300

    add test on presize_linear_length 2

--- Projects/PNGCN_PROD/Tests/test_unit_pngcn.py
+++ Projects/PNGCN_PROD/Tests/test_unit_pngcn.py
@@ -17,15 +17,38 @@ class Test_PNGCN(MockingTestCase):
         self.ProjectConnector_mock = self.mock_object('ProjectConnector', path='KPIUtils_v2.DB.PsProjectConnector')
 
         # mock 'Common' object used in toolbox
-        self.common_mock = self.mock_object('Common', path='KPIUtils_v2.DB.CommonV2')
+        self.common_mock = self.mock_object('Common.get_kpi_fk_by_kpi_name', path='KPIUtils_v2.DB.CommonV2')
+        self.common_mock.return_value = 3
 
         # mock 'data provider' object giving to the toolbox
         self.data_provider_mock = MagicMock()
 
 
     def test_calculate_presize_linear_length(self):
+        """
+        1. test if the numerator is greater then denominator (if the subgroup is greater then containing group)
+        :return:
+        """
+
+
         # mock: project_connector, common, scene_id
         scene_tool_box = PngcnSceneKpis(self.ProjectConnector_mock, self.common_mock, 132487, self.data_provider_mock)
+
+        # mock:
+        # kpi_fk = self.common.get_kpi_fk_by_kpi_name(PRESIZE_LINEAR_LENGTH_PER_LENGTH)
+        # numerator = self.scif.width_mm.sum()  # get the width of P&G products in scene
+        # denominator = self.matches_from_data_provider.width_mm.sum()  # get the width of all products in scene
+        # if denominator:
+        #     score = numerator / denominator  # get the percentage of P&G products from all products
+        #     self.common.write_to_db_result
+        a = scene_tool_box.common.get_kpi_fk_by_kpi_name
+        print '*********************************************\n',a # = self.mock_object('Common', path='KPIUtils_v2.DB.CommonV2')
+        denominator, numerator = 3, 2
+
+        self.assertGreaterEqual(denominator, numerator, 'numerator cant be greater then denominator')
+
+
+
 
         print type(scene_tool_box)
 
