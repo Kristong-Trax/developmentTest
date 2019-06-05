8f468dd1af88e793a1cfe7ef0dc8ce02f4d17df0
limor_c <limorc@traxretail.com>
2019-06-05 17:34:37 +0300

    new seed

--- Projects/DIAGEOUK/Calculations.py
+++ Projects/DIAGEOUK/Calculations.py
@@ -17,12 +17,12 @@ class DIAGEOUKCalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('diageouk calculations')
-    Config.init()
-    project_name = 'diageouk'
-    data_provider = KEngineDataProvider(project_name)
-    session = 'F2AF67CE-89BF-48E5-8F24-6A14704582C9'
-    data_provider.load_session_data(session)
-    output = Output()
-    DIAGEOUKCalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('diageouk calculations')
+#     Config.init()
+#     project_name = 'diageouk'
+#     data_provider = KEngineDataProvider(project_name)
+#     session = 'F2AF67CE-89BF-48E5-8F24-6A14704582C9'
+#     data_provider.load_session_data(session)
+#     output = Output()
+#     DIAGEOUKCalculations(data_provider, output).run_project_calculations()
