f82f20d766097925cdc5fe1dbb0f829e0562db8b
limor_c <limorc@traxretail.com>
2019-06-06 16:42:59 +0300

    new seed

--- Tests/create_sanity_tests_for_project.py
+++ Tests/create_sanity_tests_for_project.py
@@ -224,15 +224,15 @@ class ProjectsSanityData(BaseSeedData):
             f.write(data_class_content)
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('')
-    Config.init()
-    project_to_test = 'gskau'
-    creator = SeedCreator(project_to_test)
-    creator.activate_exporter(specific_sessions=
-                              ['C804124E-7223-446A-A084-A143E45D94D8'])
-    creator.rds_conn.disconnect_rds()
-    data_class = CreateTestDataProjectSanity(project_to_test)
-    data_class.create_data_class()
-    sanity = SanityTestsCreator(project_to_test, creator.TOP_SESSIONS)
-    sanity.create_test_class()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('')
+#     Config.init()
+#     project_to_test = 'gskau'
+#     creator = SeedCreator(project_to_test)
+#     creator.activate_exporter(specific_sessions=
+#                               ['C804124E-7223-446A-A084-A143E45D94D8'])
+#     creator.rds_conn.disconnect_rds()
+#     data_class = CreateTestDataProjectSanity(project_to_test)
+#     data_class.create_data_class()
+#     sanity = SanityTestsCreator(project_to_test, creator.TOP_SESSIONS)
+#     sanity.create_test_class()
