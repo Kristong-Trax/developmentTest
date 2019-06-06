10cee01bae4c9cf95ad6051bf17e8dff8129a92e
nidhinb <nidhinb@traxretail.com>
2019-06-06 12:30:06 +0800

    removing commented code

--- Projects/TWEGAU/Utils/KPIToolBox.py
+++ Projects/TWEGAU/Utils/KPIToolBox.py
@@ -8,15 +8,6 @@ from KPIUtils_v2.DB.CommonV2 import Common, PSProjectConnector
 from Trax.Algo.Calculations.Core.DataProvider import Data
 from Trax.Cloud.Services.Connector.Keys import DbUsers
 
-# from KPIUtils_v2.Calculations.AssortmentCalculations import Assortment
-# from KPIUtils_v2.Calculations.AvailabilityCalculations import Availability
-# from KPIUtils_v2.Calculations.NumberOfScenesCalculations import NumberOfScenes
-# from KPIUtils_v2.Calculations.PositionGraphsCalculations import PositionGraphs
-# from KPIUtils_v2.Calculations.SOSCalculations import SOS
-# from KPIUtils_v2.Calculations.SequenceCalculations import Sequence
-# from KPIUtils_v2.Calculations.SurveyCalculations import Survey
-
-# from KPIUtils_v2.Calculations.CalculationsUtils import GENERALToolBoxCalculations
 
 __author__ = 'nidhin'
 
