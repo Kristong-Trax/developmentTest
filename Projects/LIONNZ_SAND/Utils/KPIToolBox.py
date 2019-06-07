9b092a79772e4ce55a0240de8e2de0338cbd49d5
nidhinb <nidhinb@traxretail.com>
2019-06-07 18:46:03 +0800

    adding assortments

--- Projects/LIONNZ_SAND/Utils/KPIToolBox.py
+++ Projects/LIONNZ_SAND/Utils/KPIToolBox.py
@@ -1,13 +1,16 @@
 import os
+import json
 import pandas as pd
+import numpy as np
 
 from KPIUtils_v2.DB.CommonV2 import Common
+from Trax.Utils.Logging.Logger import Log
 from Trax.Cloud.Services.Connector.Keys import DbUsers
 from Trax.Algo.Calculations.Core.DataProvider import Data
 from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
 
 __author__ = 'nidhin'
-
+OWN_MAN_NAME = 'Lion'  # case insensitive
 TEMPLATE_PARENT_FOLDER = 'Data'
 TEMPLATE_NAME = 'Template.xlsx'
 ASSORTMENT_TEMPLATE_NAME = 'Assortments.xlsx'
@@ -48,14 +51,23 @@ PARAM_DB_MAP = {
 }
 # list of `exclude_include` sheet columns
 INC_EXC_LIST = ['stacking', 'others', 'irrelevant', 'empty',
-                'categories_to_exclude', 'scene_types_to_include', ]
+                'categories_to_exclude', 'scene_types_to_include', 'brands',
+                'ean_codes']
 # assortment KPIs
+# Codes
+OOS_CODE = 1
+PRESENT_CODE = 2
+EXTRA_CODE = 3
+# KPI Names
 DST_MAN_BY_STORE_PERC = 'DST_MAN_BY_STORE_PERC'
 OOS_MAN_BY_STORE_PERC = 'OOS_MAN_BY_STORE_PERC'
-
 PRODUCT_PRESENCE_BY_STORE_LIST = 'PRODUCT_PRESENCE_BY_STORE_LIST'
 OOS_PRODUCT_BY_STORE_LIST = 'OOS_PRODUCT_BY_STORE_LIST'
-
+# map to save list kpis
+CODE_KPI_MAP = {
+    OOS_CODE: OOS_PRODUCT_BY_STORE_LIST,
+    PRESENT_CODE:PRODUCT_PRESENCE_BY_STORE_LIST,
+}
 
 
 class LIONNZToolBox:
@@ -88,7 +100,9 @@ class LIONNZToolBox:
         self.assortment_template_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                                      '..', TEMPLATE_PARENT_FOLDER,
                                                      ASSORTMENT_TEMPLATE_NAME)
-
+        self.own_man_fk = self.all_products[
+            self.all_products['manufacturer_name'].str.lower() == OWN_MAN_NAME.lower()
+        ]['manufacturer_fk'].values[0]
         self.kpi_template = pd.ExcelFile(self.kpi_template_path)
         self.empty_prod_ids = self.all_products[
             self.all_products.product_name.str.contains('empty', case=False)]['product_fk'].values
@@ -101,28 +115,127 @@ class LIONNZToolBox:
         """
         This function calculates the KPI results.
         """
-        # self.filter_and_send_kpi_to_calc()
+        self.filter_and_send_kpi_to_calc()
         self.calculate_assortment_kpis()
-        # self.common.commit_results_data()
+        self.common.commit_results_data()
         return 0  # to mark successful run of script
 
     def calculate_assortment_kpis(self):
-        assortment_sheet = self.kpi_template.parse(ASSORTMENT_SHEET)
-        assortment_details = self.kpi_template.parse(KPI_DETAILS_SHEET)
-        for idx, each_assortment_kpi in assortment_sheet.iterrows():
-            kpi_detail = assortment_details[assortment_details[KPI_NAME_COL] == each_assortment_kpi[KPI_NAME_COL]]
-            kpi = self.kpi_static_data[(self.kpi_static_data[KPI_TYPE_COL] == kpi_detail[KPI_NAME_COL].iloc[0])
-                                       & (self.kpi_static_data['delete_time'].isnull())]
-            if not is_nan(each_assortment_kpi['opposite']):
-                opposite_kpi_detail = assortment_details[assortment_details[KPI_NAME_COL] == each_assortment_kpi['opposite']]
-                opposite_kpi = self.kpi_static_data[(self.kpi_static_data[KPI_TYPE_COL] == opposite_kpi_detail[KPI_NAME_COL].iloc[0])
+        distribution_kpi = self.kpi_static_data[(self.kpi_static_data[KPI_TYPE_COL] == DST_MAN_BY_STORE_PERC)
+                                   & (self.kpi_static_data['delete_time'].isnull())]
+        oos_kpi = self.kpi_static_data[(self.kpi_static_data[KPI_TYPE_COL] == OOS_MAN_BY_STORE_PERC)
+                                   & (self.kpi_static_data['delete_time'].isnull())]
+        prod_presence_kpi = self.kpi_static_data[(self.kpi_static_data[KPI_TYPE_COL] == PRODUCT_PRESENCE_BY_STORE_LIST)
                                            & (self.kpi_static_data['delete_time'].isnull())]
-            # check active
-            # check store_type
-            # check kpi emtpy
-            policy_data = self.get_policies()
-            # if each_assortment_kpi['kpi_family'] == ""
-            pass
+        oos_prod_kpi = self.kpi_static_data[(self.kpi_static_data[KPI_TYPE_COL] == OOS_PRODUCT_BY_STORE_LIST)
+                                           & (self.kpi_static_data['delete_time'].isnull())]
+
+        def __return_valid_store_policies(policy):
+            policy_json = json.loads(policy)
+            store_json = json.loads(self.store_info.reset_index().to_json(orient='records'))[0]
+            valid_store = True
+            for key, values in policy_json.iteritems():
+                if str(store_json[key]) in values:
+                    continue
+                else:
+                    valid_store = False
+                    break
+            return valid_store
+        policy_data = self.get_policies(distribution_kpi.iloc[0].pk)
+        resp = policy_data['policy'].apply(__return_valid_store_policies)
+        valid_policy_data = policy_data[resp]
+        # calculate and save the percentage values for distribution and oos
+        self.calculate_and_save_distribution_and_oos(
+            assortment_product_fks=valid_policy_data['product_fk'],
+            distribution_kpi_fk=distribution_kpi.iloc[0].pk,
+            oos_kpi_fk=oos_kpi.iloc[0].pk
+        )
+        # calculate and save prod presence and oos products
+        self.calculate_and_save_prod_presence_and_oos_products(
+            assortment_product_fks=valid_policy_data['product_fk'],
+            prod_presence_kpi_fk=prod_presence_kpi.iloc[0].pk,
+            oos_prod_kpi_fk=oos_prod_kpi.iloc[0].pk,
+            distribution_kpi_name=DST_MAN_BY_STORE_PERC,
+            oos_kpi_name=OOS_MAN_BY_STORE_PERC
+        )
+
+    def calculate_and_save_prod_presence_and_oos_products(self, assortment_product_fks,
+                                                          prod_presence_kpi_fk, oos_prod_kpi_fk,
+                                                          distribution_kpi_name, oos_kpi_name):
+        total_products_in_scene = self.scif["item_id"].unique()
+        present_products = np.intersect1d(total_products_in_scene, assortment_product_fks)
+        extra_products = np.setdiff1d(total_products_in_scene, present_products)
+        oos_products = np.setdiff1d(assortment_product_fks, present_products)
+        product_map = {
+            OOS_CODE: present_products,
+            PRESENT_CODE: extra_products,
+            EXTRA_CODE: oos_products
+        }
+        # save product presence; with distribution % kpi as parent
+        for assortment_code, product_fks in product_map.iteritems():
+            for each_fk in product_fks:
+                self.common.write_to_db_result(fk=prod_presence_kpi_fk,
+                                               numerator_id=each_fk,
+                                               denominator_id=self.store_id,
+                                               context_id=self.store_id,
+                                               result=assortment_code,
+                                               score=assortment_code,
+                                               identifier_result=CODE_KPI_MAP.get(assortment_code),
+                                               identifier_parent=distribution_kpi_name,
+                                               should_enter=True
+                                               )
+            if assortment_code == OOS_CODE:
+                # save OOS products; with OOS % kpi as parent
+                for each_fk in product_fks:
+                    self.common.write_to_db_result(fk=oos_prod_kpi_fk,
+                                                   numerator_id=each_fk,
+                                                   denominator_id=self.store_id,
+                                                   context_id=self.store_id,
+                                                   result=assortment_code,
+                                                   score=assortment_code,
+                                                   identifier_result=CODE_KPI_MAP.get(assortment_code),
+                                                   identifier_parent=oos_kpi_name,
+                                                   should_enter=True
+                                                   )
+
+    def calculate_and_save_distribution_and_oos(self, assortment_product_fks, distribution_kpi_fk, oos_kpi_fk):
+        """Function to calculate distribution and OOS percentage.
+        Saves distribution and oos percentage as values.
+        """
+        Log.info("Calculate distribution and OOS for {}".format(self.project_name))
+        #  count of lion sku / all sku assortment count
+        larger_dat, smaller_dat = self.scif["item_id"].unique(), assortment_product_fks
+        if len(assortment_product_fks) > len(self.scif["item_id"].unique()):
+            larger_dat = assortment_product_fks
+            smaller_dat = self.scif["item_id"].unique()
+        count_of_prod_in_scene = len([x for x in smaller_dat if x in larger_dat])
+        distribution_perc = count_of_prod_in_scene / float(len(assortment_product_fks)) * 100
+        oos_perc = 100 - distribution_perc
+        self.common.write_to_db_result(fk=distribution_kpi_fk,
+                                       numerator_id=self.own_man_fk,
+                                       denominator_id=self.store_id,
+                                       context_id=self.store_id,
+                                       result=distribution_perc,
+                                       score=distribution_perc,
+                                       )
+        self.common.write_to_db_result(fk=oos_kpi_fk,
+                                       numerator_id=self.own_man_fk,
+                                       denominator_id=self.store_id,
+                                       context_id=self.store_id,
+                                       result=oos_perc,
+                                       score=distribution_perc,
+                                       )
+
+    def get_policies(self, kpi_fk):
+        query = """ select a.kpi_fk, p.policy_name, p.policy, atag.assortment_group_fk, atp.assortment_fk, atp.product_fk, 
+                        atp.start_date, atp.end_date from pservice.assortment_to_product atp 
+                        join pservice.assortment_to_assortment_group atag on atp.assortment_fk = atag.assortment_fk 
+                        join pservice.assortment a on a.pk = atag.assortment_group_fk
+                        join pservice.policy p on p.pk = a.store_policy_group_fk
+                    where a.kpi_fk={kpi_fk};
+                    """
+        policies = pd.read_sql_query(query.format(kpi_fk=kpi_fk), self.rds_conn.db)
+        return policies
 
     def filter_and_send_kpi_to_calc(self):
         kpi_sheet = self.kpi_template.parse(KPI_NAMES_SHEET)
@@ -157,14 +270,14 @@ class LIONNZToolBox:
                 include_exclude_data_dict = get_include_exclude(_include_exclude)
                 dataframe_to_process = self.get_sanitized_match_prod_scene(include_exclude_data_dict)
             if kpi_sheet_row[KPI_FAMILY_COL] == FSOS:
-                self.calulcate_fsos(detail, groupers, query_string, dataframe_to_process)
+                self.calculate_fsos(detail, groupers, query_string, dataframe_to_process)
             elif kpi_sheet_row[KPI_FAMILY_COL] == Count:
                 self.calculate_count(detail, groupers, query_string, dataframe_to_process)
             else:
                 pass
         return True
 
-    def calulcate_fsos(self, kpi, groupers, query_string, dataframe_to_process):
+    def calculate_fsos(self, kpi, groupers, query_string, dataframe_to_process):
         if query_string:
             grouped_data_frame = dataframe_to_process.query(query_string).groupby(groupers)
         else:
@@ -216,12 +329,6 @@ class LIONNZToolBox:
 
         return True
 
-    def calculate_distribution_and_oos(self, kpi, groupers, query_string, dataframe_to_process):
-        pass
-
-    def calculate_product_presence_and_oos(self, kpi, groupers, query_string, dataframe_to_process):
-        pass
-
     def get_sanitized_match_prod_scene(self, include_exclude_data_dict):
         scene_product_data = self.match_product_in_scene.merge(
             self.products, how='left', on=['product_fk'],  suffixes=('', '_prod')
@@ -261,18 +368,6 @@ class LIONNZToolBox:
             query += ' and category not in {}'.format(categories_to_exclude)
         sanitized_products_in_scene = sanitized_products_in_scene.query(query.strip(' and '))
         return sanitized_products_in_scene
-
-
-    def get_policies(self):
-        query = """ select p.policy_name, p.policy, atag.assortment_group_fk, atp.assortment_fk, atp.product_fk, 
-                        atp.start_date, atp.end_date from pservice.assortment_to_product atp 
-                        join pservice.assortment_to_assortment_group atag on atp.assortment_fk = atag.assortment_fk 
-                        join pservice.assortment a on a.pk = atag.assortment_group_fk
-                        join pservice.policy p on p.pk = a.store_policy_group_fk;
-                    """
-        policies = pd.read_sql_query(query, self.rds_conn.db)
-        return policies
-
 
 
 def get_context_id(context_string, param_id_map):
