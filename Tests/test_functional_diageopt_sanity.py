ddbc1c82af03842adafe097b12184cb2bf4ba361
avrahamA <avrahama@traxretail.com>
2019-06-10 11:30:25 +0300

    create and fix pt & za seeds

--- Tests/test_functional_diageopt_sanity.py
+++ Tests/test_functional_diageopt_sanity.py
@@ -9,8 +9,8 @@ from Trax.Cloud.Services.Connector.Keys import DbUsers
 from Trax.Data.Testing.TestProjects import TestProjectsNames
 from Trax.Utils.Testing.Case import MockingTestCase
 
-from Tests.Data.TestData.test_data_diageoza_sanity import ProjectsSanityData
-from Projects.DIAGEOZA.Calculations import DIAGEOZACalculations
+from Tests.Data.TestData.test_data_diageopt_sanity import ProjectsSanityData
+from Projects.DIAGEOPT.Calculations import DIAGEOPTCalculations
 from Trax.Apps.Core.Testing.BaseCase import TestFunctionalCase
 
 from Tests.TestUtils import remove_cache_and_storage
@@ -44,13 +44,13 @@ class TestKEngineOutOfTheBox(TestFunctionalCase):
         self.assertNotEquals(len(kpi_results), 0)
         connector.disconnect_rds()
     
-    @seeder.seed(["mongodb_products_and_brands_seed", "diageoza_seed"], ProjectsSanityData())
-    def test_diageoza_sanity(self):
+    @seeder.seed(["mongodb_products_and_brands_seed", "diageopt_seed"], ProjectsSanityData())
+    def test_diageopt_sanity(self):
         project_name = ProjectsSanityData.project_name
         data_provider = KEngineDataProvider(project_name)
-        sessions = ['c211de22-7b8b-4b4c-a8ca-3673a0be4d9c']
+        sessions = ['963D013D-EEB6-48DF-B8EC-06C8E0C2AA6C']
         for session in sessions:
             data_provider.load_session_data(session)
             output = Output()
-            DIAGEOZACalculations(data_provider, output).run_project_calculations()
+            DIAGEOPTCalculations(data_provider, output).run_project_calculations()
             self._assert_kpi_results_filled()
