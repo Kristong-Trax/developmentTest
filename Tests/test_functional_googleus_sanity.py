2b269cfc34d16538a9936747d98a28d49a3f9ec4
yoava <yoava@traxretail.com>
2019-06-12 16:26:40 +0300

    so many fixes

--- Tests/test_functional_googleus_sanity.py
+++ Tests/test_functional_googleus_sanity.py
@@ -6,7 +6,6 @@ import MySQLdb
 from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
 from Trax.Cloud.Services.Connector.Keys import DbUsers
 from Trax.Data.Testing.TestProjects import TestProjectsNames
-from Trax.Utils.Testing.Case import MockingTestCase
 
 from Tests.Data.TestData.test_data_googleus_sanity import ProjectsSanityData
 from Projects.GOOGLEUS.Calculations import GOOGLEUSCalculations
