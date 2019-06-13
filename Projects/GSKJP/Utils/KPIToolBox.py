e509021eba5abeb0b8af79cfb02aedeccfc54dc5
limor_c <limorc@traxretail.com>
2019-06-13 16:04:33 +0300

    functional test

--- Projects/GSKJP/Utils/KPIToolBox.py
+++ Projects/GSKJP/Utils/KPIToolBox.py
@@ -2,12 +2,13 @@
 from Trax.Algo.Calculations.Core.DataProvider import Data
 from Trax.Cloud.Services.Connector.Keys import DbUsers
 from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
-# from Trax.Utils.Logging.Logger import Log
 import pandas as pd
 import os
 from KPIUtils_v2.Calculations.BlockCalculations import Block
 from KPIUtils.GlobalProjects.GSK.KPIGenerator import GSKGenerator
 from KPIUtils_v2.DB.CommonV2 import Common
+from KPIUtils_v2.GlobalDataProvider.PsDataProvider import PsDataProvider
+
 # from KPIUtils_v2.Calculations.AssortmentCalculations import Assortment
 # from KPIUtils_v2.Calculations.AvailabilityCalculations import Availability
 # from KPIUtils_v2.Calculations.NumberOfScenesCalculations import NumberOfScenes
@@ -17,6 +18,7 @@ from KPIUtils_v2.DB.CommonV2 import Common
 # from KPIUtils_v2.Calculations.SurveyCalculations import Survey
 
 # from KPIUtils_v2.Calculations.CalculationsUtils import GENERALToolBoxCalculations
+from Trax.Utils.Logging.Logger import Log
 
 __author__ = 'limorc'
 
@@ -53,15 +55,22 @@ class GSKJPToolBox:
 
         self.gsk_generator = GSKGenerator(self.data_provider, self.output, self.common, self.set_up_template)
         self.blocking_generator = Block(self.data_provider)
+        self.assortment = self.gsk_generator.get_assortment_data_provider()
+        self.store_info = self.data_provider['store_info']
+        self.ps_data_provider = PsDataProvider(self.data_provider, self.output)
+        self.targets = self.ps_data_provider.get_kpi_external_targets()
 
     def main_calculation(self, *args, **kwargs):
         """
         This function calculates the KPI results.
         """
 
-        self.brand_blocking()
-
-        # assortment_store_dict = self.gsk_generator.availability_store_function()
+        self.get_store_target()# choosing the policy
+        if self.targets.empty:
+            Log.warning('There is no target policy matching this store ')
+        else:
+            self.brand_blocking()
+        assortment_store_dict = self.gsk_generator.availability_store_function()
         # self.common.save_json_to_new_tables(assortment_store_dict)
         #
         # assortment_category_dict = self.gsk_generator.availability_category_function()
@@ -91,15 +100,45 @@ class GSKJPToolBox:
         # self.common.commit_results_data()
         return
 
+    def position_shelf(self):
+
+        brand_fk = 2
+        policy = self.targets[self.targets['brand_fk'] == brand_fk]
+        if policy.empty:
+            Log.warning('There is no target policy matching brand')  # adding branf name
+            return
+        shelf_from_bottom = policy['shelf']
+        threshold = policy['position_target']
+        target = 0.25
+        df = pd.merge(self.match_product_in_scene, self.all_products, on="product_fk")
+        df = df[df['stacking_layer'] == 1]
+        brand_df = df[df['brand_fk'] == brand_fk]
+        shelf_df = brand_df[brand_df['shelf_number_from_bottom'].isin(shelf_from_bottom)]
+        result = shelf_df.shape[0] / brand_df.shape[0]
+        score = target if result >= threshold else 0
+
+        # results_df.append({'fk': kpi_product_pk, 'numerator_id': prod, 'denominator_id': denominator_fk,
+        #                'denominator_result': result, 'numerator_result': 1, 'result':
+        #                    result, 'score': score, 'target': kpi_target, 'identifier_parent': identifier_parent,
+        #                'should_enter': True})
+
+
+
     def brand_blocking(self):
 
-        results_df=[]
+        results_df = []
         df_block = self.scif
         brands = df_block['brand_fk'].dropna().unique()
-        template_name = 'Oral Main Shelf'# figure out which template name should I use
+        template_name = 'Oral Main Shelf' # figure out which template name should I use
         stacking_param = True# false
-        product_filters = {'product_type': ['POS', 'Empty', 'Irrelevant']}
+        target = 1
+        kpi_target = 0.25
+        product_filters = {'product_type': ['POS', 'Empty', 'Irrelevant']} # from Data file
         for brand in brands:
+            policy = self.targets[self.targets['brand_fk'] == brand]
+            if policy.empty:
+                Log.warning('There is no target policy matching brand')# adding branf name
+                continue
             result = self.blocking_generator.network_x_block_together(location={'template_name': template_name},
                                 population={'brand_fk': [brand]},
                                 additional={'minimum_block_ratio': 1,
@@ -107,12 +146,32 @@ class GSKJPToolBox:
                                             'calculate_all_scenes': False,
                                             'include_stacking': stacking_param,
                                             'check_vertical_horizontal': True,
-                                            'minimum_facing_for_block': 1})
+                                            'minimum_facing_for_block': policy['block_target']})
+            if result[result['is_block'] == True].empty:
+                result = 0
+            score = result * kpi_target
 
             # results_df.append({'fk': kpi_product_pk, 'numerator_id': prod, 'denominator_id': denominator_fk,
-            #                'denominator_result': results[1], 'numerator_result': results[0], 'result':
-            #                    results[2], 'score': results[3], 'identifier_parent': identifier_level_3,
-            #                'should_enter': True})
+            #                        'denominator_result': result, 'numerator_result': 1, 'result':
+            #                            result, 'score': score, 'target': kpi_target, 'identifier_parent': identifier_parent,
+            #                        'should_enter': True})
+
+    def PLN_MSL_score(self):
+        if self.assortment is None:
+            return
+        lvl3_assort = self.assortment.calculate_lvl3_assortment()
+        lvl3_assort ### merge lvl3 
+
+    def get_store_target(self):
+
+        parameters = ['additional_attribute_1', 'additional_attribute_2', 'store_name', 'adress_city', 'region_fk']
+        for param in parameters:
+            if param in self.targets.columns:
+                self.targets = self.targets[(self.targets[param] == self.store_info[param][0].encode('utf-8')) |
+                                            (self.targets[param] == '')]
+
+
+
 
 
     # def filter_df(self):
