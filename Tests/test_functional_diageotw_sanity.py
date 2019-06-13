d7c7341911c46099a1dc6b71617dd352a52a730d
avrahamA <avrahama@traxretail.com>
2019-06-13 13:15:59 +0300

    add test files 4 diageo tw,gh,co

---
+++ Tests/test_functional_diageotw_sanity.py
@@ -0,0 +1,50 @@
+
+import os
+import MySQLdb
+
+from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
+from Trax.Data.Testing.SeedNew import Seeder
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Trax.Cloud.Services.Connector.Keys import DbUsers
+from Trax.Data.Testing.TestProjects import TestProjectsNames
+
+from Tests.Data.TestData.test_data_diageotw_sanity import ProjectsSanityData
+from Projects.DIAGEOTW.Calculations import DIAGEOTWCalculations
+from Trax.Apps.Core.Testing.BaseCase import TestFunctionalCase
+
+
+__author__ = 'avrahama'
+
+
+class TestKEngineOutOfTheBox(TestFunctionalCase):
+
+    @property
+    def import_path(self):
+        return 'Trax.Apps.Services.KEngine.Handlers.SessionHandler'
+    
+    @property
+    def config_file_path(self):
+        return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'k-engine-test.config')
+    
+    seeder = Seeder()
+    
+    def _assert_kpi_results_filled(self):
+        connector = PSProjectConnector(TestProjectsNames().TEST_PROJECT_1, DbUsers.Docker)
+        cursor = connector.db.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('''
+        SELECT * FROM report.kpi_results
+        ''')
+        kpi_results = cursor.fetchall()
+        self.assertNotEquals(len(kpi_results), 0)
+        connector.disconnect_rds()
+    
+    @seeder.seed(["diageotw_seed"], ProjectsSanityData())
+    def test_diageotw_sanity(self):
+        project_name = ProjectsSanityData.project_name
+        data_provider = KEngineDataProvider(project_name)
+        sessions = ['E9C9D024-5CD2-46F1-A759-2E527207B161']
+        for session in sessions:
+            data_provider.load_session_data(session)
+            output = Output()
+            DIAGEOTWCalculations(data_provider, output).run_project_calculations()
+            self._assert_kpi_results_filled()
