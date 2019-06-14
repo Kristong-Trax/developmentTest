3ae9d27374dbe89d0867d2135b4fcdb405ccdbf2
nidhinb <nidhinb@traxretail.com>
2019-06-14 10:46:34 +0800

    adding sand instead of prod

--- Projects/TWEGAU_SAND/Tests/test_unit_twegau.py
+++ Projects/TWEGAU_SAND/Tests/test_unit_twegau.py
@@ -3,7 +3,7 @@ from Trax.Utils.Conf.Configuration import Config
 from Trax.Utils.Testing.Case import TestCase
 from mock import MagicMock, mock
 import pandas as pd
-from Projects.TWEGAU.Utils.KPIToolBox import TWEGAUToolBox
+from Projects.TWEGAU_SAND.Utils.KPIToolBox import TWEGAUToolBox
 
 
 __author__ = 'nidhin'
@@ -11,7 +11,7 @@ __author__ = 'nidhin'
 
 class TestTWEGAU(TestCase):
 
-    @mock.patch('Projects.TWEGAU.Utils.KPIToolBox.ProjectConnector')
+    @mock.patch('Projects.TWEGAU_SAND.Utils.KPIToolBox.ProjectConnector')
     def setUp(self, x):
         Config.init('')
         self.data_provider_mock = MagicMock()
