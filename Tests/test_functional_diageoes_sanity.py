9779cc48456a641aad0bfe273d82736a8d053d9f
avrahamA <avrahama@traxretail.com>
2019-06-16 17:27:08 +0300

    create sanity test for diageoes

--- Tests/test_functional_diageoes_sanity.py
+++ Tests/test_functional_diageoes_sanity.py
@@ -7,16 +7,26 @@ from Trax.Data.Testing.SeedNew import Seeder
 from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
 from Trax.Cloud.Services.Connector.Keys import DbUsers
 from Trax.Data.Testing.TestProjects import TestProjectsNames
+from mock import patch
 
+from Tests.Data.Templates.diageomx.MPA import mpa
+from Tests.Data.Templates.diageomx.NewProducts import products
+from Tests.Data.Templates.diageomx.POSM import posm
+from Tests.Data.Templates.diageomx.RelativePosition import position
 from Tests.Data.TestData.test_data_diageoes_sanity import ProjectsSanityData
 from Projects.DIAGEOES.Calculations import DIAGEOESCalculations
 from Trax.Apps.Core.Testing.BaseCase import TestFunctionalCase
 
+from Tests.TestUtils import remove_cache_and_storage
 
 __author__ = 'avrahama'
 
 
 class TestKEngineOutOfTheBox(TestFunctionalCase):
+
+    def set_up(self):
+        super(TestKEngineOutOfTheBox, self).set_up()
+        remove_cache_and_storage()
 
     @property
     def import_path(self):
@@ -32,14 +42,26 @@ class TestKEngineOutOfTheBox(TestFunctionalCase):
         connector = PSProjectConnector(TestProjectsNames().TEST_PROJECT_1, DbUsers.Docker)
         cursor = connector.db.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('''
-        SELECT * FROM report.kpi_results
+        SELECT * FROM report.kpi_level_2_results
         ''')
         kpi_results = cursor.fetchall()
         self.assertNotEquals(len(kpi_results), 0)
         connector.disconnect_rds()
-    
-    @seeder.seed(["diageoes_seed"], ProjectsSanityData())
-    def test_diageoes_sanity(self):
+
+    @patch('KPIUtils.DIAGEO.ToolBox.DIAGEOToolBox.get_latest_directory_date_from_cloud',
+           return_value='2018-05-18')
+    @patch('KPIUtils.DIAGEO.ToolBox.DIAGEOToolBox.save_latest_templates')
+    @patch('KPIUtils.DIAGEO.ToolBox.DIAGEOToolBox.download_template',
+           return_value=mpa)
+    @patch('KPIUtils.DIAGEO.ToolBox.DIAGEOToolBox.download_template',
+           return_value=products)
+    @patch('KPIUtils.DIAGEO.ToolBox.DIAGEOToolBox.download_template',
+           return_value=position)
+    @patch('KPIUtils.DIAGEO.ToolBox.DIAGEOToolBox.download_template',
+           return_value=posm)
+
+    @seeder.seed(["mongodb_products_and_brands_seed", "diageoes_seed"], ProjectsSanityData())
+    def test_diageoes_sanity(self, x, y, json, json2, json3, json4):
         project_name = ProjectsSanityData.project_name
         data_provider = KEngineDataProvider(project_name)
         sessions = ['F9E5B557-84D2-4334-9814-6B972FA950AF']
