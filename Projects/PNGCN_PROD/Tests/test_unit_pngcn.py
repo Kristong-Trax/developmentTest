b31b2ca475117a5d5f0c2e7770b16fe54252f4ad
avrahamA <avrahama@traxretail.com>
2019-06-18 09:33:41 +0300

    edut tests

--- Projects/PNGCN_PROD/Tests/test_unit_pngcn.py
+++ Projects/PNGCN_PROD/Tests/test_unit_pngcn.py
@@ -1,7 +1,7 @@
 from Trax.Utils.Testing.Case import TestUnitCase
-from mock import MagicMock
+from mock import MagicMock, Mock
 from Projects.PNGCN_PROD.SceneKpis.KPISceneToolBox import PngcnSceneKpis
-
+import pandas as pd
 __author__ = 'avrahama'
 
 class Test_PNGCN(TestUnitCase):
@@ -32,7 +32,16 @@ class Test_PNGCN(TestUnitCase):
         :return:
         """
         # mock: project_connector, common, scene_id
-        scene_tool_box = PngcnSceneKpis(self.ProjectConnector_mock, self.common_mock, 132487, self.data_provider_mock)
+        scene_tool_box = PngcnSceneKpis(self.ProjectConnector_mock, self.common_mock, 16588190, self.data_provider_mock)
+        data = [{'scene_fk': 101,'manufacturer_fk': 2,'product_fk': 252,'width_mm': 0.84 ,'width_mm_advance': 1.23},
+                {'scene_fk': 121,'manufacturer_fk': 4,'product_fk': 132,'width_mm': 0.80 ,'width_mm_advance': 0.99},
+                {'scene_fk': 201,'manufacturer_fk': 4,'product_fk': 152,'width_mm': 0.28 ,'width_mm_advance': 0.75},
+                {'scene_fk': 151,'manufacturer_fk': 5,'product_fk': 172,'width_mm': 0.95 ,'width_mm_advance': 0.15}
+        ]
+        scene_tool_box.get_filterd_matches = MagicMock(return_value=pd.DataFrame(data))
+        scene_tool_box.common.write_to_db_result = MagicMock()
+        scene_tool_box.calculate_linear_or_presize_linear_length('width_mm')
+        print scene_tool_box.common.write_to_db_result.mock_calls
 
         # kpi_fk = self.common.get_kpi_fk_by_kpi_name(PRESIZE_LINEAR_LENGTH_PER_LENGTH)
         # numerator = self.scif.width_mm.sum()  # get the width of P&G products in scene
@@ -40,7 +49,14 @@ class Test_PNGCN(TestUnitCase):
         # if denominator:
         #     score = numerator / denominator  # get the percentage of P&G products from all products
         #     self.common.write_to_db_result
-        a = scene_tool_box.common.get_kpi_fk_by_kpi_name
+
+
+
+
+        if scene_tool_box.calculate_linear_or_presize_linear_length('width_mm'):
+            print scene_tool_box.common.write_to_db_result.call_args_list
+        else:
+            print 'no display'
         print '*********************************************\n'
         denominator, numerator = 3, 5
 
