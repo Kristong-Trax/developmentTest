9aae1d84090a663a735f216b9f96d0ff29de5897
samkowalski <samk@traxretail.com>
2019-06-26 00:12:31 -0400

    conflict

--- Projects/RINIELSENUS/Calculation.py
+++ Projects/RINIELSENUS/Calculation.py
@@ -10,6 +10,7 @@ from Trax.Utils.Conf.Configuration import Config
 from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
 from Trax.Utils.Logging.Logger import Log
+import time
 
 
 __author__ = 'nethanel'
@@ -27,7 +28,8 @@ class MarsUsCalculations(BaseCalculationsScript):
 
 
 if __name__ == '__main__':
-    LoggerInitializer.init('TREX')
+    # LoggerInitializer.init('TREX')
+    LoggerInitializer.init('rinielsenus')
     Config.init()
     # docker_user = DbUsers.Docker
     # dbusers_class_path = 'Trax.Utils.Conf.Keys'
@@ -41,18 +43,27 @@ if __name__ == '__main__':
         # 'b3970bc2-de98-4dc5-9289-75cf9255e8d0',
         # '63a38dae-84da-4214-95b3-6cdd33c27869'
         # 'e8dab645-dd5f-4ab4-89d5-f17a0e4b6aab',
-        '2d1c224a-b413-4694-b9fa-5a5aa5198dcd'
-
+        # '2d1c224a-b413-4694-b9fa-5a5aa5198dcd'
+        # 'c768c2fb-d616-4bd6-81d1-b283039b8133',
+        # '79680014-b66b-43e4-8ccc-f25b752aecff'
+        # 'e8d50c6c-a7f8-401a-a4d4-149415365cf7'
     ]
+    sessions = [
+                # 'e8dab645-dd5f-4ab4-89d5-f17a0e4b6aab',
+                '2d1c224a-b413-4694-b9fa-5a5aa5198dcd'
+                ]
 
     # sessions = pd.read_csv('/home/Ilan/Documents/projects/marus/0612_batch_300.csv')['session_uid'].tolist()[:25]
-    # sessions = ['00471ecc-4bed-4d13-ade2-01ca204d7fad']
+    # sessions = ['50271382-e2ae-46cf-82b3-f14651a68869']
     for session in sessions:
+        print
         print('*******************************************************************')
         print('--------------{}-------------'.format(session))
+        s = time.time()
         Log.info('starting session : {}'.format(session))
         data_provider = KEngineDataProvider(project_name)
         # session = Common(data_provider).get_session_id(session)
         data_provider.load_session_data(session)
         output = Output()
         MarsUsCalculations(data_provider, output).run_project_calculations()
+        print('session took {} minutes to calculate'.format((time.time() - s)/60.0))
