30eb6deea223a8d68ed803511cf8ef281058ff30
avrahamA <avrahama@traxretail.com>
2019-06-27 13:23:41 +0300

    update the tests format and add documentation

--- Projects/DIAGEOKE/Tests/test_functional_diageoke.py
+++ Projects/DIAGEOKE/Tests/test_functional_diageoke.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 import MySQLdb
 from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
@@ -24,6 +23,8 @@ class TestDiageoke(TestFunctionalCase):
         self.output = Output()
         self.mock_object('save_latest_templates', path='KPIUtils.DIAGEO.ToolBox.DIAGEOToolBox')
         self.session_uid = '08e4dbd4-9270-4352-a68b-ca27e7853de6'
+        self.data_provider = KEngineDataProvider(self.project_name)
+        self.data_provider.load_session_data(self.session_uid)
         remove_cache_and_storage()
 
     @property
@@ -42,37 +43,39 @@ class TestDiageoke(TestFunctionalCase):
 
     @seeder.seed(["mongodb_products_and_brands_seed", "diageoke_seed"], ProjectsSanityData())
     def test_diageoke_sanity(self):
-        data_provider = KEngineDataProvider(self.project_name)
-        data_provider.load_session_data(self.session_uid)
-        DIAGEOKECalculations(data_provider, self.output).run_project_calculations()
+        """
+        basic sanity test, check if we write results after runing the KPIs
+        """
+        DIAGEOKECalculations(self.data_provider, self.output).run_project_calculations()
         self._assert_kpi_results_filled()
 
     @seeder.seed(["mongodb_products_and_brands_seed", "diageoke_seed"], ProjectsSanityData())
     def test_get_match_display(self):
-        data_provider = KEngineDataProvider(self.project_name)
-        data_provider.load_session_data(self.session_uid)
-        tool_box = DIAGEOKEToolBox(data_provider, self.output)
+        """
+        test the resulte type
+        """
+        tool_box = DIAGEOKEToolBox(self.data_provider, self.output)
         result = tool_box.get_match_display()
         expected_result = pd.DataFrame
         self.assertIsInstance(result, expected_result)
 
     @seeder.seed(["mongodb_products_and_brands_seed", "diageoke_seed"], ProjectsSanityData())
     def test_get_kpi_static_data(self):
-        data_provider = KEngineDataProvider(self.project_name)
-        data_provider.load_session_data(self.session_uid)
-        tool_box = DIAGEOKEToolBox(data_provider, self.output)
+        """
+        test the resulte type
+        """
+        tool_box = DIAGEOKEToolBox(self.data_provider, self.output)
         result = tool_box.get_kpi_static_data()
         expected_result = pd.DataFrame
         self.assertIsInstance(result, expected_result)
 
     @seeder.seed(["mongodb_products_and_brands_seed", "diageoke_seed"], ProjectsSanityData())
     def test_main_calculation(self):
-        data_provider = KEngineDataProvider(self.project_name)
-        data_provider.load_session_data(self.session_uid)
-        tool_box = DIAGEOKEToolBox(data_provider, self.output)
+        """
+        test that the resulte is None
+        """
+        tool_box = DIAGEOKEToolBox(self.data_provider, self.output)
         set_names = tool_box.kpi_static_data['kpi_set_name'].unique().tolist()
         tool_box.kpi_static_data = tool_box.get_kpi_static_data()
         result = tool_box.main_calculation(set_names)
         self.assertIsNone(result, "Did Not returned None")
-
-
