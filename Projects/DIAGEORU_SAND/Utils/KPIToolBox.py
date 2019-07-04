0cac4da3ad974874918f91c63e4df0d4fe37349a
idanre <idanr@traxretail.com>
2019-07-04 15:22:47 +0300

    Adding RU as well to MENUS PROS & SAND

--- Projects/DIAGEORU_SAND/Utils/KPIToolBox.py
+++ Projects/DIAGEORU_SAND/Utils/KPIToolBox.py
@@ -110,6 +110,11 @@ class DIAGEORUToolBox:
         assortment_res_dict_v3 = self.diageo_generator.diageo_global_assortment_function_v3()
         self.commonV2.save_json_to_new_tables(assortment_res_dict_v3)
 
+        # Global Menu kpis
+        menus_res_dict = self.diageo_generator.diageo_global_share_of_menu_cocktail_function(
+            cocktail_product_level=True)
+        self.commonV2.save_json_to_new_tables(menus_res_dict)
+
         for set_name in set_names:
             set_score = 0
             if set_name not in self.tools.KPI_SETS_WITHOUT_A_TEMPLATE and set_name not in self.set_templates_data.keys():
