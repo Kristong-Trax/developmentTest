68b0580d25dc93c53a885dc0dac71d358d2ee58a
avrahamA <avrahama@traxretail.com>
2019-07-08 10:41:06 +0300

    mocking objects

--- Projects/CCBR_PROD/Tests/test_unit_ccbr.py
+++ Projects/CCBR_PROD/Tests/test_unit_ccbr.py
@@ -1,6 +1,7 @@
 import pandas as pd
-from Trax.Utils.Testing.Case import TestUnitCase
+from Trax.Utils.Testing.Case import TestUnitCase, MagicMock
 from Tests.TestUtils import remove_cache_and_storage
+from Projects.CCBR_PROD.Utils.KPIToolBox import CCBRToolBox
 
 __author__ = 'avrahama'
 
@@ -15,5 +16,21 @@ class TestCCCBR(TestUnitCase):
         super(TestCCCBR, self).set_up()
         remove_cache_and_storage()
 
+        # mock PSProjectConnector
+        self.ProjectConnector_mock = self.mock_object(
+            'Common', path='KPIUtils.DB.Common')
+        self.ProjectConnector_mock = self.mock_object(
+            'PSProjectConnector', path='KPIUtils_v2.DB.PsProjectConnector')
+        self.ProjectConnector_mock = self.mock_object(
+            'PsDataProvider', path='KPIUtils_v2.GlobalDataProvider.PsDataProvider')
+
+        # mock 'data provider' and object
+        self.data_provider_mock = MagicMock()
+        self.output_mock = MagicMock()
+
+        self.tool_box = CCBRToolBox(self.data_provider_mock, self.output_mock)
+
+
     def test_test(self):
+
         pass
