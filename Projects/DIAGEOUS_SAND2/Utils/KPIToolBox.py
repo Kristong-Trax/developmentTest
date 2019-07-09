8780b1853e8a439c94c9b8c65425b225d31109a8
elyashivm <elyashivm@traxretail.com>
2019-07-09 09:35:52 +0300

    diageous delete hierarchy

--- Projects/DIAGEOUS_SAND2/Utils/KPIToolBox.py
+++ Projects/DIAGEOUS_SAND2/Utils/KPIToolBox.py
@@ -3,7 +3,7 @@ import pandas as pd
 import json
 from Trax.Algo.Calculations.Core.DataProvider import Data
 from Trax.Utils.Logging.Logger import Log
-from Projects.DIAGEOUS.Utils.Const import Const
+from Projects.DIAGEOUS_SAND2.Utils.Const import Const
 from KPIUtils_v2.DB.CommonV2 import Common
 from KPIUtils_v2.Calculations.AssortmentCalculations import Assortment
 from KPIUtils_v2.Calculations.SurveyCalculations import Survey
