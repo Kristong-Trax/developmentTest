70efc9d380f60b62e516f8aa4eaa06bf8d660482
avrahamA <avrahama@traxretail.com>
2019-07-11 17:18:05 +0300

    decode a string

--- Projects/CCBR_PROD/Utils/KPIToolBox.py
+++ Projects/CCBR_PROD/Utils/KPIToolBox.py
@@ -162,18 +162,12 @@ class CCBRToolBox:
         row = self.survey_sheet.loc[
             self.survey_sheet[Const.ENGLISH_KPI_NAME].str.encode("utf8") == atomic_name.encode("utf8")]
 
-
-
         if row.empty:
             Log.warning("Dataframe is empty, wrong kpi name: " + atomic_name.encode("utf8"))
             return
 
         store_type_filter = self.store_info['store_type'].values[0].strip()
         store_type_template = row[Const.STORE_TYPE_TEMPLATE].values[0].strip()
-
-        if atomic_name.encode("utf8") in  ['Foi poss\\u00edvel realizar a pesquisa?',
-                                           'Caso negativo, qual foi o motivo que impediu de realizar a visita?']:
-            print atomic_name.encode("utf8")
 
         # if cell in template is not empty
         if store_type_template != "":
@@ -207,7 +201,7 @@ class CCBRToolBox:
         try:
             atomic_pk = self.common_db.get_kpi_fk_by_kpi_name_new_tables(atomic_name)
         except IndexError:
-            Log.warning("There is no matching Kpi fk for kpi name: " + atomic_name)
+            Log.warning("There is no matching Kpi fk for kpi name: " + atomic_name.encode("utf8"))
             return
 
         self.write_to_db_result_new_tables(fk=atomic_pk, numerator_id=self.session_id, numerator_result=survey_result,
@@ -230,7 +224,7 @@ class CCBRToolBox:
         try:
             atomic_pk = self.common_db.get_kpi_fk_by_kpi_name_new_tables(atomic_name)
         except IndexError:
-            Log.warning("There is no matching Kpi fk for kpi name: " + atomic_name)
+            Log.warning("There is no matching Kpi fk for kpi name: " + atomic_name.encode("utf8"))
             return
         for index, row in row.iterrows():
             sum_of_count, target, count_result = self.handle_count_row(row)
