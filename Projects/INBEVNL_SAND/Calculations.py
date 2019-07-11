f9c04414512f260076f2d5ecf76d2351e266d8a7
natalya_k <natalyak@traxretail.com>
2019-07-11 11:33:19 +0300

    copy code except poce

--- Projects/INBEVNL_SAND/Calculations.py
+++ Projects/INBEVNL_SAND/Calculations.py
@@ -1,11 +1,12 @@
-
+import os
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-# from Trax.Algo.Calculations.Core.DataProvider import ACEDataProvider, Output, KEngineDataProvider
-# from Trax.Utils.Conf.Configuration import Config
-# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-# from mock import MagicMock
+from Trax.Algo.Calculations.Core.DataProvider import ACEDataProvider, Output, KEngineDataProvider
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+from mock import MagicMock
 
 from Projects.INBEVNL_SAND.KPIGenerator import INBEVNL_SANDINBEVBEGenerator
+
 
 __author__ = 'urid'
 
@@ -13,16 +14,18 @@ __author__ = 'urid'
 class INBEVNL_SANDINBEVBECalculations(BaseCalculationsScript):
     def run_project_calculations(self):
         self.timer.start()
-        INBEVNL_SANDINBEVBEGenerator(self.data_provider, self.output).main_function()
+        TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'INBEVNL_SAND', 'Data',
+                                     'POCE_Template.xlsx')
+        INBEVNL_SANDINBEVBEGenerator(self.data_provider, self.output, TEMPLATE_PATH).main_function()
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-# if __name__ == '__main__':
-#     LoggerInitializer.init('inbevbe calculations')
-#     Config.init()
-#     project_name = 'inbevnl-sand'
-#     data_provider = KEngineDataProvider(project_name, monitor=MagicMock())
-#     session = 'F2EC07FB-3863-4337-8764-1BE5F8A573B2'
-#     data_provider.load_session_data(session)
-#     output = Output()
-#     INBEVNL_SANDINBEVBECalculations(data_provider, output).run_project_calculations()
+if __name__ == '__main__':
+    LoggerInitializer.init('inbevnl-sand calculations')
+    Config.init()
+    project_name = 'inbevnl-sand'
+    data_provider = KEngineDataProvider(project_name, monitor=MagicMock())
+    session = '4b4733e9-11ea-497c-9b77-a04899cd65cd'
+    data_provider.load_session_data(session)
+    output = Output()
+    INBEVNL_SANDINBEVBECalculations(data_provider, output).run_project_calculations()
