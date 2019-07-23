392c5f184e94726d0d9633b27704a4dcbe57f7ce
Hunter Young <huntery@traxretail.com>
2019-07-22 21:38:25 -0400

    Updated template again

--- Projects/CCBOTTLERSUS/MSC/Data/Const.py
+++ Projects/CCBOTTLERSUS/MSC/Data/Const.py
@@ -5,7 +5,7 @@ __author__ = 'hunter'
 
 class Const(object):
     TEMPLATE_PATH = os.path.join(os.path.dirname(
-        os.path.realpath(__file__)), 'MSC Template v11.xlsx')
+        os.path.realpath(__file__)), 'MSC Template v12.xlsx')
     MSC = 'Market Street Challenge'
     MANUFACTURER_FK = 1
 
