2e57863a1bd442e100b435dec38d38195c13d093
avrahamA <avrahama@traxretail.com>
2019-07-23 11:06:08 +0300

    adding new KPIs for diageoPL

--- Projects/DIAGEOPL/Utils/KPIToolBox.py
+++ Projects/DIAGEOPL/Utils/KPIToolBox.py
@@ -16,9 +16,16 @@ class DIAGEOPLToolBox:
 
     def main_calculation(self):
         """ This function calculates the KPI results """
+        # SOS Out Of The Box kpis
+        self.diageo_generator.activate_ootb_kpis(self.commonV2)
+
         # Global assortment kpis
         assortment_res_dict = self.diageo_generator.diageo_global_assortment_function_v2()
         self.commonV2.save_json_to_new_tables(assortment_res_dict)
+
+        # Global assortment kpis - v3 for NEW MOBILE REPORTS use
+        assortment_res_dict_v3 = self.diageo_generator.diageo_global_assortment_function_v3()
+        self.commonV2.save_json_to_new_tables(assortment_res_dict_v3)
 
         # Global Menu kpis
         menus_res_dict = self.diageo_generator.diageo_global_share_of_menu_cocktail_function(
