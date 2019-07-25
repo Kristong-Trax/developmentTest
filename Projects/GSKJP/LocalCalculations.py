7daab6b318746d3cdd18a6d0cff33e3a2d599dfe
limor_c <limorc@traxretail.com>
2019-07-25 14:25:59 +0300

    searching in all scenes block adding numerator denominator to block calculation

--- Projects/GSKJP/LocalCalculations.py
+++ Projects/GSKJP/LocalCalculations.py
@@ -5,12 +5,12 @@ from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 from Projects.GSKJP.Calculations import Calculations
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('gskjp calculations')
-    Config.init()
-    project_name = 'gskjp'
-    data_provider = KEngineDataProvider(project_name)
-    session ='92C9B9CE-CD2C-4259-9035-D4DA5301168E'
-    data_provider.load_session_data(session)
-    output = Output()
-    Calculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('gskjp calculations')
+#     Config.init()
+#     project_name = 'gskjp'
+#     data_provider = KEngineDataProvider(project_name)
+#     session ='92C9B9CE-CD2C-4259-9035-D4DA5301168E'
+#     data_provider.load_session_data(session)
+#     output = Output()
+#     Calculations(data_provider, output).run_project_calculations()
