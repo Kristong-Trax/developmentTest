68c449c6d676596e86a77a9403840a058f89349c
sergeypol <sergey@traxretail.com>
2019-07-30 12:00:28 +0300

    PROS-9738 - CCRU - New POS 2019 KPIs

--- Projects/CCRU/LocalCalculations.py
+++ Projects/CCRU/LocalCalculations.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     project_name = 'ccru'
     data_provider = KEngineDataProvider(project_name)
     session_uids = [
-        'a77c11de-34f1-414f-8600-166809f475ae'
+        'A7BBB2AF-51ED-4911-907D-9B99639D6F51'
 
         # SAND
         # '3a3e96ee-3dfa-47f2-8cdb-6226756f62f8',
--- Projects/CCRU/Utils/session_batches.py
+++ Projects/CCRU/Utils/session_batches.py
@@ -54,9 +54,13 @@ class CCRUSessionBatches:
         query = """
                 SELECT ss.visit_date, ss.session_uid, ss.number_of_scenes
                 FROM probedata.session ss
-                WHERE visit_type_fk<>2
+                JOIN report.kps_results ksr ON ksr.session_uid=ss.session_uid
+                JOIN static.kpi_set ks ON ks.pk=ksr.kpi_set_fk
+                WHERE ss.number_of_scenes > 0
+                AND visit_type_fk<>2
                 AND ss.visit_date >= '{}' AND ss.visit_date <= '{}'
                 AND delete_time is NULL AND status='Completed'
+                AND ks.name LIKE 'PoS 2019 - MT%'
                 ORDER BY ss.pk DESC;
                 """.format(START_DATE, END_DATE)
 
