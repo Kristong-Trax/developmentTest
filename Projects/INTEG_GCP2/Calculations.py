b0179157000aa64c82573ac4af4511765458262f
elisuiza <elis@traxretail.com>
2019-08-07 16:06:41 +0300

    Create integ_gcp2 and refer to PepsicoUK

---
+++ Projects/INTEG_GCP2/Calculations.py
@@ -0,0 +1,14 @@
+from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
+from Projects.PEPSICOUK.KPIGenerator import Generator
+
+__author__ = 'eli'
+
+
+class PEPSICOUKCalculations(BaseCalculationsScript):
+    def run_project_calculations(self):
+        self.timer.start()
+        Generator(self.data_provider, self.output).main_function()
+        self.timer.stop('KPIGenerator.run_project_calculations')
+
+
+
