3573ea811b8f2effe904c6211cb8e22f691cd07e
samkowalski <samk@traxretail.com>
2019-08-15 14:12:35 -0400

    shelf norm test

--- Projects/RINIELSENUS/Calculation.py
+++ Projects/RINIELSENUS/Calculation.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
         'a0036e16-6e0d-49ad-abfb-91f0a35d7cd9',
     ]
 
-    sessions = ['617cc83f-ebbd-4d23-b9bf-12dbea44ed1e']
+    sessions = ['0742d9f5-837b-4947-b871-ee7b62a1a6dc']
 
 
     for session in sessions:
