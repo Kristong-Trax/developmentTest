e6dc7d4d71bcdb47a717eb2f28136142c473d23d
sergeypol <sergey@traxretail.com>
2019-08-22 18:47:02 +0300

    CCRU - change log info to log debug - copy to SAND

--- Projects/CCRU_SAND/LocalCalculations.py
+++ Projects/CCRU_SAND/LocalCalculations.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     project_name = 'ccru_sand'
     data_provider = KEngineDataProvider(project_name)
     session_uids = [
-        'dae3d9a0-9602-45fe-8cda-c57d41c6aeb4',
+        '7947cb7b-16c0-4bf8-8163-d8ae9ee41d1a'
 
         # SAND
         # '3a3e96ee-3dfa-47f2-8cdb-6226756f62f8',
---
+++ Projects/CCRU_SAND/Utils/Consts.py
@@ -0,0 +1,28 @@
+__author__ = 'sergey'
+
+
+class CCRU_SANDConsts(object):
+    ALLOWED_POS_SETS = (
+        'PoS 2019 - FT - CAP',
+        'PoS 2019 - FT NS - CAP',
+        'PoS 2019 - FT NS - REG',
+        'PoS 2019 - FT - REG',
+        'PoS 2019 - IC Canteen - EDU',
+        'PoS 2019 - IC Canteen - OTH',
+        'PoS 2019 - IC Cinema - CAP',
+        'PoS 2019 - IC Cinema - REG',
+        'PoS 2019 - IC FastFood',
+        'PoS 2019 - IC HoReCa BarTavernClub',
+        'PoS 2019 - IC HoReCa RestCafeTea',
+        'PoS 2019 - IC Petroleum - CAP',
+        'PoS 2019 - IC Petroleum - REG',
+        'PoS 2019 - IC QSR',
+        'PoS 2019 - MT Conv Big - CAP',
+        'PoS 2019 - MT Conv Big - REG',
+        'PoS 2019 - MT Conv Small - CAP',
+        'PoS 2019 - MT Conv Small - REG',
+        'PoS 2019 - MT Hypermarket - CAP',
+        'PoS 2019 - MT Hypermarket - REG',
+        'PoS 2019 - MT Supermarket - CAP',
+        'PoS 2019 - MT Supermarket - REG',
+    )
