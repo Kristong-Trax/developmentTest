832157dc2f82704377260c502172c25291aecb54
elyashivm <elyashivm@traxretail.com>
2019-08-28 09:06:31 +0300

    create new project changes

--- DevloperTools/ProjectCreatorNew/copy_project_from_other_project.py
+++ DevloperTools/ProjectCreatorNew/copy_project_from_other_project.py
@@ -13,7 +13,7 @@ class CopyCodeToNewProject:
         if self.src_project != 'DUMMY' and not self.validate_projects():
             print "Try using create_new_project.py instead!"
         self.projects_path = "{}/Projects/".format(
-            os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+            os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
         self.classes_names = {}
 
     def validate_projects(self):
@@ -32,12 +32,9 @@ class CopyCodeToNewProject:
         if os.path.exists(dest_path):
             shutil.rmtree(dest_path)
         shutil.copytree(src_path, dest_path)
-        # checking for the source project's references and replacing them, while gathering all the class names
-        self.change_new_project_files(dest_path, change_class_names=False)
-        # changing all the classes' names
-        # self.change_new_project_files(dest_path, change_class_names=True)
+        self.change_new_project_files(dest_path)
 
-    def change_new_project_files(self, path, change_class_names):
+    def change_new_project_files(self, path):
         if os.path.isdir(path):
             files = os.listdir(path)
         else:
@@ -48,53 +45,24 @@ class CopyCodeToNewProject:
         for file_name in files:
             file_path = path + file_name
             if os.path.isdir(file_path):
-                self.change_new_project_files(
-                    file_path + "/", change_class_names=change_class_names)
+                self.change_new_project_files(file_path + "/")
             else:
                 if len(file_name) > 3 and file_name[-3:] == ".py":
                     with open(file_path, 'r') as content:
-                        new_content, classes_names = self.change_file_references(content.read(),
-                                                                                 change_class_names=change_class_names)
-                        for class_name in classes_names:
-                            self.classes_names[class_name] = self.dest_project + \
-                                class_name.replace(self.src_project, "")
+                        new_content = self.change_file_references(content.read())
                     with open(file_path, 'w') as content:
                         content.write(new_content)
+            new_file_path = path + self.change_file_references(file_name)
+            os.rename(file_path, new_file_path)
 
-    def change_file_references(self, original_content, change_class_names):
-        new_content = []
-        classes_names = []
-        for line in original_content.split("\n"):
-            splitted_line = line.strip().split()
-            if change_class_names:
-                for class_name in self.classes_names.keys():
-                    line = line.replace(class_name, self.classes_names[class_name])
-            else:
-                if splitted_line and splitted_line[0] == "class":  # finding declaration of classes
-                    classes_names.append(splitted_line[1][:splitted_line[1].find("(")])
-
-                project_name_appearances = re.findall(
-                    "[\.\"\']{}[\.\"\']".format(self.src_project), line, re.IGNORECASE)
-                for appearance in list(set(project_name_appearances)):
-                    replacement = self.get_case_sensitive_replacement(appearance[1:-1])
-                    line = line.replace(appearance, "{}{}{}".format(
-                        appearance[0], replacement, appearance[-1]))
-            new_content.append(line)
-
-        new_content = "\n".join(new_content)
-        return new_content, classes_names
-
-    def get_case_sensitive_replacement(self, appearance):
-        if appearance == self.src_project.upper():
-            return self.dest_project.upper()
-        elif appearance == self.src_project.lower():
-            return self.dest_project.lower()
-        else:
-            return self.dest_project.capitalize()
+    def change_file_references(self, original_content):
+        new_content = original_content.replace(self.src_project, self.dest_project).replace(
+            self.src_project_name, self.dest_project_name)
+        return new_content
 
 
 if __name__ == '__main__':
-    source_project_name = "aaaaac-sand"
-    desiny_project_name = "aaaaac"
+    source_project_name = "batmx"
+    desiny_project_name = "batmx-sand2"
     a = CopyCodeToNewProject(source_project_name, desiny_project_name)
     a.copy_project()
