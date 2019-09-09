f3bf13df3039b5ce89bca9aca7a18cde48af52bf
samkowalski <samk@traxretail.com>
2019-09-09 18:05:52 -0400

    print bad

--- Projects/RINIELSENUS/Utils/Runner.py
+++ Projects/RINIELSENUS/Utils/Runner.py
@@ -70,8 +70,8 @@ class Results(object):
             #                         'What percent of non-negotiable items are shelved in the preferred range? - numerator'
             #                         ]:
             #     continue
-            print('~~~~~~~~~~~~~~~~~~~~****************~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-            print(atomic['atomic'])
+            # print('~~~~~~~~~~~~~~~~~~~~****************~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+            # print(atomic['atomic'])
             if sum([1 for i in atomic['depend_on'] if i is not None and i != '']):
                 # if atomic['kpi_type'] == 'PreCalc Vertical Block':
                 dependency_status = self._check_atomic_dependency(
@@ -104,7 +104,7 @@ class Results(object):
             if isinstance(kpi_res, tuple):
                 errata = [i for i in kpi_res[1:]]
                 kpi_res = kpi_res[0]
-            print('||||| Result for {} is: {}'.format(atomic['atomic'], kpi_res))
+            # print('||||| Result for {} is: {}'.format(atomic['atomic'], kpi_res))
 
             result = {'result': kpi_res,
                       'set': atomic['set'],
