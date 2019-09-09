f5e785dee35fca4b5502511865d5c4c0efb0c467
ilay_s <ilays@traxretail.com>
2019-09-09 10:01:07 +0300

    adding product_group_blocking kpi to inbevnl

--- Projects/INBEVNL/LocalCalculations.py
+++ Projects/INBEVNL/LocalCalculations.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     project_name = 'inbevnl'
     data_provider = KEngineDataProvider(project_name)
     sessions = [
-        '42dbd66f-3315-4d2d-baf0-cb7a25b8dd79'
+        '5c0baab1-5d5b-4a90-92de-6efe713ef37f',
+        # '2b201dbc-1780-4800-84c5-7a4378578149'
     ]
     for session in sessions:
         data_provider.load_session_data(session)
