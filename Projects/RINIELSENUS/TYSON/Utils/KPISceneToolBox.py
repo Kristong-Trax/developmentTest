c9a4ee149721e9d5ee671a4892e352da4b224654
kristati <krishnat@traxretail.com>
2019-09-23 21:13:56 -0400

    push

--- Projects/RINIELSENUS/TYSON/Utils/KPISceneToolBox.py
+++ Projects/RINIELSENUS/TYSON/Utils/KPISceneToolBox.py
@@ -1,4 +1,6 @@
 from Trax.Algo.Calculations.Core.DataProvider import Data
+from KPIUtils_v2.Utils.Consts.DataProvider import TemplatesConsts
+from KPIUtils_v2.Utils.Consts.DataProvider import ScifConsts
 
 # from KPIUtils_v2.Calculations.AssortmentCalculations import Assortment
 # from KPIUtils_v2.Calculations.AvailabilityCalculations import Availability
@@ -18,19 +20,8 @@ class TYSONToolBox:
         self.output = output
         self.data_provider = data_provider
         self.common = common
-        # self.project_name = self.data_provider.project_name
-        # self.session_uid = self.data_provider.session_uid
-        # #self.products = self.data_provider[Data.PRODUCTS]
-        # self.all_products = self.data_provider[Data.ALL_PRODUCTS]
-        # self.match_product_in_scene = self.data_provider[Data.MATCHES]
-        # self.visit_date = self.data_provider[Data.VISIT_DATE]
-        # self.session_info = self.data_provider[Data.SESSION_INFO]
-        # self.scene_info = self.data_provider[Data.SCENES_INFO]
-        # self.template_info = self.data_provider.all_templates
-        # self.store_id = self.data_provider[Data.STORE_FK]
 
         self.scif = self.data_provider[Data.SCENE_ITEM_FACTS]
-        # self.match_display_in_scene = self.data_provider._display_set_match_display_in_scene(self, level, uid)
         self.match_display_in_scene = self.data_provider.match_display_in_scene
 
     def calculate_linear_feet(self):
@@ -44,24 +35,25 @@ class TYSONToolBox:
         '''
 
         filtered_scif = self.scif[
-            ['scene_id', 'template_fk', 'template_name', 'product_fk', 'net_len_add_stack', 'net_len_ign_stack']]
+            [ScifConsts.SCENE_ID, ScifConsts.TEMPLATE_FK, ScifConsts.TEMPLATE_NAME, ScifConsts.PRODUCT_FK,
+             ScifConsts.NET_LEN_ADD_STACK, ScifConsts.NET_LEN_IGN_STACK]]
 
-        filtered_scif[filtered_scif['template_name'].isin(["Frozen Meat and Frozen Breakfast - Secondary Locations",
-                                                           'Frozen Meat and Frozen Breakfast - Main Aisle'])]
+        filtered_scif[
+            filtered_scif[ScifConsts.TEMPLATE_NAME].isin(["Frozen Meat and Frozen Breakfast - Secondary Locations",
+                                                         'Frozen Meat and Frozen Breakfast - Main Aisle'])]
         if filtered_scif.empty:
             return
 
-
-        filtered_scif = filtered_scif.dropna(subset=['net_len_add_stack'], how='all')
+        filtered_scif = filtered_scif.dropna(subset=[ScifConsts.NET_LEN_ADD_STACK], how='all')
 
         # Need to display name as the "bunker cooler" in display name determines
         # whether the linear feet is stacked or not
 
         if not self.match_display_in_scene.empty:
-            relevant_match_display_in_scene = self.match_display_in_scene[['scene_fk', 'display_name']]
-            scif_with_display_name = filtered_scif.merge(relevant_match_display_in_scene, left_on='scene_id',
-                                                         right_on='scene_fk')
-            scif_with_display_name = scif_with_display_name.drop(columns=['scene_fk', 'scene_id'])
+            relevant_match_display_in_scene = self.match_display_in_scene[[ScifConsts.SCENE_FK, 'display_name']]
+            scif_with_display_name = filtered_scif.merge(relevant_match_display_in_scene, left_on=ScifConsts.SCENE_ID,
+                                                         right_on=ScifConsts.SCENE_FK)
+            scif_with_display_name = scif_with_display_name.drop(columns=[ScifConsts.SCENE_FK, ScifConsts.SCENE_ID])
 
         else:
             scif_with_display_name = filtered_scif
@@ -74,15 +66,16 @@ class TYSONToolBox:
 
         scif_with_display_name.loc[scif_with_display_name['display_name'] != 'Bunker Cooler', 'final_linear_feet'] = \
             scif_with_display_name.loc[
-                scif_with_display_name['display_name'] != 'Bunker Cooler', 'net_len_ign_stack']
+                scif_with_display_name['display_name'] != 'Bunker Cooler', ScifConsts.NET_LEN_IGN_STACK]
 
-        scif_with_display_name = scif_with_display_name.drop(columns=['net_len_ign_stack', 'net_len_add_stack'])
+        scif_with_display_name = scif_with_display_name.drop(columns=[ScifConsts.NET_LEN_IGN_STACK,
+                                                                      ScifConsts.NET_LEN_ADD_STACK])
 
-        denominator_results = scif_with_display_name.groupby('template_fk', as_index=False)[
+        denominator_results = scif_with_display_name.groupby(ScifConsts.TEMPLATE_FK, as_index=False)[
             ['final_linear_feet']].sum().rename(columns={'final_linear_feet': 'denominator_result'})
 
         numerator_result = scif_with_display_name.groupby(
-            ['template_fk', 'product_fk'], as_index=False).sum().rename(
+            [ScifConsts.TEMPLATE_FK, ScifConsts.PRODUCT_FK], as_index=False).sum().rename(
             columns={'final_linear_feet': 'numerator_result'})
 
         results = numerator_result.merge(denominator_results)
