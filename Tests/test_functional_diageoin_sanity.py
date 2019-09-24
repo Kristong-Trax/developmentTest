baa7136515288ac394e86707b0c237281d781e1e
avrahamA <avrahama@traxretail.com>
2019-09-24 14:50:14 +0300

    new files and seed for sanity test

--- Tests/test_functional_diageoin_sanity.py
+++ Tests/test_functional_diageoin_sanity.py
@@ -1,56 +1,80 @@
-# import os
-# import MySQLdb
-#
-# from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
-# from Trax.Data.Testing.SeedNew import Seeder
-# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-# from Trax.Cloud.Services.Connector.Keys import DbUsers
-# from Trax.Data.Testing.TestProjects import TestProjectsNames
-#
-# from Tests.Data.TestData.test_data_diageoin_sanity import ProjectsSanityData
-# from Projects.DIAGEOIN.Calculations import DIAGEOINCalculations
-# from Trax.Apps.Core.Testing.BaseCase import TestFunctionalCase
-#
-# from Tests.TestUtils import remove_cache_and_storage
-#
-# __author__ = 'avrahama'
-#
-#
-# class TestKEngineOutOfTheBox(TestFunctionalCase):
-#
-#     def set_up(self):
-#         super(TestKEngineOutOfTheBox, self).set_up()
-#         self.mock_object('save_latest_templates', path='KPIUtils.DIAGEO.ToolBox.DIAGEOToolBox')
-#         self.mock_object('save_level2_and_level3', path='Projects.PENAFLORAR.Utils.KPIToolBox.PENAFLORARToolBox')
-#         remove_cache_and_storage()
-#
-#     @property
-#     def import_path(self):
-#         return 'Trax.Apps.Services.KEngine.Handlers.SessionHandler'
-#
-#     @property
-#     def config_file_path(self):
-#         return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'k-engine-test.config')
-#
-#     seeder = Seeder()
-#
-#     def _assert_kpi_results_filled(self):
-#         connector = PSProjectConnector(TestProjectsNames().TEST_PROJECT_1, DbUsers.Docker)
-#         cursor = connector.db.cursor(MySQLdb.cursors.DictCursor)
-#         cursor.execute('''
-#         SELECT * FROM report.kpi_level_2_results
-#         ''')
-#         kpi_results = cursor.fetchall()
-#         self.assertNotEquals(len(kpi_results), 0)
-#         connector.disconnect_rds()
-#
-#     @seeder.seed(["mongodb_products_and_brands_seed", "diageoin_seed"], ProjectsSanityData())
-#     def test_diageoin_sanity(self):
-#         project_name = ProjectsSanityData.project_name
-#         data_provider = KEngineDataProvider(project_name)
-#         sessions = ['86c4bf68-933b-4e94-8de6-ab8a7655a180']
-#         for session in sessions:
-#             data_provider.load_session_data(session)
-#             output = Output()
-#             DIAGEOINCalculations(data_provider, output).run_project_calculations()
-#             self._assert_kpi_results_filled()
+
+import os
+import MySQLdb
+
+from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
+from Trax.Data.Testing.SeedNew import Seeder
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Trax.Cloud.Services.Connector.Keys import DbUsers
+from Trax.Data.Testing.TestProjects import TestProjectsNames
+from Tests.Data.TestData.test_data_diageoin_sanity import ProjectsSanityData
+from Projects.DIAGEOIN.Calculations import DIAGEOINCalculations
+
+from Trax.Apps.Core.Testing.BaseCase import TestFunctionalCase
+from Tests.TestUtils import remove_cache_and_storage
+
+
+__author__ = 'avrahama'
+
+
+class TestKEngineOutOfTheBox(TestFunctionalCase):
+
+    def set_up(self):
+        super(TestKEngineOutOfTheBox, self).set_up()
+        remove_cache_and_storage()
+
+    @property
+    def import_path(self):
+        return 'Trax.Apps.Services.KEngine.Handlers.SessionHandler'
+    
+    @property
+    def config_file_path(self):
+        return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'k-engine-test.config')
+    
+    seeder = Seeder()
+    
+    def _assert_old_tables_kpi_results_filled(self):
+        connector = PSProjectConnector(TestProjectsNames().TEST_PROJECT_1, DbUsers.Docker)
+        cursor = connector.db.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('''
+        SELECT * FROM report.kpi_results
+        ''')
+        kpi_results = cursor.fetchall()
+        self.assertNotEquals(len(kpi_results), 0)
+        connector.disconnect_rds()
+        
+    def _assert_new_tables_kpi_results_filled(self):
+        connector = PSProjectConnector(TestProjectsNames().TEST_PROJECT_1, DbUsers.Docker)
+        cursor = connector.db.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('''
+        SELECT * FROM report.kpi_level_2_results
+        ''')
+        kpi_results = cursor.fetchall()
+        self.assertNotEquals(len(kpi_results), 0)
+        connector.disconnect_rds()
+    
+    def _assert_scene_tables_kpi_results_filled(self):
+        connector = PSProjectConnector(TestProjectsNames().TEST_PROJECT_1, DbUsers.Docker)
+        cursor = connector.db.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('''
+        SELECT * FROM report.scene_kpi_results
+        ''')
+        kpi_results = cursor.fetchall()
+        self.assertNotEquals(len(kpi_results), 0)
+        connector.disconnect_rds()
+    
+    @seeder.seed(["diageoin_seed", "mongodb_products_and_brands_seed"], ProjectsSanityData())
+    def test_diageoin_sanity(self):
+        project_name = ProjectsSanityData.project_name
+        data_provider = KEngineDataProvider(project_name)
+        sessions = {'db7a9f0f-76d6-4de4-b1a0-d644eb1e4024': []}
+        for session in sessions.keys():
+            data_provider.load_session_data(str(session))
+            output = Output()
+            DIAGEOINCalculations(data_provider, output).run_project_calculations()
+            # self._assert_old_tables_kpi_results_filled()
+            # self._assert_new_tables_kpi_results_filled()
+            # for scene in sessions[session]:
+            #     data_provider.load_scene_data(str(session), scene_id=scene)
+            #     SceneCalculations(data_provider).calculate_kpis()
+            #     self._assert_scene_tables_kpi_results_filled()
