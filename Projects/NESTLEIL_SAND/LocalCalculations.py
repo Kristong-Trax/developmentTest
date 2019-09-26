4fc10780cabebf415a5c3d907bb4e0416706ffc9
natalya_k <natalyak@traxretail.com>
2019-09-26 17:03:37 +0300

    changes to sand

--- Projects/NESTLEIL_SAND/LocalCalculations.py
+++ Projects/NESTLEIL_SAND/LocalCalculations.py
@@ -1,16 +1,16 @@
 
-# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-# from Trax.Utils.Conf.Configuration import Config
-# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-# from Projects.NESTLEIL_SAND.Calculations import Calculations
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+from Projects.NESTLEIL_SAND.Calculations import Calculations
 
 
-# if __name__ == '__main__':
-#     LoggerInitializer.init('nestleil-sand calculations')
-#     Config.init()
-#     project_name = 'nestleil-sand'
-#     data_provider = KEngineDataProvider(project_name)
-#     session = ''
-#     data_provider.load_session_data(session)
-#     output = Output()
-#     Calculations(data_provider, output).run_project_calculations()
+if __name__ == '__main__':
+    LoggerInitializer.init('nestleil-sand calculations')
+    Config.init()
+    project_name = 'nestleil-sand'
+    data_provider = KEngineDataProvider(project_name)
+    session = 'da29d8b5-bc5c-4b89-b5e1-8caa4cdfc6be'
+    data_provider.load_session_data(session)
+    output = Output()
+    Calculations(data_provider, output).run_project_calculations()
--- Projects/NESTLEIL_SAND/Utils/Consts.py
+++ Projects/NESTLEIL_SAND/Utils/Consts.py
@@ -12,10 +12,23 @@ class Consts(object):
     SOS_SKU_LVL_RENAME = {PRODUCT_FK: NUMERATOR_ID, IN_STORE: NUMERATOR_RESULT}
 
     # KPI names
-    DISTRIBUTION_STORE_LEVEL = 'Distribution'
-    DISTRIBUTION_SKU_LEVEL = 'Distribution - SKU'
-    OOS_STORE_LEVEL = 'OOS'
-    OOS_SKU_LEVEL = 'OOS - SKU'
+    # DISTRIBUTION_STORE_LEVEL = 'Distribution'
+    # DISTRIBUTION_SKU_LEVEL = 'Distribution - SKU'
+    # OOS_STORE_LEVEL = 'OOS'
+    # OOS_SKU_LEVEL = 'OOS - SKU'
+
+    DISTR_SNACKS = 'Distribution Snacks'
+    DISTR_SNACKS_SKU = 'Distribution Snacks - SKU'
+    OOS_SNACKS = 'OOS Snacks'
+    OOS_SNACKS_SKU = 'OOS Snacks - SKU'
+
+    DISTR_SABRA = 'Distribution Sabra'
+    DISTR_SABRA_SKU = 'Distribution Sabra - SKU'
+    OOS_SABRA = 'OOS Sabra'
+    OOS_SABRA_SKU = 'OOS Sabra - SKU'
+
+    DIST_OOS_KPIS_MAP = {DISTR_SNACKS: OOS_SNACKS, DISTR_SABRA: OOS_SABRA, DISTR_SNACKS_SKU: OOS_SNACKS_SKU,
+                         DISTR_SABRA_SKU: OOS_SABRA_SKU}
 
     # KPI result values
     OOS_VALUE = 'OOS'
@@ -23,3 +36,9 @@ class Consts(object):
 
     # Logs
     EMPTY_ASSORTMENT_DATA = "There isn't relevant assortment data for this visit"
+
+    #assortment
+    KPI_FK_LVL2 = 'kpi_fk_lvl2'
+    KPI_FK_LVL3 = 'kpi_fk_lvl3'
+    STORE_ASS_KPI_TYPE = 'store_lvl_kpi_type'
+    SKU_ASS_KPI_TYPE = 'sku_lvl_kpi_type'
