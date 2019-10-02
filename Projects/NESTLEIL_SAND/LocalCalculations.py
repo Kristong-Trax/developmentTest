09be3cb0d6ce4fa104df9ddcb9d03fb72b9eab76
natalya_k <natalyak@traxretail.com>
2019-10-02 13:46:19 +0300

    add ses

--- Projects/NESTLEIL_SAND/LocalCalculations.py
+++ Projects/NESTLEIL_SAND/LocalCalculations.py
@@ -10,7 +10,11 @@ if __name__ == '__main__':
     Config.init()
     project_name = 'nestleil-sand'
     data_provider = KEngineDataProvider(project_name)
-    session = 'ea98ee98-58ec-4b6b-b2d6-9f205c480ff3'
+    session = "430fbbc6-7682-4266-b28f-564dd692c26a"
+    # session = "16dd8c3f-967b-442b-a9e4-7219eeddcd45"
+    # session = "e902743d-5892-4b8f-aae8-618dc0b19ab0"
+    # session = 'bc63d5f7-7d94-4c68-8843-2d58a737d442'
+    # session = '0c3a387c-6212-4da8-9a75-360fe92d05b0'
     data_provider.load_session_data(session)
     output = Output()
     Calculations(data_provider, output).run_project_calculations()
