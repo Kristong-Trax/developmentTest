ee3498e26df06e7d1aa443199800f35495745605
kristati <krishnat@traxretail.com>
2019-10-08 16:57:58 -0400

    push

---
+++ Projects/CCNAYARMX/LocalCalculations.py
@@ -0,0 +1,17 @@
+
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from Projects.CCNAYARMX.Calculations import Calculations
+
+
+# if __name__ == '__main__':
+#     LoggerInitializer.init('ccnayarmx calculations')
+#     Config.init()
+#     project_name = 'ccnayarmx'
+#     data_provider = KEngineDataProvider(project_name)
+#     session_list = ['INSERT_TEST_SESSIONS']
+#     for session in session_list:
+#         data_provider.load_session_data(session)
+#         output = Output()
+#         Calculations(data_provider, output).run_project_calculations()
---
+++ Projects/CCNAYARMX/Utils/KPIToolBox.py
@@ -0,0 +1,37 @@
+
+from Trax.Algo.Calculations.Core.DataProvider import Data
+from Trax.Utils.Logging.Logger import Log
+from KPIUtils_v2.Utils.GlobalScripts.Scripts import GlobalSessionToolBox
+# import pandas as pd
+
+from Projects.CCNAYARMX.Data.LocalConsts import Consts
+
+# from KPIUtils_v2.Utils.Consts.DataProvider import 
+# from KPIUtils_v2.Utils.Consts.DB import 
+# from KPIUtils_v2.Utils.Consts.PS import 
+# from KPIUtils_v2.Utils.Consts.GlobalConsts import 
+# from KPIUtils_v2.Utils.Consts.Messages import 
+# from KPIUtils_v2.Utils.Consts.Custom import 
+# from KPIUtils_v2.Utils.Consts.OldDB import 
+
+# from KPIUtils_v2.Calculations.AssortmentCalculations import Assortment
+# from KPIUtils_v2.Calculations.AvailabilityCalculations import Availability
+# from KPIUtils_v2.Calculations.NumberOfScenesCalculations import NumberOfScenes
+# from KPIUtils_v2.Calculations.PositionGraphsCalculations import PositionGraphs
+# from KPIUtils_v2.Calculations.SOSCalculations import SOS
+# from KPIUtils_v2.Calculations.SequenceCalculations import Sequence
+# from KPIUtils_v2.Calculations.SurveyCalculations import Survey
+
+# from KPIUtils_v2.Calculations.CalculationsUtils import GENERALToolBoxCalculations
+
+__author__ = 'krishnat'
+
+
+class ToolBox(GlobalSessionToolBox):
+
+    def __init__(self, data_provider, output):
+        GlobalSessionToolBox.__init__(self, data_provider, output)
+
+    def main_calculation(self):
+        score = 0
+        return score
