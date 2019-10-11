751547760b82a386ad3e0de72ec72baf092a3979
sergeypol <sergey@traxretail.com>
2019-10-11 10:39:32 +0300

    PROS-10194 - CCRU - Promo tracking KPIs

--- Projects/CCRU/LocalCalculations.py
+++ Projects/CCRU/LocalCalculations.py
@@ -11,10 +11,15 @@ if __name__ == '__main__':
     project_name = 'ccru-sand'
     data_provider = KEngineDataProvider(project_name)
     session_uids = [
-        'ff5a9495-b910-4ffe-b526-69f7c7348d09',
-        'ff3daa13-0e4b-49dc-9241-55626ba7c779',
-        'ff3ab26f-d43f-416e-a2f8-44727ddc6588',
-        'FEFEB55D-A571-42B3-ABE4-87F887FB7A00'
+        'FFFF36A6-57C5-407D-8B18-9D8A3FF3E20C',
+        'FFFF0222-11BC-4480-9D0A-9B0A500B98B9',
+        'fffb03f5-850f-47b4-a9a7-4582ce2217c1',
+        'ffdf0e81-4026-471e-91ed-04fea8c50178',
+
+        # 'ff5a9495-b910-4ffe-b526-69f7c7348d09',
+        # 'ff3daa13-0e4b-49dc-9241-55626ba7c779',
+        # 'ff3ab26f-d43f-416e-a2f8-44727ddc6588',
+        # 'FEFEB55D-A571-42B3-ABE4-87F887FB7A00'
 
         # SAND
         # '3a3e96ee-3dfa-47f2-8cdb-6226756f62f8',
