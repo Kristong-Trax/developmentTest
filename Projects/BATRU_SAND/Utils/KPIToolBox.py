96daef773a44636b55901b568ade95e303c4de34
natalya_k <natalyak@traxretail.com>
2019-10-23 09:56:14 +0300

    change from lead

--- Projects/BATRU_SAND/Utils/KPIToolBox.py
+++ Projects/BATRU_SAND/Utils/KPIToolBox.py
@@ -1508,7 +1508,7 @@ class BATRU_SANDToolBox:
                                 section_shelf_data[section_shelf_data['product_ean_code_lead'].isin(misplaced_products_eans)][
                                     'product_name'].unique().tolist()
                             misplaced_products_fks = self.all_products[self.all_products['product_ean_code_lead'].\
-                                isin(misplaced_products_eans)]['product_fk_lead'].unique().tolist()
+                                isin(misplaced_products_eans)]['product_fk'].unique().tolist()
 
                 # Initial score values
                 sku_presence_score = 0
