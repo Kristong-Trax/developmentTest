ba339c4efda0ff582666431b72215ff149beff26
Hunter Young <huntery@traxretail.com>
2019-10-23 11:56:04 -0400

    Updated template for DiageoMX and Liberty

--- Projects/DIAGEOMX/Utils/KPIToolBox.py
+++ Projects/DIAGEOMX/Utils/KPIToolBox.py
@@ -38,7 +38,7 @@ class DIAGEOMXToolBox:
 
         # global touch point kpi
         template_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'Data',
-                                     'TOUCH POINT 2019 Sep.xlsx')
+                                     'TOUCH POINT Noviembre 2019.xlsx')
         res_dict = self.diageo_generator.diageo_global_touch_point_function(template_path)
         self.commonV2.save_json_to_new_tables(res_dict)
 
