47c4cd46a691002fcd219a05a8d3526ae75bfdb3
Hunter Young <huntery@traxretail.com>
2019-11-14 19:23:33 -0500

    a

--- Projects/CCNAYARMX_SAND2/Utils/KPIToolBox.py
+++ Projects/CCNAYARMX_SAND2/Utils/KPIToolBox.py
@@ -2,6 +2,7 @@
 from Trax.Algo.Calculations.Core.DataProvider import Data
 from Trax.Utils.Logging.Logger import Log
 from KPIUtils_v2.Utils.GlobalScripts.Scripts import GlobalSessionToolBox
+from KPIUtils_v2.GlobalDataProvider.PsDataProvider import PsDataProvider
 import pandas as pd
 import os
 
@@ -38,11 +39,12 @@ PLATFORMAS_SCORING = 'Platformas Scoring'
 SCORING = 'Scoring'
 ROLLBACKS = 'Rollbacks'
 BAY_COUNT = 'Bay Count'
+TOTEM = 'Totem'
 
 PORTAFOLIO_PRODUCTS = 'Portafolio Products Details'
 
 SHEETS = [PER_SCENE_SOS, SOS, SURVEY, SURVEY_PASSTHROUGH, AVAILABILITY, PLATFORMAS, PLATFORMAS_SCORING, ROLLBACKS,
-          BAY_COUNT, SCORING, KPIS, PORTAFOLIO_PRODUCTS]
+          BAY_COUNT, SCORING, KPIS, PORTAFOLIO_PRODUCTS, TOTEM]
 
 TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'Data', 'INSERTSTUFFHERE')
 
@@ -56,12 +58,17 @@ class ToolBox(GlobalSessionToolBox):
 
     def __init__(self, data_provider, output):
         GlobalSessionToolBox.__init__(self, data_provider, output)
+        self.ps_data_provider = PsDataProvider(self.data_provider, self.output)
         self.results_df = pd.DataFrame(columns=['kpi_name', 'kpi_fk', 'numerator_id', 'numerator_result',
                                                 'denominator_id', 'denominator_result', 'result', 'score',
                                                 'identifier_result', 'identifier_parent', 'should_enter'])
         self.templates = {}
         self.parse_template()
         self.own_manuf_fk = int(self.data_provider.own_manufacturer.param_value.values[0])
+        self.scif = self.scif[self.scif['product_type'] != 'Irrelevant']
+        self.scene_survey_results = self.get_scene_survey_response()
+        self.session_survey_results = self.get_session_survey_response()
+        self.custom_entities = self.ps_data_provider.get_custom_entities(1002)
 
     def parse_template(self):
         for sheet in SHEETS:
@@ -174,7 +181,7 @@ class ToolBox(GlobalSessionToolBox):
         else:
             return None
 
-    def calculate_availability(self, row):
+    def calculate_assortment(self, row):
         kpi_name = row[KPI_NAME]
         kpi_fk = self.get_kpi_fk_by_kpi_type(kpi_name)
         sku_kpi_name = kpi_name + ' - SKU'
@@ -183,12 +190,8 @@ class ToolBox(GlobalSessionToolBox):
 
         relevant_scif = self.scif.copy()
 
-        template_group = self.does_exist(row, TEMPLATE_GROUP)
-        if template_group:
-            relevant_scif = relevant_scif[relevant_scif['template_group'].isin(template_group)]
-
         template_name = self.does_exist(row, TEMPLATE_NAME)
-        if template_group:
+        if template_name:
             relevant_scif = relevant_scif[relevant_scif['template_name'].isin(template_name)]
 
         sub_category = row['sub_category']
@@ -215,20 +218,278 @@ class ToolBox(GlobalSessionToolBox):
         results_list.append(result_dict)
         return results_list
 
+    def calculate_availability(self, row):
+        kpi_name = row[KPI_NAME]
+        kpi_fk = self.get_kpi_fk_by_kpi_type(kpi_name)
+
+        template_name = row['Template_name']
+
+        totem_products = self.templates[TOTEM]['POSM'].unique().tolist()
+
+        relevant_scif = self.scif[self.scif['template_name'] == template_name]
+        if relevant_scif.empty:
+            return {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'result': pd.np.nan}
+
+        relevant_scif = relevant_scif[relevant_scif['product_name'].isin(totem_products)]
+
+        unique_products = len(relevant_scif['product_name'].unique().tolist())
+        target = row['target']
+
+        if unique_products >= target:
+            result = 1
+        else:
+            result = 0
+
+        result_dict = {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'numerator_id': self.own_manuf_fk,
+                       'denominator_id': self.store_id, 'result': result}
+        return result_dict
+
     def calculate_sos(self, row):
-        pass
+        kpi_name = row[KPI_NAME]
+        kpi_fk = self.get_kpi_fk_by_kpi_type(kpi_name)
+
+        template_name = row['Template_name']
+
+        denominator_scif = self.scif[(self.scif['template_name'] == template_name) &
+                                     (self.scif['product_type'] != 'POS')]
+        if denominator_scif.empty:
+            return {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'result': pd.np.nan}
+
+        denominator_id = denominator_scif['template_fk'].iloc[0]
+        denominator_facings = denominator_scif['facings'].sum()
+
+        sub_category = row['sub_category']
+        if pd.notna(sub_category):
+            numerator_scif = denominator_scif[denominator_scif['sub_category'] == sub_category]
+            numerator_id = numerator_scif['sub_category_fk'].iloc[0]
+
+        manufacturer_name = row['manufacturer_name']
+        if pd.notna(manufacturer_name):
+            numerator_scif = denominator_scif[denominator_scif['manufacturer_name'] == manufacturer_name]
+            numerator_id = numerator_scif['manufacturer_fk'].iloc[0]
+
+        numerator_facings = numerator_scif['facings'].sum()
+
+        result = numerator_facings / float(denominator_facings)
+        min_target, max_target = self._get_target_range(row['target'])
+
+        if min_target <= result * 100 <= max_target:
+            score = 1
+        else:
+            score = 0
+
+        result_dict = {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'numerator_id': numerator_id,
+                       'denominator_id': denominator_id, 'result': result, 'score': score}
+        return result_dict
+
+    @staticmethod
+    def _get_target_range(target_range):
+        min_target, max_target = [int(x) for x in target_range.split('-')]
+        return min_target, max_target
 
     def calculate_per_scene_sos(self, row):
-        pass
+        kpi_name = row[KPI_NAME]
+        kpi_fk = self.get_kpi_fk_by_kpi_type(kpi_name)
+
+        template_name = row['Template_name']
+        relevant_scif = self.scif[(self.scif['template_name'] == template_name) &
+                                  (self.scif['product_type'] != 'POS')]
+        if relevant_scif.empty:
+            return {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'result': pd.np.nan}
+
+        denominator_scif = relevant_scif.groupyby('scene_id', as_index=False)['facings'].sum()
+        denominator_scif.rename(columns={'facings': 'denominator'}, inplace=True)
+
+        numerator_scif = relevant_scif[relevant_scif['manufacturer_name'] == 'TCCC']
+        numerator_scif = numerator_scif.groupby('scene_id', as_index=False)['facings'].sum()
+        numerator_scif.rename(columns={'facings': 'numerator'}, inplace=True)
+
+        merged_df = pd.merge(denominator_scif, numerator_scif, how='left', on='scene_id').fillna(0)
+        merged_df['sos'] = (merged_df['numerator'] / merged_df['denominator'])
+
+        min_target, max_target = self._get_target_range(row['target'])
+
+        merged_df['passing'] = merged_df['sos'].apply(lambda x: min_target <= x <= max_target)
+
+        failing_scenes = merged_df[~merged_df['passing']]
+        if failing_scenes.empty:
+            result = 1
+        else:
+            result = 0
+
+        result_dict = {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'numerator_id': self.own_manuf_fk,
+                       'denominator_id': self.store_id, 'result': result}
+        return result_dict
 
     def calculate_bay_count(self, row):
+        kpi_name = row[KPI_NAME]
+        kpi_fk = self.get_kpi_fk_by_kpi_type(kpi_name)
+
+        template_name = self.does_exist(row, 'Template_name')
+        relevant_scif = self.scif[self.scif['template_name'].isin(template_name)]
+        if relevant_scif.empty:
+            return {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'result': pd.np.nan}
+
+        scenes = relevant_scif['scene_id'].unique().tolist()
+        products = relevant_scif['product_fk'].unique().tolist()
+
+        bay_mpis = self.matches[(self.matches['scene_id'].isin(scenes)) &
+                                (self.matches['product_fk'].isin(products))]
+
+        bays = len(bay_mpis['bay_number'].unique().tolist())
+        target_mapping = self._get_target_mapping(row['targets'])
+        target = target_mapping.get(self.store_info['additional_attribute_1'].iloc[0])
+        if not target:
+            return {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'result': pd.np.nan}
+
+        if bays > target:
+            result = 1
+        else:
+            result = 0
+
+        result_dict = {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'numerator_id': self.own_manuf_fk,
+                       'denominator_id': self.store_id, 'result': result}
+        return result_dict
+
+    def calculate_planograma(self, row):
+        kpi_name = row[KPI_NAME]
+        kpi_fk = self.get_kpi_fk_by_kpi_type(kpi_name)
+
+        template_name = self.does_exist(row, 'Template_name')
+        relevant_scif = self.scif[self.scif['template_name'].isin(template_name)]
+        if relevant_scif.empty:
+            return {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'result': pd.np.nan}
+
+        brands = self.does_exist(row, 'Brands')
+        minimum_brands_required = row['minimum number of brands']
+
+        relevant_scif = relevant_scif[relevant_scif['brand_name'].isin(brands)]
+
+        unique_brands = relevant_scif['brand_name'].unique().tolist()
+
+        if len(unique_brands) >= minimum_brands_required:
+            result = 1
+        else:
+            result = 0
+
+        result_dict = {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'numerator_id': self.own_manuf_fk,
+                       'denominator_id': self.store_id, 'result': result}
+        return result_dict
+
+    def calculate_communicacion(self, row):
         pass
+
+    def calculate_cooler_cc(self, row):
+        kpi_name = row[KPI_NAME]
+        kpi_fk = self.get_kpi_fk_by_kpi_type(kpi_name)
+        brand_kpi_name = kpi_name + ' - Brand'
+        brand_kpi_fk = self.get_kpi_fk_by_kpi_type(brand_kpi_name)
+        results_list = []
+
+        relevant_question_fks = self.does_exist(row, 'Survey Question PK')
+
+        relevant_results = \
+            self.scene_survey_results[self.scene_survey_results['question_fk'].isin(relevant_question_fks)]
+
+        cooler_brands = relevant_results['selected_option_text'].unique().tolist()
+        if cooler_brands:
+            for cooler_brand_name in cooler_brands:
+                if cooler_brand_name and pd.notna(cooler_brand_name):
+                    cooler_brand_fk = self._get_cooler_brand_fk_by_cooler_brand_name(cooler_brand_name)
+                    result_dict = {'kpi_name': brand_kpi_name, 'kpi_fk': brand_kpi_fk, 'numerator_id': cooler_brand_fk,
+                                   'denominator_id': self.store_id, 'result': 1, 'identifier_parent': kpi_name}
+                    results_list.append(result_dict)
+
+        if len(cooler_brands) > 1:
+            result = 1
+        else:
+            result = 0
+
+        result_dict = {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'numerator_id': self.own_manuf_fk,
+                       'denominator_id': self.store_id, 'result': result}
+        results_list.append(result_dict)
+        return results_list
+
+    def _get_cooler_brand_fk_by_cooler_brand_name(self, cooler_brand_name):
+        entity = self.custom_entities[self.custom_entities['name'] == cooler_brand_name]
+        if entity.empty:
+            return None
+        else:
+            return entity['pk'].iloc[0]
+
+    @staticmethod
+    def _get_target_mapping(column_value):
+        target_dict = {}
+        for pair in column_value.split(','):
+            key, value = pair.split(';')
+            target_dict.update({key: int(value)})
+        return target_dict
 
     def calculate_survey(self, row):
         pass
 
+    def calculate_scene_survey(self, row):
+        kpi_name = row[KPI_NAME]
+        kpi_fk = self.get_kpi_fk_by_kpi_type(kpi_name)
+
+        relevant_question_fks = self.does_exist(row, 'Survey Question PK')
+        required_answer = row['Required Answer']
+
+        relevant_results = \
+            self.scene_survey_results[self.scene_survey_results['question_fk'].isin(relevant_question_fks)]
+
+        if relevant_results.empty:
+            return {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'result': pd.np.nan}
+
+        failing_results = relevant_results[relevant_results['selected_option_text'] != required_answer]
+        if failing_results.empty:
+            result = 1
+        else:
+            result = 0
+
+        result_dict = {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'numerator_id': self.own_manuf_fk,
+                       'denominator_id': self.store_id, 'result': result}
+        return result_dict
+
+    def calculate_scoring_survey(self, row):
+        kpi_name = row[KPI_NAME]
+        kpi_fk = self.get_kpi_fk_by_kpi_type(kpi_name)
+
+        relevant_question_fks = self.does_exist(row, 'Survey Question PK')
+
+        relevant_results = \
+            self.session_survey_results[self.session_survey_results['question_fk'].isin(relevant_question_fks)]
+
+        if relevant_results.empty:
+            result = 0
+            score = 0
+        else:
+            score = len(relevant_results['selected_option_text'].iloc[0]) / float(3)
+            result = 1
+
+        result_dict = {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'numerator_id': self.own_manuf_fk,
+                       'denominator_id': self.store_id, 'result': result, 'score': score}
+        return result_dict
+
     def _get_product_fk_from_name(self, product_name):
         return self.all_products[self.all_products['product_name'] == product_name]['product_fk'].iloc[0]
+
+    def get_scene_survey_response(self):
+        query = """SELECT session_uid,question_fk,selected_option_text, sce.pk as 'scene_id'
+                FROM probedata.scene_survey_response res
+                LEFT JOIN probedata.scene sce ON res.scene_fk =  sce.pk
+                WHERE session_uid = '{}';""".format(self.session_uid)
+
+        scene_survey_response = pd.read_sql_query(query, self.ps_data_provider.rds_conn.db)
+        return scene_survey_response
+
+    def get_session_survey_response(self):
+        query = """SELECT session_uid,question_fk,selected_option_text
+                        FROM probedata.survey_response res
+                        WHERE session_uid = '{}';""".format(self.session_uid)
+
+        session_survey_response = pd.read_sql_query(query, self.ps_data_provider.rds_conn.db)
+        return session_survey_response
 
     @staticmethod
     def does_exist(kpi_line, column_name):
