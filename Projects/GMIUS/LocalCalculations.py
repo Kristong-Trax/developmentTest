6e6189bd13335ff4489c4ff76222113f879d6e6f
samkowalski <samk@traxretail.com>
2019-11-18 16:21:13 -0500

    updated logic for primary location max block adj

--- Projects/GMIUS/LocalCalculations.py
+++ Projects/GMIUS/LocalCalculations.py
@@ -11,14 +11,14 @@ if __name__ == '__main__':
 
     sessions = [
                 'c85d058d-6fcf-4865-830b-ab6563230096',
-                # 'd44b436c-7df6-4a64-9197-b2c441dc5bef',
-                # 'ccbfc4c6-7ad8-4dcb-b3bc-833b9d6d5a32',
-                # '8b9d2a2b-1272-48ef-b50a-2a745f1e0829',
-                # '8968eeb4-b487-42de-8939-02cbfa433a0e',
-                # '09a94f4a-038b-4f1c-8cbc-637d5ea08fdc',
+                'd44b436c-7df6-4a64-9197-b2c441dc5bef',
+                'ccbfc4c6-7ad8-4dcb-b3bc-833b9d6d5a32',
+                '8b9d2a2b-1272-48ef-b50a-2a745f1e0829',
+                '8968eeb4-b487-42de-8939-02cbfa433a0e',
+                '09a94f4a-038b-4f1c-8cbc-637d5ea08fdc',
                 ]
 
-    sessions = ['5782fac6-f360-478f-8a77-75b4845666a6']
+    sessions = ['c85d058d-6fcf-4865-830b-ab6563230096']
 
     for session in sessions:
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'.format(session))
