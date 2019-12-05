bbb55960af3bc473c98b8738f90263ead6cdb9ed
natalya_k <natalyak@traxretail.com>
2019-12-05 14:23:34 +0200

    m

--- Projects/CCZA_SAND/Calculations.py
+++ Projects/CCZA_SAND/Calculations.py
@@ -1,8 +1,8 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 from Projects.CCZA_SAND.KPIGenerator import CCZAGenerator
 
@@ -16,14 +16,14 @@ class CCZACalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('ccza-sand calculations')
-    Config.init()
-    project_name = 'ccza-sand'
-    sessions = ['4ca9af97-155e-4fb8-b51a-e7ea67622979']
-    for session in sessions:
-        data_provider = KEngineDataProvider(project_name)
-        print session
-        data_provider.load_session_data(session)
-        output = Output()
-        CCZACalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('ccza-sand calculations')
+#     Config.init()
+#     project_name = 'ccza-sand'
+#     sessions = ['7a2cd748-4ca4-43b9-942c-78a80edd3c07']
+#     for session in sessions:
+#         data_provider = KEngineDataProvider(project_name)
+#         print session
+#         data_provider.load_session_data(session)
+#         output = Output()
+#         CCZACalculations(data_provider, output).run_project_calculations()
