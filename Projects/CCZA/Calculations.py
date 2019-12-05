142a73659097a88f332da05e1317af318bf62867
natalya_k <natalyak@traxretail.com>
2019-12-05 11:47:59 +0200

    cleanup

--- Projects/CCZA/Calculations.py
+++ Projects/CCZA/Calculations.py
@@ -1,8 +1,8 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 from Projects.CCZA.KPIGenerator import CCZAGenerator
 
@@ -16,20 +16,20 @@ class CCZACalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('ccza calculations')
-    Config.init()
-    project_name = 'ccza'
-    sessions = [
-                # '50cf705c-bf8f-4eff-a813-6aa7c0835132', # missing survey codes
-                # 'FF39172D-EE62-4184-ABF7-91CD2D05800B',
-                # 'FE8E0E52-9DF1-40BF-81E2-C6F618682806',
-                # 'FE1CF09E-272A-4B15-9BF4-E9E4228A2BF2'
-                'F031B961-12D6-4648-A22B-7F13872799DE' # wholesaler for new kpis
-    ]
-    for session in sessions:
-        data_provider = KEngineDataProvider(project_name)
-        print session
-        data_provider.load_session_data(session)
-        output = Output()
-        CCZACalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('ccza calculations')
+#     Config.init()
+#     project_name = 'ccza'
+#     sessions = [
+#                 # '50cf705c-bf8f-4eff-a813-6aa7c0835132', # missing survey codes
+#                 # 'FF39172D-EE62-4184-ABF7-91CD2D05800B',
+#                 # 'FE8E0E52-9DF1-40BF-81E2-C6F618682806',
+#                 # 'FE1CF09E-272A-4B15-9BF4-E9E4228A2BF2'
+#                 'F031B961-12D6-4648-A22B-7F13872799DE' # wholesaler for new kpis
+#     ]
+#     for session in sessions:
+#         data_provider = KEngineDataProvider(project_name)
+#         print session
+#         data_provider.load_session_data(session)
+#         output = Output()
+#         CCZACalculations(data_provider, output).run_project_calculations()
