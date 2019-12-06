93d2b80752ca9a11606ca0816e6fbf8f715b432a
ryanrensh <ryan.ren@traxretail.com>
2019-12-06 04:48:45 +0200

    PROS-12863 PNGHK Sanity Test

--- Tests/test_functional_pnghk_sanity.py
+++ Tests/test_functional_pnghk_sanity.py
@@ -7,14 +7,14 @@ from Trax.Data.Testing.SeedNew import Seeder
 from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
 from Trax.Cloud.Services.Connector.Keys import DbUsers
 from Trax.Data.Testing.TestProjects import TestProjectsNames
-from Projects.PNGHK.SceneKpis.SceneCalculations import SceneCalculations
 from Tests.Data.TestData.test_data_pnghk_sanity import ProjectsSanityData
 from Projects.PNGHK.Calculations import Calculations
-from Trax.Apps.Core.Testing.BaseCase import TestFunctionalCase
 
+from Trax.Apps.Core.Testing.BaseCase import TestFunctionalCase
 from Tests.TestUtils import remove_cache_and_storage
 
-__author__ = 'ilays'
+
+__author__ = 'ryan'
 
 
 class TestKEngineOutOfTheBox(TestFunctionalCase):
@@ -22,8 +22,6 @@ class TestKEngineOutOfTheBox(TestFunctionalCase):
     def set_up(self):
         super(TestKEngineOutOfTheBox, self).set_up()
         remove_cache_and_storage()
-        self.mock_object(object_name='commit_results_data', path='KPIUtils_v2.DB.CommonV2.Common')
-
 
     @property
     def import_path(self):
@@ -35,7 +33,17 @@ class TestKEngineOutOfTheBox(TestFunctionalCase):
     
     seeder = Seeder()
     
-    def _assert_kpi_results_filled(self):
+    def _assert_old_tables_kpi_results_filled(self):
+        connector = PSProjectConnector(TestProjectsNames().TEST_PROJECT_1, DbUsers.Docker)
+        cursor = connector.db.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('''
+        SELECT * FROM report.kpi_results
+        ''')
+        kpi_results = cursor.fetchall()
+        self.assertNotEquals(len(kpi_results), 0)
+        connector.disconnect_rds()
+        
+    def _assert_new_tables_kpi_results_filled(self):
         connector = PSProjectConnector(TestProjectsNames().TEST_PROJECT_1, DbUsers.Docker)
         cursor = connector.db.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('''
@@ -44,27 +52,29 @@ class TestKEngineOutOfTheBox(TestFunctionalCase):
         kpi_results = cursor.fetchall()
         self.assertNotEquals(len(kpi_results), 0)
         connector.disconnect_rds()
-
-    def _assert_scene_kpi_results_filled(self):
+    
+    def _assert_scene_tables_kpi_results_filled(self):
         connector = PSProjectConnector(TestProjectsNames().TEST_PROJECT_1, DbUsers.Docker)
         cursor = connector.db.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('''
-         SELECT * FROM report.scene_kpi_results
-         ''')
+        SELECT * FROM report.scene_kpi_results
+        ''')
         kpi_results = cursor.fetchall()
         self.assertNotEquals(len(kpi_results), 0)
         connector.disconnect_rds()
-
+    
     @seeder.seed(["pnghk_seed", "mongodb_products_and_brands_seed"], ProjectsSanityData())
     def test_pnghk_sanity(self):
         project_name = ProjectsSanityData.project_name
         data_provider = KEngineDataProvider(project_name)
-        sessions = ['7c64c9c4-a439-42af-8b23-2dea2f909f3d']
-        for session in sessions:
-            data_provider.load_session_data(session)
+        sessions = {'a5cd3bfd-76ba-40f1-af2f-e6392f16f0fa': []}
+        for session in sessions.keys():
+            data_provider.load_session_data(str(session))
             output = Output()
             Calculations(data_provider, output).run_project_calculations()
-            self._assert_kpi_results_filled()
-            data_provider.load_scene_data(session, scene_id=185596)
-            SceneCalculations(data_provider).calculate_kpis()
-            self._assert_scene_kpi_results_filled()
+            # self._assert_old_tables_kpi_results_filled()
+            # self._assert_new_tables_kpi_results_filled()
+            # for scene in sessions[session]:
+            #     data_provider.load_scene_data(str(session), scene_id=scene)
+            #     SceneCalculations(data_provider).calculate_kpis()
+            self._assert_new_tables_kpi_results_filled()
