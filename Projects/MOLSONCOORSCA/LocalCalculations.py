51369c6ba5794c1da915b866f76234fea5185c98
limor_c <limorc@traxretail.com>
2019-12-15 17:11:14 +0200

    deleting copies of block calculation and adjacency graph + changing adjacency calculation

--- Projects/MOLSONCOORSCA/LocalCalculations.py
+++ Projects/MOLSONCOORSCA/LocalCalculations.py
@@ -17,28 +17,10 @@ if __name__ == '__main__':
     #     'mysql.', 'mysql.vpn.')
 
     sessions = [
-                # 'AD5CDDE6-4157-4F1F-B6FF-49BDF3F8EC82',  # 15775
-        # '9F19D5B1-B95D-40E6-A7EA-B36E963A0770',
-        # 'F3ABA765-DEF6-46A4-8C1E-BC818532398A',
-        # '26F8B85F-F6B9-4BAE-89EC-4A3C499BAE19',
-        '8B46BA22-FFC6-438B-9B5B-AAB189F4F498',
-        '39D117D6-B2A0-4523-92CE-E286CFC4AB60',
-        'F3ABA765-DEF6-46A4-8C1E-BC818532398A',
-        'A7C7232E-06C8-43DB-BE2A-FB241E4736F0',
 
-        '4B02965C-290D-4221-B91E-B430A51E2498',
-        'F14AB1FB-268B-4299-85AC-927E082646CB',
-        'A67BAF56-883D-4275-A28C-22AECCD6940B'
+        '39D117D6-B2A0-4523-92CE-E286CFC4AB60'
 
                 ]
-    # Purity and Violations
-    # sessions = ['4B02965C-290D-4221-B91E-B430A51E2498',
-    #             'F14AB1FB-268B-4299-85AC-927E082646CB',
-    #             'A67BAF56-883D-4275-A28C-22AECCD6940B']
-
-    # sessions = ['E7698DAB-1C7B-4E6D-8E77-6B8656428175']
-    # sessions = ['98201508-80C9-4E8A-8572-C94DD7E77CDA']
-    sessions = ['FE573514-8BBD-45F3-B6B8-DB4437EF4E51']
 
     for session in sessions:
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'.format(session))
