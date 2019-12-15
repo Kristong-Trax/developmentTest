a447689cd1db46b0e39ae3fe5b2e174801d36992
natalya_k <natalyak@traxretail.com>
2019-12-15 20:04:11 +0200

    starting validator

--- Projects/CCZA/Utils/TemplateValidator.py
+++ Projects/CCZA/Utils/TemplateValidator.py
@@ -4,21 +4,25 @@ from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
 from Trax.Utils.Conf.Keys import DbUsers
 import pandas as pd
 import numpy as np
-from Projects.CCZA.Utils.Const import Const
+from Projects.CCZA_SAND.Utils.Const import Const
 
 
 class CczaTemplateValidator(Main_Template):
 
-    LIST_OF_SHEETS = [Const.KPIS, Const.LIST_OF_ENTITIES, Const.SOS_WEIGHTS, Const.SOS_TARGETS, Const.PRICING_WEIGHTS,
-                      Const.PRICING_TARGETS, Const.SURVEY_QUESTIONS, Const.FLOW_PARAMETERS]
+    #
+    # SHEETS_COL_MAP = {Const.KPIS:  set([Const.KPI_NAME, Const.KPI_GROUP, Const.KPI_TYPE, 'Tested KPI Group',
+    #                                     Const.TARGET, Const.WEIGHT_SHEET, 'SCORE']),
+    #                  Const.LIST_OF_ENTITIES: set([]), Const.SOS_WEIGHTS, Const.SOS_TARGETS, Const.PRICING_WEIGHTS,
+    #                   Const.PRICING_TARGETS, Const.SURVEY_QUESTIONS, Const.FLOW_PARAMETERS}
 
     def __init__(self, project_name, file_url):
         Main_Template.__init__(self)
         self.project = project_name
-        self.file_path = file_url
+        self.template_path = file_url
         self.rds_conn = self.rds_connect
         self.store_data = self.get_store_data
         self.all_products = self.get_product_data
+        self.kpi_sheets = {}
 
     @property
     def rds_connect(self):
@@ -43,5 +47,20 @@ class CczaTemplateValidator(Main_Template):
         return self.store_data
 
     def validate_template_data(self):
+        self.check_all_tabs_exist()
+
+        # validate sheets, columns format
+        # validate sheets contents against sheet contents
+        # validate kpi names in the db both against old tables and new tables
+        # validate specific template parameters
+        # validate all weights and targets are aligned
+        #
         pass
 
+    def check_all_tabs_exist(self):
+        for name in Const.sheet_names_and_rows:
+            try:
+                self.kpi_sheets[name] = parse_template(self.template_path, sheet_name=name,
+                                                       lower_headers_row_index=Const.sheet_names_and_rows[name])
+            except Exception as e: # look up the type of exception in case sheet name is missing
+                self.errorHandler.log_error('Sheet {} is missing in the file'.format(name))
