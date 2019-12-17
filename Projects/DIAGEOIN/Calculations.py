69c9212cbbbd41d6ebd459c7c63b400a23615b14
nidhinb <nidhinb@traxretail.com>
2019-12-17 17:12:17 +0800

    fixing calculations

--- Projects/DIAGEOIN/Calculations.py
+++ Projects/DIAGEOIN/Calculations.py
@@ -1,8 +1,6 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+
 
 from Projects.DIAGEOIN.KPIGenerator import DIAGEOINGenerator
 
@@ -18,6 +16,9 @@ class DIAGEOINCalculations(BaseCalculationsScript):
 
 
 if __name__ == '__main__':
+    from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+    from Trax.Utils.Conf.Configuration import Config
+    from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
     LoggerInitializer.init('DIAGEOIN calculations')
     Config.init()
     project_name = 'diageoin'
