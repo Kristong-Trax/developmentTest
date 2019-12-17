5ea28712ac37fab63caf1b205d6cab769e38af55
nidhinb <nidhinb@traxretail.com>
2019-12-17 17:13:53 +0800

    commenting local calc code

--- Projects/DIAGEOIN/Calculations.py
+++ Projects/DIAGEOIN/Calculations.py
@@ -15,17 +15,17 @@ class DIAGEOINCalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-if __name__ == '__main__':
-    from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-    from Trax.Utils.Conf.Configuration import Config
-    from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-    LoggerInitializer.init('DIAGEOIN calculations')
-    Config.init()
-    project_name = 'diageoin'
-    data_provider = KEngineDataProvider(project_name)
-    sessions = ['fe4cdf53-dab1-4b6d-b640-0d631110e763']
-
-    for session in sessions:
-        data_provider.load_session_data(session)
-        output = Output()
-        DIAGEOINCalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+#     from Trax.Utils.Conf.Configuration import Config
+#     from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+#     LoggerInitializer.init('DIAGEOIN calculations')
+#     Config.init()
+#     project_name = 'diageoin'
+#     data_provider = KEngineDataProvider(project_name)
+#     sessions = ['fe4cdf53-dab1-4b6d-b640-0d631110e763']
+#
+#     for session in sessions:
+#         data_provider.load_session_data(session)
+#         output = Output()
+#         DIAGEOINCalculations(data_provider, output).run_project_calculations()
