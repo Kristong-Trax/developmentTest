eb902f6a47fba71f1b6e6d77a2308e54548217ce
idanre <idanr@traxretail.com>
2019-12-18 14:55:24 +0200

    It is NOT Diageo Australia time for GDPA

--- Projects/DIAGEOAU/Utils/KPIToolBox.py
+++ Projects/DIAGEOAU/Utils/KPIToolBox.py
@@ -15,9 +15,6 @@ class DIAGEOAUToolBox:
         self.diageo_generator = DIAGEOGenerator(self.data_provider, self.output, self.common)
 
     def main_calculation(self):
-        """
-        This function calculates the KPI results.
-        """
         # SOS Out Of The Box kpis
         self.diageo_generator.activate_ootb_kpis(self.commonV2)
 
@@ -25,8 +22,12 @@ class DIAGEOAUToolBox:
         self.diageo_generator.sos_by_scene_type(self.commonV2)
 
         # Global assortment kpis
-        assortment_results = self.diageo_generator.diageo_global_grouping_assortment_calculation()
-        self.commonV2.save_json_to_new_tables(assortment_results)
+        assortment_res_dict = self.diageo_generator.diageo_global_assortment_function_v2()
+        self.commonV2.save_json_to_new_tables(assortment_res_dict)
+
+        # Global assortment kpis - v3 for NEW MOBILE REPORTS use
+        assortment_res_dict_v3 = self.diageo_generator.diageo_global_assortment_function_v3()
+        self.commonV2.save_json_to_new_tables(assortment_res_dict_v3)
 
         # committing to new tables
         self.commonV2.commit_results_data()
