10f3e01287e0e687121ca1265b783d0be84a73ad
yasmingr <yaming@traxretail.com>
2019-12-24 22:22:14 +0200

    add sanity test

--- Projects/DIAGEOUG/Calculations.py
+++ Projects/DIAGEOUG/Calculations.py
@@ -1,7 +1,7 @@
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 from Projects.DIAGEOUG.KPIGenerator import DIAGEOUGGenerator
 
@@ -15,12 +15,12 @@ class DIAGEOUGCalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-if __name__ == '__main__':
-    LoggerInitializer.init('diageoug calculations')
-    Config.init()
-    project_name = 'diageoug'
-    data_provider = KEngineDataProvider(project_name)
-    session = '0f81682b-c541-479d-a38f-ae4b8fcc1da2'
-    data_provider.load_session_data(session)
-    output = Output()
-    DIAGEOUGCalculations(data_provider, output).run_project_calculations()
+# if __name__ == '__main__':
+#     LoggerInitializer.init('diageoug calculations')
+#     Config.init()
+#     project_name = 'diageoug'
+#     data_provider = KEngineDataProvider(project_name)
+#     session = '0f81682b-c541-479d-a38f-ae4b8fcc1da2'
+#     data_provider.load_session_data(session)
+#     output = Output()
+#     DIAGEOUGCalculations(data_provider, output).run_project_calculations()
