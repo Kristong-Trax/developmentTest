de9f62ef0790de5612d2f6b3b13bfabd9bd9658e
kristati <krishnat@traxretail.com>
2019-12-26 14:34:24 -0500

    Changed the 1 and 0 to 4 and 5. This was done to match the id of pass and fail respectively.

--- Projects/MOLSONCOORSCA/Utils/KPIToolBox.py
+++ Projects/MOLSONCOORSCA/Utils/KPIToolBox.py
@@ -711,7 +711,7 @@ class ToolBox:
             if sum(clusters['is_block']) >= 1:
                 result = 1
             total += result
-            result = 5 if result == 0 else 4  # 5 is fail and 4 is Pass in DB (check self.result_values_dict)
+            result = self.result_values_dict['Fail'] if result == 0 else self.result_values_dict['Pass']  # 5 is fail and 4 is Pass in DB (check self.result_values_dict)
             results.append({'score': result, 'result': result, 'numerator_result': result,
                             'numerator_id': self.brands_dict[brand]['brand_fk'],
                             'denominator_id': self.store_id,
@@ -720,7 +720,8 @@ class ToolBox:
         result = 0
         if total == len(brands):
             result = 1
-        result = 5 if result == 0 else 4  # 5 is fail and 4 is Pass in DB (check self.result_values_dict)
+        result = self.result_values_dict['Fail'] if result == 0 else self.result_values_dict[
+            'Pass']  # 5 is fail and 4 is Pass in DB (check self.result_values_dict)
         results.append({'score': result, 'result': result, 'numerator_result': total, 'denominator_result': len(brands),
                         'numerator_id': self.manufacturer_fk,
                         'denominator_id': self.store_id,
