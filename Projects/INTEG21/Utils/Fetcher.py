509caa0d088f1ca7379e257fefb2054ea948f647
limor_c <limorc@traxretail.com>
2019-12-29 17:38:25 +0200

    integ21

--- Projects/INTEG21/Utils/Fetcher.py
+++ Projects/INTEG21/Utils/Fetcher.py
@@ -1,11 +1,8 @@
 
-__author__ = 'urid'
+__author__ = 'Nethanel'
 
 
-class INTEG21INBEVBEQueries:
-
-    def __init__(self):
-        pass
+class MarsUsQueries(object):
 
     @staticmethod
     def get_all_kpi_data():
@@ -13,19 +10,10 @@ class INTEG21INBEVBEQueries:
             select api.name as atomic_kpi_name, api.pk as atomic_kpi_fk,
                    kpi.display_text as kpi_name, kpi.pk as kpi_fk,
                    kps.name as kpi_set_name, kps.pk as kpi_set_fk
-            from static.kpi_set kps
-            left join static.kpi kpi on kpi.kpi_set_fk = kps.pk
-            left join static.atomic_kpi api on api.kpi_fk = kpi.pk
+            from static.atomic_kpi api
+            left join static.kpi kpi on kpi.pk = api.kpi_fk
+            join static.kpi_set kps on kps.pk = kpi.kpi_set_fk
         """
-
-    @staticmethod
-    def get_business_unit_data(store_fk):
-        return """
-            select bu.name
-            from static.stores st
-            join static.business_unit bu on bu.pk = st.business_unit_fk
-            where st.pk = {}
-        """.format(store_fk)
 
     @staticmethod
     def get_delete_session_results_query(session_uid):
@@ -34,95 +22,48 @@ class INTEG21INBEVBEQueries:
                 "delete from report.kpi_results where session_uid = '{}';".format(session_uid))
 
     @staticmethod
-    def get_match_display(session_uid):
+    def get_store_attribute(attribute, store_fk):
         return """
-            select sdb.name, m.scene_fk, d.display_name, m.bay_number, m.rect_x, m.rect_y
-            from probedata.match_display_in_scene m
-            join probedata.scene s on s.pk = m.scene_fk
-            join static.display d on d.pk = m.display_fk
-            join static.display_brand sdb on sdb.pk=d.display_brand_fk
-            where s.session_uid = '{}'
+                    select additional_attribute_{} from static.stores
+                    where pk = {}
+                    """.format(attribute, store_fk)
+
+    @staticmethod
+    def get_store_retailer(store_fk):
+        return """
+                    select R.name from static.stores S
+                    join static.retailer R
+                    on S.retailer_fk = R.pk
+                    where S.pk =  {}
+                    """.format(store_fk)
+
+    @staticmethod
+    def get_probe_group(session_uid):
+        return """
+                select distinct
+                sub_group_id probe_group_id,
+                mpip.pk probe_match_fk
+                from
+                probedata.stitching_probe_info as spi
+                inner join probedata.stitching_scene_info ssi on
+                ssi.pk = spi.stitching_scene_info_fk
+                inner join probedata.match_product_in_probe mpip on
+                mpip.probe_fk = spi.probe_fk
+                inner join probedata.scene as sc on
+                sc.pk = ssi.scene_fk
+                where ssi.delete_time is null
+                and sc.session_uid = '{}';
         """.format(session_uid)
 
     @staticmethod
-    def get_oos_messages(session_uid):
-        return """
-            SELECT * FROM probedata.oos_exclude oe
-            join static.oos_message om on om.pk=oe.oos_message_fk
-            join static.oos_message_type omt on omt.pk=om.type
-            where oe.session_uid = '{}';
-        """.format(session_uid)
+    def add_kpi_to_mvp_sr(kpi, pk):
+        return '''
+        insert into static.match_product_in_probe_state_reporting
+        (pk, name, display_name)
+        values
+        ({}, "{}", "{}")
+        '''.format(pk, kpi, kpi)
 
     @staticmethod
-    def get_osa_table(store_fk):
-        return """
-            SELECT * FROM pservice.custom_store_assortments_products
-            WHERE store_fk = {} AND end_date is null AND assortment_fk = 1
-        """.format(store_fk)
-
-    @staticmethod
-    def get_must_have_table(store_fk):
-        return """
-            SELECT * FROM pservice.custom_store_assortments_products
-            WHERE store_fk = {} AND end_date is null AND assortment_fk = 2
-        """.format(store_fk)
-
-    @staticmethod
-    def get_delete_osa_records_query(product_fk, store_fk, delete_time, assortment_fk):
-        return """
-            UPDATE pservice.custom_osa
-            SET end_date = '{}'
-            WHERE product_fk = {} AND store_fk = {} and assortment_fk= {}
-        """.format(delete_time, product_fk, store_fk, assortment_fk)
-
-    @staticmethod
-    def get_delete_scif_records_query(product_fk, session_fk):
-        return """
-            UPDATE pservice.custom_scene_item_facts
-            SET  in_assortment_osa=0, oos_osa=0
-            WHERE product_fk = {} AND session_fk = {}
-        """.format(product_fk, session_fk)
-
-    @staticmethod
-    def get_store_number_1(store_fk):
-        return """
-            SELECT store_number_1
-            FROM static.stores
-            WHERE pk = {}
-        """.format(store_fk)
-
-    @staticmethod
-    def get_product_att4(product_fk):
-        return """
-            SELECT att4 from static.product
-            WHERE pk = {}
-        """.format(product_fk)
-
-    @staticmethod
-    def get_delete_custom_scif_query(session_id):
-        return """
-            delete from pservice.custom_scene_item_facts
-            where session_fk = {}
-        """.format(session_id)
-
-    @staticmethod
-    def get_att3_att4_for_products():
-        return """
-                select pk as product_fk,att3,att4 from static.product
-                """
-
-    @staticmethod
-    def get_store_attribute_8(store_fk):
-        return """
-                select additional_attribute_8 from static.stores
-                where pk = {}
-                """.format(store_fk)
-
-    @staticmethod
-    def get_rect_values_query(session_uid):
-        return """
-            select mpis.pk as scene_match_fk,product_fk,scene_fk,bay_number,shelf_number ,rect_x,rect_y, width_mm_advance
-            from probedata.match_product_in_scene mpis
-            join probedata.scene sc on sc.pk=mpis.scene_fk
-            where session_uid = '{}'
-            """.format(session_uid)
+    def get_updated_mvp_sr():
+        return 'select * from static.match_product_in_probe_state_reporting'
