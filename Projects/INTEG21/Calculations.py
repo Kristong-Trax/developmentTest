d9b72f98cf8f9b88e3a8d476aa1989587369c64e
limor_c <limorc@traxretail.com>
2020-01-02 11:52:11 +0200

    integ21

---
+++ Projects/INTEG21/Calculations.py
@@ -0,0 +1,28 @@
+from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+
+from Projects.PNGJP.KPIGenerator import PNGJPGenerator
+
+__author__ = 'Nimrod'
+
+
+class INTEG21Calculations(BaseCalculationsScript):
+    def run_project_calculations(self):
+        self.timer.start()
+        PNGJPGenerator(self.data_provider, self.output).main_function()
+        self.timer.stop('KPIGenerator.run_project_calculations')
+
+if __name__ == '__main__':
+    LoggerInitializer.init('pngjp calculations')
+    Config.init()
+    project_name = 'integ21'
+    data_provider = KEngineDataProvider(project_name)
+    sessions = ["3323D6AE-BBFA-4A12-B0A2-9803DD7C52CC",
+                "180DF2CD-7BC9-492C-A4F6-D4418DBBCBCA"]
+
+    for session in sessions:
+        data_provider.load_session_data(session)
+        output = Output()
+        INTEG21Calculations(data_provider, output).run_project_calculations()
---
+++ Projects/INTEG21/Tests/test_unit_pngjp.py
@@ -0,0 +1,69 @@
+# from mock import MagicMock, mock
+# from KPIUtils_v2.Utils.INTEG21Consts.OldDB import KpiResults
+# from Projects.INTEG21.Utils.KpiQualitative import INTEG21KpiQualitative_ToolBox
+# from Trax.Utils.Testing.Case import TestCase
+#
+# import pandas as pd
+# import Projects.INTEG21.Tests.test_data as Data
+#
+# __author__ = 'israels'
+#
+#
+# class PNGJP_SAND2TestRbusSand(TestCase):
+#
+#     @mock.patch('Projects.INTEG21.Utils.KpiQualitative.ProjectConnector')
+#     def setUp(self, x):
+#         self.data_provider_mock = MagicMock()
+#         self.data_provider_mock.project_name = 'integ21'
+#         self.data_provider_mock.rds_conn = MagicMock()
+#         self.output = MagicMock()
+#         # self.test_objects = PNGJP_SAND2TestRbusSand()
+#         # self.calc = RBUSCalculations(self.data_provider_mock, self.output)
+#         # self.generator = RBUSGenerator(self.data_provider_mock, self.output)
+#         # self.tool = RBUSToolBox(self.data_provider_mock, self.output)
+#         self.data = Data
+#         self.tool_box = INTEG21KpiQualitative_ToolBox(self.data_provider_mock, self.output)
+#         # self.checks = Checks(self.data_provider_mock)
+#
+#
+# class PNGJP_SAND2TestPlacementCount(PNGJP_SAND2TestRbusSand):
+#
+#     def test_df_head(self):
+#         df = self.data.get_scene_data_head()
+#         self.assertEquals(len(df.loc[df.template_name == 'ADDITIONAL AMBIENT PLACEMENT']),
+#                          self.tool_box.get_scene_count(df, 'ADDITIONAL AMBIENT PLACEMENT'))
+#
+#     def test_num_of_scene_types_ambient(self):
+#         df = self.data.get_scene_data_complete()
+#         self.assertEquals(len(df.loc[df.template_name == 'ADDITIONAL AMBIENT PLACEMENT']),
+#                           self.tool_box.get_scene_count(df, 'ADDITIONAL AMBIENT PLACEMENT'))
+#
+#     def test_num_of_scene_types_main_placement(self):
+#         df = self.data.get_scene_data_complete()
+#         self.assertEquals(len(df.loc[df.template_name == 'MAIN PLACEMENT']),
+#                           self.tool_box.get_scene_count(df, 'MAIN PLACEMENT'))
+#
+#     def test_num_of_scene_types_non_exits(self):
+#         df = self.data.get_scene_data_complete()
+#         self.assertEquals(len(df.loc[df.template_name == 'ABC']),
+#                           self.tool_box.get_scene_count(df, 'ABC'))
+#
+#
+# class PNGJP_SAND2TestShelfOccupation(PNGJP_SAND2TestRbusSand):
+#     def test_get_atomic_kpi(self):
+#         kpi1 = 'K004'
+#         kpi2 = 'K005'
+#         df = self.data.get_atomic_fk()
+#         self.assertLess(df[df['atomic_kpi_name'] == kpi1][KpiResults.ATOMIC_KPI_FK].values[0],
+#                         df[df['atomic_kpi_name'] == kpi2][KpiResults.ATOMIC_KPI_FK].values[0])
+#
+#
+# if __name__ == '__main__':
+#     placement_count_tester = PNGJP_SAND2TestPlacementCount()
+#     placement_count_tester.test_df_head()
+#     placement_count_tester.test_num_of_scene_types_ambient()
+#     placement_count_tester.test_num_of_scene_types_main_placement()
+#     placement_count_tester.test_num_of_scene_types_non_exits()
+#
+#     shelf_occupation_tester = PNGJP_SAND2TestShelfOccupation()
+#     shelf_occupation_tester.test_get_atomic_kpi()
