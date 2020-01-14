6e7e30e52d2b62b484de70c6f951acae766bcd03
ilay_s <ilays@traxretail.com>
2020-01-14 09:42:12 +0200

    fix pngro test

--- Projects/PNGRO/Tests/Data/kpi_results.py
+++ Projects/PNGRO/Tests/Data/kpi_results.py
@@ -12,7 +12,7 @@ class PNGROKpiResults:
                 0: None, 1: None}, 'client_name': {
                     0: u'PSKUs Assortment', 1: u'PSKUs Assortment - SKU'}, 'session_fk': {
                         0: 251632, 1: 251632}, 'numerator_result': {
-                            0: 119, 1: 0}, 'denominator_result': {
+                            0: 118, 1: 0}, 'denominator_result': {
                                 0: 290, 1: 1}, 'result': {
-                                    0: 41.0345, 1: 0.0}, 'denominator_id': {
+                                    0: 40.6897, 1: 0.0}, 'denominator_id': {
                                         0: None, 1: 863.0}}
