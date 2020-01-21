6696899f7ee77698b93d02d70c1f844448deb5c7
limor_c <limorc@traxretail.com>
2020-01-21 10:38:51 +0200

    live assortment

--- Projects/CCUSLIVEDEMO/LiveSessionKpis/Assortment.py
+++ Projects/CCUSLIVEDEMO/LiveSessionKpis/Assortment.py
@@ -1,17 +1,19 @@
 import pandas as pd
 from datetime import datetime
-
 from Projects.CCUSLIVEDEMO.LiveSessionKpis.PSAssortmentProvider import LiveAssortmentDataProvider
+
 
 class LiveAssortmentCalculation:
     LVL3_HEADERS = ['assortment_group_fk', 'assortment_fk', 'target', 'product_fk', 'kpi_fk_lvl1',
-                             'kpi_fk_lvl2', 'kpi_fk_lvl3', 'group_target_date', 'assortment_super_group_fk',
-                             'super_group_target', 'additional_attributes']
+                    'kpi_fk_lvl2', 'kpi_fk_lvl3', 'group_target_date', 'assortment_super_group_fk',
+                    'super_group_target', 'additional_attributes']
     ASSORTMENT_FK = 'assortment_fk'
     ASSORTMENT_GROUP_FK = 'assortment_group_fk'
     ASSORTMENT_SUPER_GROUP_FK = 'assortment_super_group_fk'
     LVL2_HEADERS = ['assortment_super_group_fk', 'assortment_group_fk', 'assortment_fk', 'target',
                     'kpi_fk_lvl1', 'kpi_fk_lvl2', 'group_target_date', 'super_group_target']
+    EMPTY_VAL = -1
+    LVL1_HEADERS = ['assortment_super_group_fk', 'assortment_group_fk', 'super_group_target', 'kpi_fk_lvl1']
 
     def __init__(self, data_provider):
         self.store_assortment = LiveAssortmentDataProvider(data_provider).execute()
@@ -25,8 +27,9 @@ class LiveAssortmentCalculation:
         """
         if self.store_assortment.empty:
             return pd.DataFrame()
-        assortment_result = self.store_assortment.loc[self.store_assortment['start_date'] <= self.current_date][self.LVL3_HEADERS]
-        assortment_result['in_store'] = 0
+        assortment_result = self.store_assortment.loc[self.store_assortment['start_date'] <=
+                                                      self.current_date][self.LVL3_HEADERS]
+        assortment_result.loc[:, 'in_store'] = 0
         return assortment_result.drop_duplicates(subset=[self.ASSORTMENT_GROUP_FK, self.ASSORTMENT_FK, 'product_fk'],
                                                  keep='last')
 
@@ -57,6 +60,21 @@ class LiveAssortmentCalculation:
         Indicates for each assortment group how many products were in the store (passes) out of the total\ target
         (total\ target).
         """
-        lvl2_res = lvl3_assortment.groupby([self.ASSORTMENT_FK, self.ASSORTMENT_GROUP_FK])[self.LVL2_HEADERS].agg(
-            passes=('in_store', 'sum'), total=('in_store', 'count')).reset_index()
+        lvl3_res = lvl3_assortment.copy()
+        lvl3_res = lvl3_res.fillna(self.EMPTY_VAL)
+        lvl2_res = lvl3_res.groupby(self.LVL2_HEADERS)['in_store'].agg([('total', 'sum'), ('passes', 'count')]).reset_index()
         return lvl2_res
+
+    def calculate_lvl_1_assortment(self, lvl2_assortment):
+        ass_super = lvl2_assortment[~lvl2_assortment['kpi_fk_lvl1'] == self.EMPTY_VAL]
+        ass_super = ass_super.groupby(self.LVL1_HEADERS).agg({'total': 'count', 'passes': 'sum'}).reset_index()
+        if ass_super.empty:
+            return ass_super
+        ass_super.loc[:, 'passes'] = ass_super.apply(lambda row: 1 if row.passes > row.super_group_target else 0, axis=1)
+        return ass_super
+
+
+
+
+
+
--- Projects/CCUSLIVEDEMO/LiveSessionKpis/Calculation.py
+++ Projects/CCUSLIVEDEMO/LiveSessionKpis/Calculation.py
@@ -1,21 +1,101 @@
+import pandas as pd
+from datetime import datetime
 from Trax.Apps.Services.KEngine.Handlers.Utils.Scripts import LiveSessionBaseClass
 from KPIUtils_v2.GlobalDataProvider.PSAssortmentProvider import PSAssortmentDataProvider
 from Projects.CCUSLIVEDEMO.LiveSessionKpis.PSAssortmentProvider import LiveAssortmentDataProvider
 from Projects.CCUSLIVEDEMO.LiveSessionKpis.Assortment import LiveAssortmentCalculation
+from KPIUtils_v2.DB.LiveCommon import LiveCommon
+
 
 class CalculateKpi(LiveSessionBaseClass):
+
+    SESSION_RESULTS_COL = ['fk', 'numerator_id', 'numerator_result', 'denominator_id',
+                                 'denominator_result', 'result', 'score']
+    LVL2_SESSION_RESULTS_COL = ['fk', 'numerator_id', 'numerator_result', 'denominator_id',
+                                'denominator_result', 'result', 'target', 'score']
+
+    def __init__(self, data_provider):
+        LiveSessionBaseClass.__init__(self, data_provider)
+        self.common = LiveCommon(data_provider)
+        self.store_fk = data_provider.store_fk
+        self.current_date = datetime.now()
+        self.assortment = LiveAssortmentCalculation(data_provider)
+
     def calculate_session_live_kpi(self):
-        data_provider = self._data_provider
-        # store_assortment = PSAssortmentDataProvider(data_provider).execute()
-        matche = data_provider.matches
-        scif = data_provider.scene_item_facts
-        assortment = LiveAssortmentCalculation(data_provider)
-        a= assortment.get_lvl3_relevant_ass()
-        b= assortment.calculate_lvl3_assortment(False)
-        c = assortment.calculate_lvl2_assortment(b)
 
-        print("ok")
+        # getting results
+        lvl3_result = self.assortment.calculate_lvl3_assortment(False)
+        lvl_2_result = self.assortment.calculate_lvl2_assortment(lvl3_result)
 
-    #
-    # def assortment(self):
-    #     data_provider = self._data_provider
+        dist_result = self.assortment_sku_level(lvl3_result.copy())
+        dist_results_sku = dist_result.to_dict('records')
+        self.common.save_json_to_new_tables_sessions(dist_results_sku)
+
+        # oos_sku_res = self.oos_sku(dist_result)
+        # common.save_json_to_new_tables_sessions(oos_sku_res)
+
+        lvl_2_result.rename(columns={'passes': 'numerator_result', 'total': 'denominator_result', 'kpi_fk_lvl2': 'fk'},
+                            inplace=True)
+        dist_results_lvl2 = self.assortment_group_level(lvl_2_result)
+        dist_results_lvl2 = dist_results_lvl2.to_dict('records')
+        self.common.save_json_to_new_tables_sessions(dist_results_lvl2)
+
+        # oos_group_res = self.oos_group_level(lvl_2_result)
+        # self.common.save_json_to_new_tables_sessions(oos_group_res)
+
+        self.common.commit_live_queries()
+
+    def assortment_group_level(self, lvl_2_result):
+        lvl_2_result = lvl_2_result.copy()
+        lvl_2_result.loc[lvl_2_result['target'] == -1, 'target'] = None
+        lvl_2_result.loc[:, 'denominator_result'] = \
+            lvl_2_result.apply(lambda row: row['target'] if (row['target'] >= 0 and row['group_target_date'] >
+                                                             self.current_date) else row['denominator_result'], axis=1)
+        lvl_2_result.loc[:, 'result'] = lvl_2_result.numerator_result / lvl_2_result.denominator_result
+        self.manipulate_result_row(lvl_2_result)
+        lvl_2_result.loc[:, 'score'] = lvl_2_result['result']
+        manufacturer_fk = 1
+        lvl_2_result.loc[:, 'denominator_id'] = self.store_fk
+        lvl_2_result.loc[:, 'numerator_id'] = manufacturer_fk
+        lvl_2_result = lvl_2_result[self.LVL2_SESSION_RESULTS_COL]
+
+        return lvl_2_result
+
+    def assortment_sku_level(self, lvl_3_result):
+
+        lvl_3_result.rename(columns={'product_fk': 'numerator_id', 'assortment_group_fk': 'denominator_id',
+                                     'in_store': 'result', 'kpi_fk_lvl3': 'fk'}, inplace=True)
+        lvl_3_result.loc[:, 'result'] *= 100
+        lvl_3_result = lvl_3_result.assign(numerator_result=lvl_3_result['result'],
+                                           denominator_result=lvl_3_result['result'],
+                                           score=lvl_3_result['result'])
+        lvl_3_result = lvl_3_result[self.SESSION_RESULTS_COL]
+
+        return lvl_3_result
+
+    # todo complete it
+    def oos_sku(self, lvl_3_result):
+
+        # filter distrubution kpis
+
+        oos_results = lvl_3_result.loc[lvl_3_result['result'] == 0]
+        oos_results.loc[:, 'fk'] = self.get_kpi_fk('OOS_sku')
+
+    def oos_group_level(self, lvl_2_result):
+        lvl_2_result = lvl_2_result.copy()
+        lvl_2_result.loc[:, 'numerator_result'] = lvl_2_result['denominator_result'] - lvl_2_result['numerator_result']
+        lvl_2_result.loc[:, 'result'] = lvl_2_result.numerator_result / lvl_2_result.denominator_result
+        self.manipulate_result_row(lvl_2_result)
+        return lvl_2_result
+
+    @staticmethod
+    def manipulate_result_row(df):
+        df[df.result > 100] = 100
+        df.loc[:, 'result'] = round(df['result'], 2) * 100
+
+    def get_kpi_fk(self):
+        return 5
+
+
+
+
