2605ec851d696b69fb6d56f69aa996e137c7ef0d
kristati <krishnat@traxretail.com>
2020-01-30 10:26:10 -0500

    test

--- Projects/CCNAYARMX/Fondas/Utils/KPIToolBox.py
+++ Projects/CCNAYARMX/Fondas/Utils/KPIToolBox.py
@@ -28,6 +28,7 @@ PARENT_KPI = 'Parent KPI'
 TASK_TEMPLATE_GROUP = 'Task/ Template Group'
 TEMPLATE_NAME = 'template_name'
 MANUFACTURER_NAME = 'manufacturer_name'
+RELEVANT_QUESTION_FK = 'question_fk'
 
 # Excel Sheets
 SHEETS = [KPIS, AVALIABILITY, SOS, SHARE_OF_EMPTY, SURVEY, DISTRIBUTION, ASSORTMENT, SCORING]
@@ -60,6 +61,10 @@ class FONDASToolBox(GlobalSessionToolBox):
         self.store_type = self.store_info['store_type'].iloc[0]
         self.templates = {}
         self.parse_template()
+        self.own_manuf_fk = int(self.data_provider.own_manufacturer.param_value.values[0])
+        self.survey_response = self.data_provider[Data.SURVEY_RESPONSES]
+        # self.survey = Survey(self.data_provider, output=output, ps_data_provider=self.ps_data_provider,
+        #                      common=self.common_v2)
         self.results_df = pd.DataFrame(columns=['kpi_name', 'kpi_fk', 'numerator_id', 'numerator_result',
                                                 'denominator_id', 'denominator_result', 'result', 'score',
                                                 'identifier_result', 'identifier_parent', 'should_enter'])
@@ -70,32 +75,108 @@ class FONDASToolBox(GlobalSessionToolBox):
 
     def main_calculation(self):
         relevant_kpi_template = self.templates[KPIS]
-        foundation_kpi_types = [SURVEY]
+        foundation_kpi_types = [DISTRIBUTION]
         foundation_kpi_template = relevant_kpi_template[relevant_kpi_template[KPI_TYPE].isin(foundation_kpi_types)]
 
         self._calculate_kpis_from_template(foundation_kpi_template)
 
-        def _calculate_kpis_from_template(self, template_df):
-            for i, row in template_df.iterrows():
-                calculation_function = self._get_calculation_function_by_kpi_type(row[KPI_TYPE])
+    def _calculate_kpis_from_template(self, template_df):
+        for i, row in template_df.iterrows():
+            calculation_function = self._get_calculation_function_by_kpi_type(row[KPI_TYPE])
+            try:
+                kpi_row = self.templates[row[KPI_TYPE]][
+                    self.templates[row[KPI_TYPE]][KPI_NAME].str.encode('utf-8') == row[KPI_NAME].encode('utf-8')].iloc[
+                    0]
+            except IndexError:
+                pass
+            result_data = calculation_function(kpi_row)
+            if result_data:
+                if isinstance(result_data, dict):
+                    weight = row['Score']
+                    if weight and pd.notna(weight) and pd.notna(result_data['result']):
+                        if row[KPI_TYPE] == SCORING and 'score' not in result_data.keys():
+                            result_data['score'] = weight * result_data['result']
+                        elif row[KPI_TYPE] != SCORING:
+                            result_data['score'] = weight * result_data['result']
+                    parent_kpi_name = self._get_parent_name_from_kpi_name(result_data['kpi_name'])
+                    if parent_kpi_name and 'identifier_parent' not in result_data.keys():
+                        result_data['identifier_parent'] = parent_kpi_name
+                    if 'identifier_result' not in result_data.keys():
+                        result_data['identifier_result'] = result_data['kpi_name']
+                    if result_data['result'] <= 1:
+                        result_data['result'] = result_data['result'] * 100
+                    self.results_df.loc[len(self.results_df), result_data.keys()] = result_data
+                else:  # must be a list
+                    for result in result_data:
+                        weight = row['Score']
+                        if weight and pd.notna(weight) and pd.notna(result['result']):
+                            if row[KPI_TYPE] == SCORING and 'score' not in result.keys():
+                                result['score'] = weight * result['result']
+                            elif row[KPI_TYPE] != SCORING:
+                                result['score'] = weight * result['result']
+                        parent_kpi_name = self._get_parent_name_from_kpi_name(result['kpi_name'])
+                        if parent_kpi_name and 'identifier_parent' not in result.keys():
+                            result['identifier_parent'] = parent_kpi_name
+                        if 'identifier_result' not in result.keys():
+                            result['identifier_result'] = result['kpi_name']
+                        if result['result'] <= 1:
+                            result['result'] = result['result'] * 100
+                        self.results_df.loc[len(self.results_df), result.keys()] = result
+    def _get_calculation_function_by_kpi_type(self, kpi_type):
+        if kpi_type == SOS:
+            return self.calculate_sos
+        elif kpi_type == AVALIABILITY:
+            return self.calculate_availability
+        elif kpi_type == SURVEY:
+            return self.calculate_survey
+        elif kpi_type == DISTRIBUTION:
+            return self.calculate_assortment
+        elif kpi_type == SHARE_OF_EMPTY:
+            return self.calculate_share_of_empty
+        elif kpi_type == SCORING:
+            return self.calculate_scoring
 
-        def _get_calculation_function_by_kpi_type(self, kpi_type):
-            if kpi_type == SOS:
-                return self.calculate_sos
-            elif kpi_type == AVALIABILITY:
-                return self.calculate_availability
-            elif kpi_type == SURVEY:
-                return self.calculate_survey
-            elif kpi_type == DISTRIBUTION:
-                return self.calculate_assortment
-            elif kpi_type == SHARE_OF_EMPTY:
-                return self.calculate_share_of_empty
-            elif kpi_type == SCORING:
-                return self.calculate_scoring
+    def calculate_survey(self, row):
+        kpi_name = row[KPI_NAME]
+        kpi_fk = self.common.get_kpi_fk_by_kpi_type(kpi_name)
+        relevant_question_fk = row[RELEVANT_QUESTION_FK]
 
-        def calculate_survey(self, row):
-            b = 2
-            a = row
-            b = self.templates
+        relevant_survey_row = self.survey_response[self.survey_response.question_fk.isin([relevant_question_fk])]
+        survey_answer = relevant_survey_row.selected_option_text.iat[0]
+
+        numerator_id = self.own_manuf_fk
+        denominator_id = self.store_id
+        result = 1 if survey_answer == 'Si' else 0
+
+        result_dict = {'kpi_name': kpi_name, 'kpi_fk': kpi_fk, 'numerator_id': numerator_id,
+                       'denominator_id': denominator_id,
+                       'result': result}
+        return result_dict
+
+    def calculate_assortment(self, row):
+        kpi_name = row[KPI_NAME]
+        kpi_fk = self.common.get_kpi_fk_by_kpi_type(kpi_name)
+
+
+
+    def _get_parent_name_from_kpi_name(self, kpi_name):
+        template = self.templates[KPIS]
+        parent_kpi_name = \
+            template[template[KPI_NAME].str.encode('utf-8') == kpi_name.encode('utf-8')][PARENT_KPI].iloc[0]
+        if parent_kpi_name and pd.notna(parent_kpi_name):
+            return parent_kpi_name
+        else:
+            return None
+
+    @staticmethod
+    def sanitize_values(item):
+        if pd.isna(item):
+            return item
+        else:
+            if type(item) == int:
+                return str(item)
+            else:
+                items = [x.strip() for x in item.split(',')]
+                return items
 
 
