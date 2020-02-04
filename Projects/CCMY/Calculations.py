eceb828dfe75fb6759832f2b57b36e09953c4be4
sathiyanarayanand <sathiyanarayanan@traxretail.com>
2020-02-04 22:16:58 +0800

    PROS-12225 - CCMY sanity tests

--- Projects/CCMY/Calculations.py
+++ Projects/CCMY/Calculations.py
@@ -1,8 +1,8 @@
 
 from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-# from Trax.Utils.Conf.Configuration import Config
-# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Trax.Utils.Conf.Configuration import Config
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 from Projects.CCMY.KPIGenerator import CCMYGenerator
 
@@ -16,14 +16,14 @@ class CCMYCalculations(BaseCalculationsScript):
         self.timer.stop('KPIGenerator.run_project_calculations')
 
 
-# if __name__ == '__main__':
-#     LoggerInitializer.init('ccmy calculations')
-#     Config.init()
-#     project_name = 'ccmy'
-#     data_provider = KEngineDataProvider(project_name)
-#     sessions = ['41584c7b-a9b8-43dd-b055-f0db7534af4b']
-#
-#     for session in sessions:
-#         data_provider.load_session_data(session)
-#         output = Output()
-#         CCMYCalculations(data_provider, output).run_project_calculations()
+if __name__ == '__main__':
+    LoggerInitializer.init('ccmy calculations')
+    Config.init()
+    project_name = 'ccmy'
+    data_provider = KEngineDataProvider(project_name)
+    sessions = ['afa3b599-8228-4ebf-9409-14d2269a01f8']
+
+    for session in sessions:
+        data_provider.load_session_data(session)
+        output = Output()
+        CCMYCalculations(data_provider, output).run_project_calculations()
