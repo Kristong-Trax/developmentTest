569121fb249cc8377cadef0c412ae4e704a8ff49
nidhinb <nidhinb@traxretail.com>
2020-02-04 10:22:15 +0800

    added changes in sand env

--- Projects/LIONNZ_SAND/LocalCalculations.py
+++ Projects/LIONNZ_SAND/LocalCalculations.py
@@ -2,15 +2,15 @@
 from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
 from Trax.Utils.Conf.Configuration import Config
 from Projects.LIONNZ_SAND.Calculations import Calculations
+from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 
 if __name__ == '__main__':
-    # LoggerInitializer.init('lionnz-sand calculations')
+    LoggerInitializer.init('lionnz-sand calculations')
     Config.init()
     project_name = 'lionnz-sand'
     data_provider = KEngineDataProvider(project_name)
     sessions = [
-        '29ffddd9-8114-4249-b9ec-76c2db070a9f',
-        'abe00951-82b0-4e2b-942d-310a1ea04c14',
+        '944A33D9-125B-4B6C-A628-DF3630C3FE19',
     ]
     for session in sessions:
         print "Running session >>", session
