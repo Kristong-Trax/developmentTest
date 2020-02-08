262a6899e60a3c1de965e0a2f90dc0eb3b5a2f45
idanre <idanr@traxretail.com>
2020-02-08 09:35:40 -0500

    configure PS sand Envs UK--->PS1 IE--->PS2 IT--->PS3

--- Projects/PS3_SAND/Utils/KPIToolBox.py
+++ Projects/PS3_SAND/Utils/KPIToolBox.py
@@ -1,14 +1,12 @@
 from Trax.Algo.Calculations.Core.DataProvider import Data
-import os
 from KPIUtils.GlobalProjects.DIAGEO.KPIGenerator import DIAGEOGenerator
 from KPIUtils.DB.Common import Common
 from KPIUtils_v2.DB.CommonV2 import Common as CommonV2
 
 
-class PS2SandToolBox:
+class PS3SandToolBox:
 
     def __init__(self, data_provider, output):
-        self.output = output
         self.data_provider = data_provider
         self.store_id = self.data_provider[Data.STORE_FK]
         self.scif = self.data_provider[Data.SCENE_ITEM_FACTS]
@@ -20,7 +18,7 @@ class PS2SandToolBox:
     def main_calculation(self):
         """
         This function calculates the KPI results.
-         """
+        """
         # SOS Out Of The Box kpis
         self.diageo_generator.activate_ootb_kpis(self.commonV2)
 
@@ -34,13 +32,6 @@ class PS2SandToolBox:
         # Global Menu kpis
         menus_res = self.diageo_generator.diageo_global_new_share_of_menu_function()
         self.commonV2.save_json_to_new_tables(menus_res)
-
-        # Global Tap Brand Score
-        template_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
-                                     'Data', 'Brand Score.xlsx')
-        res_dict = self.diageo_generator.diageo_global_tap_brand_score_function(template_path, save_to_tables=False,
-                                                                                calculate_components=True)
-        self.commonV2.save_json_to_new_tables(res_dict)
 
         # Global Visible to Consumer function
         sku_list = filter(None, self.scif[self.scif['product_type'] == 'SKU'].product_ean_code.tolist())
