b13e189adace8a58a7192dc7b1743d7dffe4674e
sergeypol <sergey@traxretail.com>
2020-02-14 13:40:06 +0300

    PROS-13075 - CCRU - POS KPI 2020 change - to SAND

--- Projects/CCRU_SAND/LocalCalculations.py
+++ Projects/CCRU_SAND/LocalCalculations.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     data_provider = KEngineDataProvider(project_name)
     session_uids = \
         [
-            "FEF2A68D-E934-48C9-AED7-FCCC847E5815",
+            "F26E2E6B-D12B-415C-AC0C-CAB929BEFC9F",
 
             # "f97d6aeb-1e6c-4f41-9da0-935f3a7bcb01",
             # "f2e5eb75-3bef-43d6-b195-02099f60c48b",
