23ed29a3cc3ad4e0b685afcf2e3a533c26e7c294
nicolaske <nicolaske@traxretail.com>
2020-02-18 11:31:52 -0500

    new project

---
+++ Projects/HEINEKENMX/Bebidas/Utils/Const.py
@@ -0,0 +1,11 @@
+import os
+__author__ = 'Nicolas'
+
+
+class Const(object):
+    SOS_EXCLUDE_FILTERS = {'product_type': ['Irrelevant', 'Empty']}
+    ALLOWED_FILTERS = {'product_type': ['Other', 'Empty']}
+    IGN_STACKING = {"stacking_layer": 1}
+
+
+
