842139a19f09387b91a63aeb11644c044dcaabe5
natalya_k <natalyak@traxretail.com>
2020-02-19 19:14:42 +0200

    clnup

--- Projects/BATRU/LocalCalculations.py
+++ Projects/BATRU/LocalCalculations.py
@@ -19,4 +19,3 @@ if __name__ == '__main__':
         data_provider.load_session_data(session)
         output = Output()
         BATRUCalculations(data_provider, output).run_project_calculations()
-git
