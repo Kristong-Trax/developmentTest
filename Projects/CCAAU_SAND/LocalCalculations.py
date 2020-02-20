000393d804c41bed419e748bc6b49671a3283b54
nidhinb <nidhinb@traxretail.com>
2020-02-20 13:07:45 +0800

    local calc file

--- Projects/CCAAU_SAND/LocalCalculations.py
+++ Projects/CCAAU_SAND/LocalCalculations.py
@@ -11,22 +11,20 @@ if __name__ == '__main__':
     Config.init()
     project_name = 'ccaau-sand'
     data_provider = KEngineDataProvider(project_name)
+    # DST_Group_NW_SI -- '012C162C-B252-4F09-A5C0-3FB714EDDEFE'
+    # DST_Group_FS_SI -- '05DB1C22-AD3B-49E1-A004-6EFE70A1E519'
+    # DST_Group_BF_SI -- '08CDAE94-6765-4F60-B295-E851801E92FC'
+    # DST_Group_RW_SI -- '730BE6BE-FBE5-41C9-8E34-5C79C70D5E85'
     sessions = [
-        '012C162C-B252-4F09-A5C0-3FB714EDDEFE',
-        "08CDAE94-6765-4F60-B295-E851801E92FC",
-        "730BE6BE-FBE5-41C9-8E34-5C79C70D5E85",
+        '8ab44317-3dbc-4170-bc0b-f389bd407902',  # prev of 3FB714EDDEFE
+        # '012C162C-B252-4F09-A5C0-3FB714EDDEFE',
+        # "08CDAE94-6765-4F60-B295-E851801E92FC",
+        # "730BE6BE-FBE5-41C9-8E34-5C79C70D5E85",
         # excluded '05DB1C22-AD3B-49E1-A004-6EFE70A1E519', '2633487', 'CCA-Standard checkout cooler'
-        "05DB1C22-AD3B-49E1-A004-6EFE70A1E519",
+        # "05DB1C22-AD3B-49E1-A004-6EFE70A1E519",
     ]
     for each_sess in sessions:
         print("Running for session ", each_sess)
         data_provider.load_session_data(each_sess)
         output = Output()
         Calculations(data_provider, output).run_project_calculations()
-
-
-
-# DST_Group_NW_SI -- '012C162C-B252-4F09-A5C0-3FB714EDDEFE'
-# DST_Group_FS_SI -- '05DB1C22-AD3B-49E1-A004-6EFE70A1E519'
-# DST_Group_BF_SI -- '08CDAE94-6765-4F60-B295-E851801E92FC'
-# DST_Group_RW_SI -- '730BE6BE-FBE5-41C9-8E34-5C79C70D5E85'
