b24eff09e69a43adab78d4016bfe71767bac5bfc
kristati <krishnat@traxretail.com>
2020-03-02 11:57:56 -0500

    Updated the Template and added a kpi

--- Projects/NESTLEBEVUS/Data/LocalConsts.py
+++ Projects/NESTLEBEVUS/Data/LocalConsts.py
@@ -4,10 +4,16 @@ class Consts(object):
     KPIS = 'KPIs'
     SOS = 'SOS'
     DISTRIBUTION = 'Distribution'
+    ADJACENCY_BRAND_WITHIN_BAY = 'Adjacency Brand within Bay'
 
     # Excel Column Names
     KPI_NAME = 'KPI Name'
     KPI_TYPE = 'KPI Type'
     OUTPUT = 'Output'
+
+    #Scif Columns
+    BRAND_FK = 'brand_fk'
+    SCENE_FK = 'scene_fk'
+    BAY_NUMBER = 'bay_number'
 
     pass
