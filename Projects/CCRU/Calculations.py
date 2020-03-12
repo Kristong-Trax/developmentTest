6a103497710f8fef6fc96646df5ae6bc72332588
sergeypol <sergey@traxretail.com>
2020-03-12 12:44:14 +0300

    Sanity test fix

--- Projects/CCRU/Calculations.py
+++ Projects/CCRU/Calculations.py
@@ -76,7 +76,6 @@ class CCRUProjectCalculations:
             Log.warning('Warning. Session with Segmentation visit type has no KPI calculations.')
 
         elif self.tool_box.visit_type in [self.tool_box.PROMO_VISIT]:
-            Log.warning('Warning. Session with Promo visit type has no KPI calculations.')
             self.calculate_promo_compliance()
 
         else:
