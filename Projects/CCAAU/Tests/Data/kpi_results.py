05b23291bb9e4eb0a993c6107578e4d245d7f654
PrasannaVeerapandi <prasanna.veerapandi@traxretail.com>
2020-03-31 16:21:06 +0800

    [PROS-14584] CCAAU Sanity_test Added:

---
+++ Projects/CCAAU/Tests/Data/kpi_results.py
@@ -0,0 +1,18 @@
+
+class CCAAUKpiResults:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_kpi_results():
+        return {
+            'numerator_id': {
+                0: 0, 1: 0}, 'kpi_level_2_fk': {
+                0: 1152, 1: 1153}, 'context_id': {
+                0: None, 1: None}, 'client_name': {
+                    0: u'FACINGS_SOS_SCENE_TYPE_BY_MANUFACTURER', 1: u'LINEAR_SOS_SCENE_TYPE_BY_MANUFACTURER'}, 'session_fk': {
+                        0: 7151446, 1: 7151446}, 'numerator_result': {
+                            0: 46, 1: 3836}, 'denominator_result': {
+                                0: 46, 1: 3836}, 'result': {
+                                    0: 100.0, 1: 100.0}, 'denominator_id': {
+                                        0: 514, 1: 514}}
