bf6ebe59ab3ebac7b6fc11bf3e82839afe9e73e5
Trevaris Smail <trevaris.small@traxretail.com>
2020-04-02 12:13:05 -0400

    Add retrieval of test sessions from excel file

--- Projects/NESTLEUS/LocalCalculations.py
+++ Projects/NESTLEUS/LocalCalculations.py
@@ -2,7 +2,11 @@
 from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
 from Trax.Utils.Conf.Configuration import Config
 from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+
 from Projects.NESTLEUS.Calculations import Calculations
+from Projects.NESTLEUS.Utils import Const
+
+import pandas as pd
 
 
 if __name__ == '__main__':
@@ -10,7 +14,11 @@ if __name__ == '__main__':
     Config.init()
     project_name = 'nestleus'
     data_provider = KEngineDataProvider(project_name)
+
+    test_sessions = pd.read_excel(Const.TEST_SESSIONS_PATH)
     session = 'ff474ba2-fb55-422a-adb1-44ae3410bbf3' #'9bffe5d5-ee61-4246-b545-560c2acfbb1b'
-    data_provider.load_session_data(session)
-    output = Output()
-    Calculations(data_provider, output).run_project_calculations()
+
+    for session in test_sessions.itertuples():
+        data_provider.load_session_data(session.Session_uid)
+        output = Output()
+        Calculations(data_provider, output).run_project_calculations()
