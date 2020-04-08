736c1a2598fb612154f8e92d5aaaa293dd6570ca
ilay_s <ilays@traxretail.com>
2020-04-08 11:41:27 +0300

    Adding sanity tests to daogeoau, diageobenelux, diageoes, diageoga, diageogr, daigeoie, diageoit, diageonordics, diageopl, diageopt, daigeoru

---
+++ Projects/DIAGEOAU/Tests/test_functional_diageoau_sanity.py
@@ -0,0 +1,44 @@
+
+
+from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+from Projects.DIAGEOAU.Tests.Data.data_test_diageoau_sanity import ProjectsSanityData
+from Projects.DIAGEOAU.Calculations import DIAGEOAUCalculations
+from DevloperTools.SanityTests.PsSanityTests import PsSanityTestsFuncs
+from Projects.DIAGEOAU.Tests.Data.kpi_results import DIAGEOAUKpiResults
+# import os
+# import json
+
+__author__ = 'ilays'
+
+
+class TestKEnginePsCode(PsSanityTestsFuncs):
+
+    def add_mocks(self):
+        # with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Data', 'Relative Position.txt'),
+        #           'rb') as f:
+        #     relative_position_template = json.load(f)
+        # self.mock_object('save_latest_templates',
+        #                  path='KPIUtils.GlobalProjects.DIAGEO.Utils.TemplatesUtil.TemplateHandler')
+        # self.mock_object('download_template',
+        #                  path='KPIUtils.GlobalProjects.DIAGEO.Utils.TemplatesUtil.TemplateHandler').return_value = \In
+        #     relative_position_template
+        return
+
+    @PsSanityTestsFuncs.seeder.seed(["diageoau_seed", "mongodb_products_and_brands_seed"], ProjectsSanityData())
+    def test_diageoau_sanity(self):
+        self.add_mocks()
+        project_name = ProjectsSanityData.project_name
+        data_provider = KEngineDataProvider(project_name)
+        sessions = {'7AD9790F-E0B2-4518-8567-005A5ABA92E8': []}
+        kpi_results = DIAGEOAUKpiResults().get_kpi_results()
+        for session in sessions.keys():
+            data_provider.load_session_data(str(session))
+            output = Output()
+            DIAGEOAUCalculations(data_provider, output).run_project_calculations()
+            # for scene in sessions[session]:
+            # data_provider.load_scene_data(str(session), scene_id=scene)
+            # SceneCalculations(data_provider).calculate_kpis()
+        self._assert_DIAGEO_test_results_matches_reality(kpi_results)
+        # self._assert_old_tables_kpi_results_filled()
+        # self._assert_new_tables_kpi_results_filled(distinct_kpis_num=None, list_of_kpi_names=None)
+        # self._assert_scene_tables_kpi_results_filled(distinct_kpis_num=None)
