ec27179934a5274cb001dd3d33cd140c90eb0278
sergeypol <sergey@traxretail.com>
2020-04-09 15:46:36 +0300

    DNP KPI - sanity test

--- Projects/BATRU/LocalCalculations.py
+++ Projects/BATRU/LocalCalculations.py
@@ -9,9 +9,12 @@ if __name__ == '__main__':
     LoggerInitializer.init('BATRU calculations')
     Config.init()
 
-    project_name = 'batru-sand'
+    project_name = 'batru'
     data_provider = KEngineDataProvider(project_name)
-    sessions = ['ffe79c33-ca54-40c1-854b-535458ce567b']
+    sessions = [
+        'AF410EF7-90D8-431A-AF12-60DE6ED7438B',
+        # 'ffe79c33-ca54-40c1-854b-535458ce567b'
+    ]
     for session in sessions:
         data_provider.load_session_data(session)
         output = Output()
