1298066bf898b81063455effd9560fb6f367a462
nidhinb <nidhinb@traxretail.com>
2020-04-15 11:42:08 +0800

    fixing local calc

--- Projects/CARLSBRGHK_SAND/LocalCalculations.py
+++ Projects/CARLSBRGHK_SAND/LocalCalculations.py
@@ -8,9 +8,9 @@ from Projects.CARLSBRGHK_SAND.Calculations import Calculations
 if __name__ == '__main__':
     LoggerInitializer.init('carlsberg-sand calculations')
     Config.init()
-    project_name = 'carlsberg-sand'
+    project_name = 'carlsbrghk-sand'
     data_provider = KEngineDataProvider(project_name)
-    session = ''
+    session = 'e1b28eca-4cfb-423d-8c0e-379428e14a76'
     data_provider.load_session_data(session)
     output = Output()
     Calculations(data_provider, output).run_project_calculations()
