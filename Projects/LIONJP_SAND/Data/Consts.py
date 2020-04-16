8e2b9f58653f9290db498ba44bae12486689cd1e
sathiyanarayanand <sathiyanarayanan@traxretail.com>
2020-04-16 11:51:55 +0800

    PROS-14717 LionJP adjacency KPI

---
+++ Projects/LIONJP_SAND/Data/Consts.py
@@ -0,0 +1,18 @@
+
+class Consts:
+    KPI_SHEET_NAME = 'kpi_list'
+    PS_KPI_FAMILY = 19
+    KPI_FAMILY = 'kpi_family_fk'
+    TYPE = 'type'
+    KPI_TYPE = 'kpi_type'
+    KPI_NAME_DB = 'type'
+    KPI_NAME = 'kpi_name'
+    FSOS = "FSOS"
+    ADJACENCY = "ADJACENCY"
+
+    #  KPI CONSTANTS
+    FACINGS_IN_CELL_PER_PRODUCT = "FACINGS_IN_CELL_PER_PRODUCT"
+    PRODUCT_GROUP_ADJACENCY_IN_WHOLE_STORE = "PRODUCT_GROUP_ADJACENCY_IN_WHOLE_STORE"
+
+    def __init__(self):
+        pass
--- Projects/LIONJP_SAND/Utils/KPIToolBox.py
+++ Projects/LIONJP_SAND/Utils/KPIToolBox.py
@@ -1,22 +1,22 @@
 import os
 import pandas as pd
 
+from KPIUtils_v2.Calculations.SequenceCalculationsV2 import Sequence
+from KPIUtils_v2.Calculations.CalculationsUtils.Constants import AdditionalAttr
+from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
 from KPIUtils_v2.DB.CommonV2 import Common
+
 from Trax.Cloud.Services.Connector.Keys import DbUsers
 from Trax.Algo.Calculations.Core.DataProvider import Data
-from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
+from Trax.Utils.Logging.Logger import Log
+
+from Projects.LIONJP_SAND.Data.Consts import Consts
+
 
 __author__ = 'nidhin'
 
-KPI_SHEET_NAME = 'kpi_list'
-PS_KPI_FAMILY = 19
-KPI_FAMILY = 'kpi_family_fk'
-TYPE = 'type'
-KPI_TYPE = 'kpi_type'
-KPI_NAME = 'kpi_name'
 
-
-class LIONJPToolBox:
+class LIONJP_SANDToolBox:
     LEVEL1 = 1
     LEVEL2 = 2
     LEVEL3 = 3
@@ -38,49 +38,134 @@ class LIONJPToolBox:
         self.rds_conn = PSProjectConnector(self.project_name, DbUsers.CalculationEng)
         self.kpi_static_data = self.common.get_kpi_static_data()
         self.kpi_results_queries = []
-        self.templates_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'Data')
-        self.excel_file_path = os.path.join(self.templates_path, 'Template.xlsx')
+        self.setup_file = "setup.xlsx"
+        self.kpi_sheet = self.get_setup(Consts.KPI_SHEET_NAME)
+
+    def get_setup(self, sheet_name):
+        setup_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'Data')
+        setup_path = os.path.join(setup_path, self.setup_file)
+        setup = pd.read_excel(setup_path, sheet_name=sheet_name)
+        return setup
 
     def main_calculation(self, *args, **kwargs):
         """
         This function calculates the KPI results.
         """
-        self.calculate_facings_in_cell_per_product()
-        self.common.commit_results_data()
-        return
+        try:
+            if self.kpi_sheet.empty:
+                Log.error("'kpi_list' sheet in setup file is empty.")
+                return
+            kpi_types = [x.strip() for x in self.kpi_sheet[Consts.KPI_TYPE].unique()]
+
+            for kpi_type in kpi_types:
+                kpis = self.kpi_sheet[self.kpi_sheet[Consts.KPI_TYPE] == kpi_type]
+                if kpi_type == Consts.FSOS:
+                    self.main_sos_calculations(kpis)
+                elif kpi_type == Consts.ADJACENCY:
+                    self.main_adjacency_calculations(kpis)
+                else:
+                    Log.warning("KPI_TYPE:{kt} not found in setup=>kpi_list sheet.".format(kt=kpi_type))
+                    continue
+
+                self.common.commit_results_data()
+            return
+        except Exception as err:
+            Log.error("LionJP KPI calculation failed due to the following error: {}".format(err))
+
+    def main_sos_calculations(self, kpis):
+        for row_number, row_data in kpis.iterrows():
+            if row_data[Consts.KPI_NAME] == Consts.FACINGS_IN_CELL_PER_PRODUCT:
+                self.calculate_facings_in_cell_per_product()
 
     def calculate_facings_in_cell_per_product(self):
-        kpi_sheet = self.get_template_details(KPI_SHEET_NAME)
-        for index, kpi_sheet_row in kpi_sheet.iterrows():
-            kpi = self.kpi_static_data[(self.kpi_static_data[KPI_FAMILY] == PS_KPI_FAMILY)
-                                       & (self.kpi_static_data[TYPE] == kpi_sheet_row[KPI_TYPE])
-                                       & (self.kpi_static_data['delete_time'].isnull())]
-            if kpi.empty:
-                print("KPI Name:{} not found in DB".format(kpi_sheet_row[KPI_NAME]))
+        kpi_db = self.kpi_static_data[
+              (self.kpi_static_data[Consts.KPI_FAMILY] == Consts.PS_KPI_FAMILY)
+              & (self.kpi_static_data[Consts.KPI_NAME_DB] == Consts.FACINGS_IN_CELL_PER_PRODUCT)
+              & (self.kpi_static_data['delete_time'].isnull())]
+
+        if kpi_db.empty:
+            print("KPI Name:{} not found in DB".format(Consts.FACINGS_IN_CELL_PER_PRODUCT))
+        else:
+            print("KPI Name:{} found in DB".format(Consts.FACINGS_IN_CELL_PER_PRODUCT))
+            kpi_fk = kpi_db.pk.values[0]
+            match_prod_scene_data = self.match_product_in_scene.merge(
+                self.products, how='left', on='product_fk', suffixes=('', '_prod'))
+            grouped_data = match_prod_scene_data.query(
+                '(stacking_layer==1) or (product_type=="POS")'
+            ).groupby(
+                ['scene_fk', 'bay_number', 'shelf_number', 'product_fk']
+            )
+            for data_tup, scene_data_df in grouped_data:
+                scene_fk, bay_number, shelf_number, product_fk = data_tup
+                facings_count_in_cell = len(scene_data_df)
+                cur_template_fk = int(self.scene_info[self.scene_info['scene_fk'] == scene_fk].get('template_fk'))
+                self.common.write_to_db_result(fk=kpi_fk,
+                                               numerator_id=product_fk,
+                                               denominator_id=self.store_id,
+                                               context_id=cur_template_fk,
+                                               numerator_result=bay_number,
+                                               denominator_result=shelf_number,
+                                               result=facings_count_in_cell,
+                                               score=scene_fk)
+
+    @staticmethod
+    def get_entity_query():
+        query = """select 
+                    pk product_fk,
+                    "entity_1" entity,
+                    ean_code
+                    from 
+                    static_new.product p 
+                    where 1=1 
+                    and ean_code in ("4902430916028")
+                    union 
+                    select
+                    pk product_fk,
+                    "entity_2" entity,
+                    ean_code
+                    from 
+                    static_new.product p 
+                    where 1=1 
+                    and ean_code in ("4903301282945")
+                    union 
+                    select
+                    pk product_fk,
+                    "entity_3" entity,
+                    ean_code
+                    from 
+                    static_new.product p 
+                    where 1=1 
+                    and ean_code in ("4582469502593")
+                    """
+        return query
+
+    def main_adjacency_calculations(self, kpis):
+        for row_number, row_data in kpis.iterrows():
+            if row_data[Consts.KPI_NAME] == Consts.PRODUCT_GROUP_ADJACENCY_IN_WHOLE_STORE:
+                self.calculate_sequence_per_scene()
             else:
-                print("KPI Name:{} found in DB".format(kpi_sheet_row[KPI_NAME]))
-                kpi_fk = kpi.pk.values[0]
-                match_prod_scene_data = self.match_product_in_scene.merge(
-                    self.products, how='left', on='product_fk', suffixes=('', '_prod'))
-                grouped_data = match_prod_scene_data.query(
-                    '(stacking_layer==1) or (product_type=="POS")'
-                ).groupby(
-                    ['scene_fk', 'bay_number', 'shelf_number', 'product_fk']
-                )
-                for data_tup, scene_data_df in grouped_data:
-                    scene_fk, bay_number, shelf_number, product_fk = data_tup
-                    facings_count_in_cell = len(scene_data_df)
-                    cur_template_fk = int(self.scene_info[self.scene_info['scene_fk'] == scene_fk].get('template_fk'))
-                    self.common.write_to_db_result(fk=kpi_fk,
-                                                   numerator_id=product_fk,
-                                                   denominator_id=self.store_id,
-                                                   context_id=cur_template_fk,
-                                                   numerator_result=bay_number,
-                                                   denominator_result=shelf_number,
-                                                   result=facings_count_in_cell,
-                                                   score=scene_fk)
+                Log.warning("KPI_NAME:{kn} not found in setup=>kpi_list sheet.".format(kn=row_data[Consts.KPI_NAME]))
 
-    def get_template_details(self, sheet_name):
-        template = pd.read_excel(self.excel_file_path, sheetname=sheet_name)
-        return template
+    def calculate_sequence_per_scene(self):
+        scene_fks = self.scif['scene_fk'].unique()
+        for scene_fk in scene_fks:
+            if scene_fk != 964190:
+                continue
+            location = {"scene_fk": scene_fk}
+            query = self.get_entity_query()
+            df_entities = pd.read_sql_query(query, self.rds_conn.db)
+            population = {'entity': ["entity_1", "entity_3"]}
+            sequence_params = {AdditionalAttr.DIRECTION: 'DOWN',
+                               AdditionalAttr.EXCLUDE_FILTER: {"product_type": ["Empty", "Other", "Irrelevant", "POS"]},
+                               AdditionalAttr.CHECK_ALL_SEQUENCES: False,
+                               AdditionalAttr.STRICT_MODE: False,
+                               AdditionalAttr.REPEATING_OCCURRENCES: True,
+                               AdditionalAttr.INCLUDE_STACKING: False,
+                               AdditionalAttr.ALLOWED_PRODUCTS_FILTERS: {'entity': ["entity_2"]},
+                               AdditionalAttr.MIN_TAGS_OF_ENTITY: 1}
 
+            self.data_provider._set_all_products(self.data_provider.all_products.merge(df_entities, on="product_fk"))
+            sequence_res = Sequence(self.data_provider).calculate_sequence(population, location, sequence_params)
+            for row_num, row_data in sequence_res.iterrows():
+                for column, data in row_data.items():
+                    print ("{} {}".format(column, data))
