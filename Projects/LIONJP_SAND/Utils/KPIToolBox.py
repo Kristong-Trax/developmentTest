b7ad2c27538239dd248bb315d41b11a75c830937
sathiyanarayanand <sathiyanarayanan@traxretail.com>
2020-04-17 19:01:33 +0800

    PROS-14717 LionJP Adjacency KPI

--- Projects/LIONJP_SAND/Utils/KPIToolBox.py
+++ Projects/LIONJP_SAND/Utils/KPIToolBox.py
@@ -5,15 +5,14 @@ from KPIUtils_v2.Calculations.SequenceCalculationsV2 import Sequence
 from KPIUtils_v2.Calculations.CalculationsUtils.Constants import AdditionalAttr
 from KPIUtils_v2.DB.PsProjectConnector import PSProjectConnector
 from KPIUtils_v2.DB.CommonV2 import Common
+from KPIUtils_v2.GlobalDataProvider.PsDataProvider import PsDataProvider
 
 from Trax.Cloud.Services.Connector.Keys import DbUsers
 from Trax.Algo.Calculations.Core.DataProvider import Data
 from Trax.Utils.Logging.Logger import Log
-
 from Projects.LIONJP_SAND.Data.Consts import Consts
 
-
-__author__ = 'nidhin'
+__author__ = 'satya'
 
 
 class LIONJP_SANDToolBox:
@@ -38,8 +37,28 @@ class LIONJP_SANDToolBox:
         self.rds_conn = PSProjectConnector(self.project_name, DbUsers.CalculationEng)
         self.kpi_static_data = self.common.get_kpi_static_data()
         self.kpi_results_queries = []
+        self.ps_data_provider = PsDataProvider(self.data_provider, self.output)
+        self.targets = self.ps_data_provider.get_kpi_external_targets()
         self.setup_file = "setup.xlsx"
         self.kpi_sheet = self.get_setup(Consts.KPI_SHEET_NAME)
+
+    def get_kpi_level_2_fk(self, kpi_level_2_type):
+        query = \
+            """
+            SELECT pk FROM static.kpi_level_2
+            WHERE type = '{}';
+            """.format(kpi_level_2_type)
+        data = pd.read_sql_query(query, self.rds_conn.db)
+        return None if data.empty else data.values[0][0]
+
+    def get_kpi_name(self, kpi_level_2_fk):
+        query = \
+            """
+            SELECT type kpi_name FROM static.kpi_level_2
+            WHERE pk = {};
+            """.format(kpi_level_2_fk)
+        data = pd.read_sql_query(query, self.rds_conn.db)
+        return None if data.empty else data.values[0][0]
 
     def get_setup(self, sheet_name):
         setup_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'Data')
@@ -79,9 +98,9 @@ class LIONJP_SANDToolBox:
 
     def calculate_facings_in_cell_per_product(self):
         kpi_db = self.kpi_static_data[
-              (self.kpi_static_data[Consts.KPI_FAMILY] == Consts.PS_KPI_FAMILY)
-              & (self.kpi_static_data[Consts.KPI_NAME_DB] == Consts.FACINGS_IN_CELL_PER_PRODUCT)
-              & (self.kpi_static_data['delete_time'].isnull())]
+            (self.kpi_static_data[Consts.KPI_FAMILY] == Consts.PS_KPI_FAMILY)
+            & (self.kpi_static_data[Consts.KPI_NAME_DB] == Consts.FACINGS_IN_CELL_PER_PRODUCT)
+            & (self.kpi_static_data['delete_time'].isnull())]
 
         if kpi_db.empty:
             print("KPI Name:{} not found in DB".format(Consts.FACINGS_IN_CELL_PER_PRODUCT))
@@ -108,64 +127,146 @@ class LIONJP_SANDToolBox:
                                                result=facings_count_in_cell,
                                                score=scene_fk)
 
-    @staticmethod
-    def get_entity_query():
-        query = """select 
-                    pk product_fk,
-                    "entity_1" entity,
-                    ean_code
-                    from 
-                    static_new.product p 
-                    where 1=1 
-                    and ean_code in ("4902430916028")
-                    union 
-                    select
-                    pk product_fk,
-                    "entity_2" entity,
-                    ean_code
-                    from 
-                    static_new.product p 
-                    where 1=1 
-                    and ean_code in ("4903301282945")
-                    union 
-                    select
-                    pk product_fk,
-                    "entity_3" entity,
-                    ean_code
-                    from 
-                    static_new.product p 
-                    where 1=1 
-                    and ean_code in ("4582469502593")
-                    """
-        return query
-
     def main_adjacency_calculations(self, kpis):
         for row_number, row_data in kpis.iterrows():
-            if row_data[Consts.KPI_NAME] == Consts.PRODUCT_GROUP_ADJACENCY_IN_WHOLE_STORE:
-                self.calculate_sequence_per_scene()
+            if row_data[Consts.KPI_NAME] == Consts.ADJACENCY_PRODUCT_GROUP_IN_SCENE_TYPE:
+                kpi_level_2_fk = self.get_kpi_level_2_fk(Consts.ADJACENCY_PRODUCT_GROUP_IN_SCENE_TYPE)
+                self.calculate_adjacency_per_scene(kpi_level_2_fk)
             else:
                 Log.warning("KPI_NAME:{kn} not found in setup=>kpi_list sheet.".format(kn=row_data[Consts.KPI_NAME]))
 
-    def calculate_sequence_per_scene(self):
-        scene_fks = self.scif['scene_fk'].unique()
-        for scene_fk in scene_fks:
-            if scene_fk != 964190:
-                continue
-            location = {"scene_fk": scene_fk}
-            query = self.get_entity_query()
-            df_entities = pd.read_sql_query(query, self.rds_conn.db)
-            population = {'entity': ["entity_1", "entity_3"]}
-            sequence_params = {AdditionalAttr.DIRECTION: 'DOWN',
-                               AdditionalAttr.EXCLUDE_FILTER: {"product_type": ["Empty", "Other", "Irrelevant", "POS"]},
-                               AdditionalAttr.CHECK_ALL_SEQUENCES: False,
-                               AdditionalAttr.STRICT_MODE: False,
-                               AdditionalAttr.REPEATING_OCCURRENCES: True,
-                               AdditionalAttr.INCLUDE_STACKING: False,
-                               AdditionalAttr.ALLOWED_PRODUCTS_FILTERS: {'entity': ["entity_2"]},
-                               AdditionalAttr.MIN_TAGS_OF_ENTITY: 1}
+    @staticmethod
+    def prepare_data_adjacency(kpi_config):
+        adj_config_list = []
+        for row_number, row_data in kpi_config.iterrows():
+            adj_config = {"template_fks": row_data['template_fks'],
+                          "group_entity": row_data['group_entity'],
+                          "orientation": row_data['orientation'],
+                          "minimum_tagging": row_data['minimum_tagging'],
+                          "sequence_mode": row_data['sequence_mode'],
+                          "include_stacking": row_data['include_stacking'],
+                          "include_empty": row_data['include_empty'],
+                          "include_others": row_data['include_others'],
+                          "include_irrelevant": row_data['include_irrelevant'],
+                          "include_pos": row_data['include_pos'],
+                          "entity": row_data['entity'],
+                          "entity_values": row_data['entity_values']}
+            adj_config_list.append(adj_config)
+        return adj_config_list
 
-            self.data_provider._set_all_products(self.data_provider.all_products.merge(df_entities, on="product_fk"))
-            sequence_res = Sequence(self.data_provider).calculate_sequence(population, location, sequence_params)
-            for row_num, row_data in sequence_res.iterrows():
-                for column, data in row_data.items():
-                    print ("{} {}".format(column, data))
+    @staticmethod
+    def exclude_and_include_filter(adj_config):
+        allowed_filters = []
+        exclude_filters = []
+
+        include_empty = adj_config['include_empty']
+        include_others = adj_config['include_others']
+        include_irrelevant = adj_config['include_irrelevant']
+        include_pos = adj_config['include_pos']
+
+        if include_empty == 0:
+            allowed_filters.append("Empty")
+        else:
+            exclude_filters.append("Empty")
+
+        if include_others == 0:
+            allowed_filters.append("Other")
+        else:
+            exclude_filters.append("Other")
+
+        if include_irrelevant == 0:
+            allowed_filters.append("Irrelevant")
+        else:
+            exclude_filters.append("Irrelevant")
+
+        if include_pos == 0:
+            allowed_filters.append("POS")
+        else:
+            exclude_filters.append("POS")
+
+        if len(allowed_filters) == 0:
+            allowed_products_filters = None
+        else:
+            allowed_products_filters = {"product_type": allowed_filters}
+
+        if len(exclude_filters) == 0:
+            exclude_products_filters = None
+        else:
+            exclude_products_filters = {"product_type": exclude_filters}
+
+        return exclude_products_filters, allowed_products_filters
+
+    def build_ean_groups(self, adj_config):
+        group_entities = adj_config['group_entity']
+        entity_values = adj_config['entity_values']
+        entities = {}
+        df_entities = pd.DataFrame()
+        if group_entities.upper() == "Y":
+            for entity_key, entity_value in enumerate(entity_values):
+                if isinstance(entity_value, list):
+                    entities["entity_{}".format(entity_key)] = entity_value
+                else:
+                    Log.warning("ean_codes are not in list of lists format")
+                    return
+        else:
+            for entity_key, entity_value in enumerate(entity_values):
+                entities["entity_{}".format(entity_key)] = entity_value
+
+        for entity_key, entity_value in entities.items():
+            df_entity = self.data_provider.all_products[['product_fk', 'product_ean_code']].copy()
+
+            df_entity = df_entity[['product_fk']][df_entity['product_ean_code'].isin(entity_value)].copy()
+            df_entity['entity'] = entity_key
+            df_entities = df_entities.append(df_entity)
+
+        df_entities = df_entities.reset_index(drop=True)
+        df_custom_matches = self.data_provider.matches.copy()
+        df_custom_matches = df_custom_matches.merge(df_entities, on="product_fk")
+
+        population = {'entity': entities.keys()}
+        exclude_filter, allowed_products_filter = self.exclude_and_include_filter(adj_config)
+
+        sequence_params = {AdditionalAttr.DIRECTION: adj_config['orientation'].upper(),
+                           AdditionalAttr.EXCLUDE_FILTER: exclude_filter,
+                           AdditionalAttr.CHECK_ALL_SEQUENCES: False,
+                           AdditionalAttr.STRICT_MODE: True if adj_config['sequence_mode'] == 1 else False,
+                           AdditionalAttr.REPEATING_OCCURRENCES: True,
+                           AdditionalAttr.INCLUDE_STACKING: True if adj_config['include_stacking'] == 1 else False,
+                           AdditionalAttr.ALLOWED_PRODUCTS_FILTERS: allowed_products_filter,
+                           AdditionalAttr.MIN_TAGS_OF_ENTITY: adj_config['minimum_tagging']}
+        return population, sequence_params, df_custom_matches
+
+    def calculate_adjacency_per_scene(self, kpi_level_2_fk):
+        kpi_config = self.targets[self.targets["kpi_fk"] == kpi_level_2_fk]
+
+        if kpi_config.empty:
+            kpi_name = self.get_kpi_name(kpi_level_2_fk)
+            message = "kpi_level_2_fk:{}, kpi_name:{} ".format(kpi_level_2_fk, kpi_name)
+            message += " not found in static.kpi_external_targets table"
+            Log.warning(message)
+            return
+
+        adj_configs = self.prepare_data_adjacency(kpi_config)
+
+        for adj_config in adj_configs:
+            scene_fks = self.scif['scene_fk'][self.scif['template_fk'].isin(adj_config['template_fks'])].unique()
+            for scene_fk in scene_fks:
+                if adj_config['entity'] == "ean_code":
+                    location = {"scene_fk": scene_fk}
+                    population, sequence_params, custom_matches = self.build_ean_groups(adj_config)
+
+                    if custom_matches.empty:
+                        print("scene_fk:{}, Custom Entities are empty. Please check the ean_codes".format(scene_fk))
+                        continue
+
+                    sequence_res = Sequence(self.data_provider, custom_matches).calculate_sequence(population, location,
+                                                                                                   sequence_params)
+                    if sequence_res.empty:
+                        Log.warning("scene_fk:{}, No matches available for the given entities".format(scene_fk))
+                        continue
+
+                    for row_num, row_data in sequence_res.iterrows():
+                        for column, data in row_data.items():
+                            print ("{} {}".format(column, data))
+                else:
+                    pass
