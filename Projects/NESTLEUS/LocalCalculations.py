283104e4695094fe5a3ebe2f10d2590b9adcb15f
Trevaris Small <trevaris.small@traxretail.com>
2020-04-20 10:59:07 -0400

    Change erroneus `shelf_number` reference to `shelf_number_from_bottom`

--- Projects/NESTLEUS/LocalCalculations.py
+++ Projects/NESTLEUS/LocalCalculations.py
@@ -19,16 +19,16 @@ if __name__ == '__main__':
     project_name = 'nestleus'
     data_provider = KEngineDataProvider(project_name)
 
-    #checkout('0d71ea3c-6430-488e-88e1-dd83ef64d2c9')
+    # checkout('03c096f9-21a1-4468-b6a5-49421a576c92')
 
     test_sessions = pd.read_excel(Const.TEST_SESSIONS_PATH)
 
-    i = 0
+    i = 0.0
     for session in test_sessions.itertuples():
         data_provider.load_session_data(session.Session_uid)
         output = Output()
         Calculations(data_provider, output).run_project_calculations()
         i += 1
-        print("Completed {}% of sessions".format(1/len(test_sessions)*100))
+        print("Completed {}% of local test sessions".format(round(i/len(test_sessions)*100, 2)))
 
     print("Done")
