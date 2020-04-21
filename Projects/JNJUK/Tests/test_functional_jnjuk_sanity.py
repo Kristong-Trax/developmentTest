c84f22d61fb489b0144a1213a32ca9c980678c5c
ilay_s <ilays@traxretail.com>
2020-04-21 12:26:37 +0300

    change straussil to save only 2 figures after decimak point

--- Projects/JNJUK/Tests/test_functional_jnjuk_sanity.py
+++ Projects/JNJUK/Tests/test_functional_jnjuk_sanity.py
@@ -14,12 +14,13 @@ __author__ = 'ilays'
 class TestKEnginePsCode(PsSanityTestsFuncs):
 
     def add_mocks(self):
-        # with open(os.path.join('Data', 'Relative Position'), 'rb') as f:
+        # with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Data', 'Relative Position.txt'),
+        #           'rb') as f:
         #     relative_position_template = json.load(f)
         # self.mock_object('save_latest_templates',
         #                  path='KPIUtils.GlobalProjects.DIAGEO.Utils.TemplatesUtil.TemplateHandler')
         # self.mock_object('download_template',
-        #                  path='KPIUtils.GlobalProjects.DIAGEO.Utils.TemplatesUtil.TemplateHandler').return_value = \
+        #                  path='KPIUtils.GlobalProjects.DIAGEO.Utils.TemplatesUtil.TemplateHandler').return_value = \In
         #     relative_position_template
         return
 
@@ -37,7 +38,7 @@ class TestKEnginePsCode(PsSanityTestsFuncs):
             # for scene in sessions[session]:
             # data_provider.load_scene_data(str(session), scene_id=scene)
             # SceneCalculations(data_provider).calculate_kpis()
-        # self._assert_test_results_matches_reality(kpi_results)
+        self._assert_test_results_matches_reality(kpi_results)
         # self._assert_old_tables_kpi_results_filled()
         # self._assert_new_tables_kpi_results_filled(distinct_kpis_num=None, list_of_kpi_names=None)
         # self._assert_scene_tables_kpi_results_filled(distinct_kpis_num=None)
