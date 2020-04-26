28443fecd3bfa6665ba037eba812617642b61f53
natalya_k <natalyak@traxretail.com>
2020-04-26 14:40:15 +0300

    m

--- Projects/PEPSICOUK/KPIs/Session/Secondary_Location/SecondaryHeroAvailabilitySKU.py
+++ Projects/PEPSICOUK/KPIs/Session/Secondary_Location/SecondaryHeroAvailabilitySKU.py
@@ -11,28 +11,29 @@ class HeroAvailabilitySkuKpi(UnifiedCalculationsScript):
     def __init__(self, data_provider, config_params=None, **kwargs):
         super(HeroAvailabilitySkuKpi, self).__init__(data_provider, config_params=config_params, **kwargs)
         self.util = PepsicoUtil(None, data_provider)
-        self.kpi_name = self.kpi_type()
+        self.kpi_name = self._config_params['kpi_type']
         self.location = self._config_params['location']
 
     def calculate(self):
-        kpi_type = self._config_params['kpi_type']
         self.util.filtered_scif, self.util.filtered_matches = \
             self.util.commontools.set_filtered_scif_and_matches_for_specific_kpi(self.util.filtered_scif,
                                                                                  self.util.filtered_matches,
-                                                                                 kpi_type)
+                                                                                 self.kpi_name)
         self.calculate_kpi_for_secondary_shelf()
         self.util.reset_filtered_scif_and_matches_to_exclusion_all_state()
 
     def kpi_type(self):
-        return self._config_params['kpi_type']
+        pass
 
     def calculate_kpi_for_secondary_shelf(self):
+        # change to the appropriate template attribute
         location_type_fk = self.util.all_templates[self.util.all_templates[ScifConsts.LOCATION_TYPE] == self.location] \
             [ScifConsts.LOCATION_TYPE_FK].values[0]
+
         for i, result in self.util.lvl3_ass_result.iterrows():
             score = result.in_store * 100
             custom_res = self.util.commontools.get_yes_no_result(score)
-            self.write_to_db_result(fk=result.kpi_fk_lvl3, numerator_id=result.product_fk,
+            self.write_to_db_result(fk=self.kpi_name, numerator_id=result.product_fk,
                                     numerator_result=result.in_store, result=custom_res,
                                     denominator_id=self.util.own_manuf_fk, denominator_result=1, score=score,
                                     context_id=location_type_fk)
