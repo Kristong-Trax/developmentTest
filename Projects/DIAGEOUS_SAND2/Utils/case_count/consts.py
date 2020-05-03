9b7b2bbeeada79447ba215846b1029b47859b239
idanre <idanr@traxretail.com>
2020-05-03 18:38:56 +0300

    first take - case count v2

---
+++ Projects/DIAGEOUS_SAND2/Utils/case_count/consts.py
@@ -0,0 +1,19 @@
+
+
+class Consts(object):
+
+    # KPI Names
+    TOTAL_CASES_KPI = 'Display Size Total Cases'
+    FACINGS_KPI = 'Display Size Facings'
+    SHOPPABLE_CASES_KPI = 'Display Size Shoppable Cases'
+    IMPLIED_SHOPPABLE_CASES_KPI = 'Display Size Implied Shoppable Cases'
+    NON_SHOPPABLE_CASES_KPI = 'Display Size Non-Shoppable Cases'
+
+    DISPLAY_SKU = 'closest_sku_to_the_display'
+    RELEVANT_DISPLAYS_SUFFIX = "Open|Close|open|close"
+    DISPLAY_IN_SCENE_FK = 'display_in_scene_fk'
+    FACINGS_SKU_TYPES = ['Bottle', 'Carton', 'Pouch', 'VAP']
+
+
+
+
