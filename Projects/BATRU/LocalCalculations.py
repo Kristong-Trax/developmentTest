fe59bf36fa3016d091fb5d196a04c802e4d4d0e9
sergeypol <sergey@traxretail.com>
2020-05-10 15:42:20 +0300

    Menu prices KPI - Sanity test is removed for further fixes

--- Projects/BATRU/LocalCalculations.py
+++ Projects/BATRU/LocalCalculations.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     sessions = [
         # 'FFF7C170-12E7-4EC6-B292-D8BF1870741C',
         # 'c312125d-6814-49e5-af73-8e894e0e557d',
-        'fffc4533-2b49-472b-b25c-30af62123bdd',
+        # 'fffc4533-2b49-472b-b25c-30af62123bdd',
         'AF410EF7-90D8-431A-AF12-60DE6ED7438B'
     ]
     for session in sessions:
