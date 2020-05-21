86a9e929bee600b28c8be309ac4dad35f55d55be
ilay_s <ilays@traxretail.com>
2020-05-21 14:22:13 +0300

    Add straussfritolayil all KPIs classes

---
+++ Projects/STRAUSSFRITOLAYIL/KPIs/Session/OOSNpaSKU.py
@@ -0,0 +1,24 @@
+from Projects.STRAUSSFRITOLAYIL.KPIs.Util import StraussfritolayilUtil
+from Trax.Algo.Calculations.Core.KPI.UnifiedKPICalculation import UnifiedCalculationsScript
+from Projects.STRAUSSFRITOLAYIL.Data.LocalConsts import Consts
+
+
+class OOSNpaSKUKpi(UnifiedCalculationsScript):
+
+    def __init__(self, data_provider, config_params=None, **kwargs):
+        super(OOSNpaSKUKpi, self).__init__(data_provider, config_params=config_params, **kwargs)
+        self.utils = StraussfritolayilUtil(None, data_provider)
+
+    def calculate(self):
+        sku_kpi_fk = self.utils.common.get_kpi_fk_by_kpi_type(Consts.NUMBER_OF_UNQIUE_SKUS_KPI)
+        own_manufacturer_skus = self.utils.scif[self.utils.scif['manufacturer_fk'] == self.utils.own_manuf_fk]
+        own_manufacturer_skus = own_manufacturer_skus[~own_manufacturer_skus['product_type'].isin(['Other', 'Empty'])]
+        for i, sku_row in own_manufacturer_skus.iterrows():
+            product_fk = sku_row['product_fk']
+            result = sku_row['facings']
+            score = 1 if result > 0 else 0
+            self.write_to_db_result(fk=sku_kpi_fk, numerator_id=product_fk, result=result,
+                                    denominator_id=self.utils.own_manuf_fk, score=score)
+
+    def kpi_type(self):
+        pass
---
+++ Projects/STRAUSSFRITOLAYIL/KPIs/Session/SOSBrandOutOfManufacturerInCategory.py
@@ -0,0 +1,22 @@
+from Projects.STRAUSSFRITOLAYIL.KPIs.Util import StraussfritolayilUtil
+from Trax.Algo.Calculations.Core.KPI.UnifiedKPICalculation import UnifiedCalculationsScript
+from Projects.STRAUSSFRITOLAYIL.Data.LocalConsts import Consts
+
+
+class SOSBrandOutOfManufacturerInCategoryKpi(UnifiedCalculationsScript):
+
+    def __init__(self, data_provider, config_params=None, **kwargs):
+        super(SOSBrandOutOfManufacturerInCategoryKpi, self).__init__(data_provider, config_params=config_params, **kwargs)
+        self.utils = StraussfritolayilUtil(None, data_provider)
+
+    def calculate(self):
+        sku_results = self.dependencies_data
+        distribution_kpi_fk = self.utils.common.get_kpi_fk_by_kpi_type(Consts.NUMBER_OF_UNQIUE_SKUS_KPI)
+        result = len(sku_results)
+        self.write_to_db_result(fk=distribution_kpi_fk, numerator_id=self.utils.own_manuf_fk,
+                                result=result, denominator_id=self.utils.store_id)
+        # self.util.add_kpi_result_to_kpi_results_df(
+        #     [distribution_kpi_fk, self.util.own_manuf_fk, self.util.store_id, res, score])
+
+    def kpi_type(self):
+        pass
