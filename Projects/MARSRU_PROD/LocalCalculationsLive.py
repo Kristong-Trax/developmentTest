319418daae5e0efe8a66d2c6b3ee4aca2811761a
sergeypol <sergey@traxretail.com>
2020-05-26 15:45:15 +0300

    Live KPIs changes

--- Projects/MARSRU_PROD/LocalCalculationsLive.py
+++ Projects/MARSRU_PROD/LocalCalculationsLive.py
@@ -9,10 +9,7 @@ if __name__ == '__main__':
     project_name = 'marsru-prod'
     session_uids = [
 
-        '50a37969-5b4d-4787-8cd3-4125e78e9038',
-        '559b9f05-7d28-4aa0-8ca5-4a65a1833ef7',
-        'df19c937-8216-435c-a5a9-563b40fa5c0f',
-        'e9eef7bc-582a-49db-a5eb-51a0ae30c4fb'
+        'e0dfe73b-e33b-4b4c-99d6-0e43c85d18c8'
     ]
     data_provider = KEngineSessionDataProviderLive(project_name, None, None)
     output = None
