9f758f748e1c019102a35b8352cf29f1f4220314
ilay_s <ilays@traxretail.com>
2020-05-26 18:31:11 +0300

    creating linear sku and facings sku KPIs

--- Projects/STRAUSSFRITOLAYIL/KPIs/Session/SKULinear.py
+++ Projects/STRAUSSFRITOLAYIL/KPIs/Session/SKULinear.py
@@ -10,16 +10,18 @@ class SKULinearKpi(UnifiedCalculationsScript):
         self.utils = StraussfritolayilUtil(None, data_provider)
 
     def calculate(self):
-        return
         kpi_fk = self.utils.common.get_kpi_fk_by_kpi_type(Consts.SKU_LINEAR_KPI)
-        own_manufacturer_skus = self.utils.scif[self.utils.scif['manufacturer_fk'] == self.utils.own_manuf_fk]
-        own_manufacturer_skus = own_manufacturer_skus[~own_manufacturer_skus['product_type'].isin(['Other', 'Empty'])]
-        for i, sku_row in own_manufacturer_skus.iterrows():
+        custom_scif = self.utils.scif.copy()
+        custom_scif = custom_scif[custom_scif['template_name'] == 'Primary Shelf']
+        custom_scif = custom_scif[~custom_scif['product_type'].isin(['Irrelevant'])]
+        custom_scif = custom_scif[['product_fk', 'manufacturer_fk', 'gross_len_ign_stack']]
+        combined_scenes_scif = custom_scif.groupby(['product_fk', 'manufacturer_fk']).sum()
+        for i, sku_row in combined_scenes_scif.iterrows():
             product_fk = sku_row['product_fk']
-            result = sku_row['facings']
-            score = 1 if result > 0 else 0
+            result = sku_row['gross_len_ign_stack']
+            manufacturer_fk = sku_row['manufacturer_fk']
             self.write_to_db_result(fk=kpi_fk, numerator_id=product_fk, result=result,
-                                    denominator_id=self.utils.own_manuf_fk, score=score)
+                                    denominator_id=manufacturer_fk, score=result)
 
     def kpi_type(self):
         pass
