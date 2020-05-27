7e52cdde44417926851a58bc9c51fb3664ac1bed
sathiyanarayanand <sathiyanarayanan@traxretail.com>
2020-05-27 17:22:08 +0800

    PROS-14717 LionJP Template validation

--- Projects/LIONJP_SAND/Utils/KPITemplateValidater.py
+++ Projects/LIONJP_SAND/Utils/KPITemplateValidater.py
@@ -5,6 +5,7 @@ from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 from Trax.Utils.Conf.Configuration import Config
 from Trax.Data.Projects.Connector import ProjectConnector
 from Trax.Cloud.Services.Connector.Keys import DbUsers
+from collections import Counter
 
 
 class KPITemplateValidater:
@@ -119,7 +120,7 @@ class KPITemplateValidater:
             if not check.empty:
                 Log.error("Invalid input, allowed_values: {}".format(value))
                 for idx, row_data in check.iterrows():
-                    Log.error("row_number: {}, column_name: {}, value: {}".format(idx+2, name, row_data[name]))
+                    Log.error("row_number: {}, column_name: {}, value: {}".format(idx+3, name, row_data[name]))
                 result = False
 
         if result:
@@ -163,12 +164,82 @@ class KPITemplateValidater:
                                 if len(df_result) != len(values):
                                     message = "{xl_column_name}={values}".format(xl_column_name=xl_column_name,
                                                                                  values=values)
-                                    message += "one or more values not found in table {table_name}".format(table_name=table_name)
+                                    message += " one or more values not found in table {table_name}".format(table_name=table_name)
                                     Log.error(message)
                                     result = False
                     except Exception as ex:
                         result = False
                         Log.error("{} {}".format(ex.message, ex.args))
+        return result
+
+    def check_entity_values(self, entity_name_key, entity_value_key):
+        result = True
+        dbcon = ProjectConnector(self.project_name, DbUsers.CalculationEng)
+
+        if entity_name_key == "others":
+            entity = {
+                'others': ('static_new.product', 'ean_code')
+            }
+        else:
+            entity = {
+                      'product': ('static_new.product', 'ean_code'),
+                      'brand': ('static_new.brand', 'name'),
+                      'category': ('static_new.category', 'name'),
+                      'sub_category': ('static_new.sub_category', 'name')
+                      }
+
+        for row_num, row_data in self.template.iterrows():
+            try:
+                if entity_name_key != "others":
+                    xl_entity_type = row_data[entity_name_key]
+                else:
+                    xl_entity_type = "others"
+
+                if pd.isnull(row_data[entity_value_key]):
+                    continue
+
+                xl_entity_values = row_data[entity_value_key].strip()
+
+                if xl_entity_type not in entity.keys():
+                    Log.error("{} not an valid entity, allowed entities={}".format(xl_entity_type, entity.keys()))
+                    result = False
+                    continue
+
+                entity_table_name, entity_column_name = entity[xl_entity_type]
+                values = tuple(["{}".format(v.strip()) for v in xl_entity_values.split(",")])
+                values_count = Counter(values)
+                for value in values_count.items():
+                    if value[1] > 1:
+                        Log.error("row_number:{} Duplicate(s) values={} found in {}".format(row_num+2,
+                                                                                            entity_name_key,
+                                                                                            value))
+                        result = False
+
+                query = "SELECT {db_column_name} FROM {table_name} WHERE {db_column_name} IN {values}"
+                query = query.format(db_column_name=entity_column_name, table_name=entity_table_name, values=values)
+                query = query.strip().replace(",)", ")")
+                df_result = pd.read_sql(query, dbcon.db)
+                if df_result.empty:
+                    message = "row_num:{row_num} {xl_column_name}={values} ".format(row_num=row_num+3,
+                                                                                    xl_column_name=entity_column_name,
+                                                                                    values=values)
+                    message += " not in table {table_name}".format(table_name=entity_table_name)
+                    Log.error(message)
+                    result = False
+                else:
+                    entity_values_rt = set(df_result[entity_column_name].unique())
+                    entity_values_ck = set(values)
+                    missing_values = list(entity_values_ck - entity_values_rt)
+                    if len(missing_values) != 0:
+                        message = "row_number:{} {} =>{} missing values={}:".format(row_num+3,
+                                                                                    entity_name_key,
+                                                                                    entity_column_name,
+                                                                                    missing_values)
+                        Log.error(message)
+                        result = False
+            except Exception as ex:
+                result = False
+                Log.error("{} {}".format(ex.message, ex.args))
         return result
 
     def validate(self):
@@ -179,6 +250,13 @@ class KPITemplateValidater:
             result = False
         if not self.check_db_values():
             result = False
+        if not self.check_entity_values("entity_1_type", 'entity_1_values'):
+            result = False
+        if not self.check_entity_values("entity_2_type", 'entity_2_values'):
+            result = False
+        if not self.check_entity_values("others", 'include_other_ean_codes'):
+            result = False
+
 
         return result
 
