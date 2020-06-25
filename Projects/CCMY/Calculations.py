e1ed177f28722e09c10a370de6066550ca4d25a8
sathiyanarayanand <sathiyanarayanan@traxretail.com>
2020-06-25 19:15:56 +0800

    PROS-15569 CCMY exclude POS from the shelf purity KPI

--- Projects/CCMY/Calculations.py
+++ Projects/CCMY/Calculations.py
@@ -1,29 +1,29 @@
-
-from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
-from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
-from Trax.Utils.Conf.Configuration import Config
-from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-
-from Projects.CCMY.KPIGenerator import CCMYGenerator
-
-__author__ = 'Nimrod'
-
-
-class CCMYCalculations(BaseCalculationsScript):
-    def run_project_calculations(self):
-        self.timer.start()
-        CCMYGenerator(self.data_provider, self.output).main_function()
-        self.timer.stop('KPIGenerator.run_project_calculations')
-
-
-if __name__ == '__main__':
-    LoggerInitializer.init('ccmy calculations')
-    Config.init()
-    project_name = 'ccmy'
-    data_provider = KEngineDataProvider(project_name)
-    sessions = ['afa3b599-8228-4ebf-9409-14d2269a01f8']
-
-    for session in sessions:
-        data_provider.load_session_data(session)
-        output = Output()
-        CCMYCalculations(data_provider, output).run_project_calculations()
+#
+# from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
+# from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
+# from Trax.Utils.Conf.Configuration import Config
+# from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
+#
+# from Projects.CCMY.KPIGenerator import CCMYGenerator
+#
+# __author__ = 'Nimrod'
+#
+#
+# class CCMYCalculations(BaseCalculationsScript):
+#     def run_project_calculations(self):
+#         self.timer.start()
+#         CCMYGenerator(self.data_provider, self.output).main_function()
+#         self.timer.stop('KPIGenerator.run_project_calculations')
+#
+#
+# if __name__ == '__main__':
+#     LoggerInitializer.init('ccmy calculations')
+#     Config.init()
+#     project_name = 'ccmy'
+#     data_provider = KEngineDataProvider(project_name)
+#     sessions = ['6daeedb4-eae5-4349-bafd-648d08d3e7d3']
+#
+#     for session in sessions:
+#         data_provider.load_session_data(session)
+#         output = Output()
+#         CCMYCalculations(data_provider, output).run_project_calculations()
