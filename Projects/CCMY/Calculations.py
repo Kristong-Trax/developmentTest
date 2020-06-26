86e8d34db83dfc906e8bad2d8556c158ab33131a
sathiyanarayanand <sathiyanarayanan@traxretail.com>
2020-06-26 10:32:45 +0800

    PROS-15569 CCMY Shelf Purity POS exclusion

--- Projects/CCMY/Calculations.py
+++ Projects/CCMY/Calculations.py
@@ -1,21 +1,19 @@
-#
-# from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
+from Trax.Algo.Calculations.Core.CalculationsScript import BaseCalculationsScript
+from Projects.CCMY.KPIGenerator import CCMYGenerator
 # from Trax.Algo.Calculations.Core.DataProvider import KEngineDataProvider, Output
 # from Trax.Utils.Conf.Configuration import Config
 # from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
-#
-# from Projects.CCMY.KPIGenerator import CCMYGenerator
-#
-# __author__ = 'Nimrod'
-#
-#
-# class CCMYCalculations(BaseCalculationsScript):
-#     def run_project_calculations(self):
-#         self.timer.start()
-#         CCMYGenerator(self.data_provider, self.output).main_function()
-#         self.timer.stop('KPIGenerator.run_project_calculations')
-#
-#
+
+__author__ = 'Nimrod'
+
+
+class CCMYCalculations(BaseCalculationsScript):
+    def run_project_calculations(self):
+        self.timer.start()
+        CCMYGenerator(self.data_provider, self.output).main_function()
+        self.timer.stop('KPIGenerator.run_project_calculations')
+
+
 # if __name__ == '__main__':
 #     LoggerInitializer.init('ccmy calculations')
 #     Config.init()
