3971f68af3e8bcbeea45af7ceb60645dbbdbc21a
sathiyanarayanand <sathiyanarayanan@traxretail.com>
2020-06-26 12:58:07 +0800

    PROS-15829 PNGJP QTemplate Validator

--- Projects/PNGJP/Data/TemplateValidator.py
+++ Projects/PNGJP/Data/TemplateValidator.py
@@ -3,6 +3,7 @@ import os
 from Trax.Cloud.Services.Connector.Logger import LoggerInitializer
 from KPIUtils.Utils.Validators.Template.TemplateValidator import TemplateValidator
 
+
 def templateValidator():
     # pay attention if there any number column in template like product en please put string in the last column
     project_name = 'pngjp'
@@ -11,6 +12,7 @@ def templateValidator():
     validate_file_path = '/home/Shani/Desktop/PNGJP/pngjp.xlsx'
     validation_object = TemplateValidator(project_name, helper_path, file_path, validate_file_path)
     validation_object.run()
+
 
 if __name__ == '__main__':
     LoggerInitializer.init('start template validate')
