f070f5f32759e0946dc1277e1130591f15d41331
kristati <krishnat@traxretail.com>
2020-06-27 10:47:22 -0400

    changed the logic of the code

--- Projects/MONDELEZUSPS/Data/LocalConsts.py
+++ Projects/MONDELEZUSPS/Data/LocalConsts.py
@@ -1,24 +1,4 @@
 
 class Consts(object):
-    # Template Column Name
-    KPI_NAME = 'kpi_client_name'
-    KPI_TYPE = 'KPI Type'
-    SCENE_FK = 'scene_fk'
 
-
-    # KPI Types
-    SHARE_OF_SCENES = 'Share of Scenes'
-    SCENE_LOCATION = 'Scene Location'
-    BLOCKING = 'Blocking'
-    SHELF_POSITION = 'Shelf Position'
-    DISTRIBUTION = 'Distribution'
-    BAY_POSITION = 'Bay Position'
-    DIAMOND_POSITION = 'Diamond Position'
-
-    # random
-    SHELF_MAP = 'Shelf Map'
-    COMPLETED_STATUS = 'Completed'
-
-    DATASET_1_JSON = 'Dataset 1: JSON'
-    FINAL_FACINGS = 'final_facings'
-
+    pass
