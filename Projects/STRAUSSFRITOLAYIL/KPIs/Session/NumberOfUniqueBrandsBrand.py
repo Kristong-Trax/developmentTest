5f03c1eaf5105ca4cb2cfba4abbd796bd6f03340
ilay_s <ilays@traxretail.com>
2020-07-05 13:22:16 +0300

    adding score to number_of_unique_brands-brand

--- Projects/STRAUSSFRITOLAYIL/KPIs/Session/NumberOfUniqueBrandsBrand.py
+++ Projects/STRAUSSFRITOLAYIL/KPIs/Session/NumberOfUniqueBrandsBrand.py
@@ -32,7 +32,8 @@ class NumberOfUniqueBrandsBrandKpi(UnifiedCalculationsScript):
         sub_brands = set(own_manufacturer_matches['sub_brand_fk'])
         for sub_brand_fk in sub_brands:
             sub_brand_df = own_manufacturer_matches[own_manufacturer_matches['sub_brand_fk'] == sub_brand_fk]
-            result = score = sub_brand_df['facings'].sum()
+            result = sub_brand_df['facings'].sum()
+            score = Consts.PASS if result > 0 else Consts.FAIL
             self.write_to_db_result(fk=kpi_fk, numerator_id=sub_brand_fk, result=result,
                                     denominator_id=self.utils.store_id, score=score)
 
