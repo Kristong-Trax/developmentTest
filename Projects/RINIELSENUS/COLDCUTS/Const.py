369c773fd8d98931cb749a8d348a799dba9cdd6f
nicolaske <nicolaske@traxretail.com>
2020-07-06 08:53:32 -0400

    add const

---
+++ Projects/RINIELSENUS/COLDCUTS/Const.py
@@ -0,0 +1,15 @@
+class Consts(object):
+    SHELF_MAP = 'shelf_map'
+    SHARE_OF_SCENES = 'Share of Scenes'
+    SCENE_LOCATION = 'Scene Location'
+    SHELF_POSITION = 'Shelf Position'
+    BLOCKING = 'Blocking'
+    BAY_POSITION = 'Bay Position'
+    DIAMOND_POSITION = 'Diamond Position'
+    KPI_TYPE = 'kpi_type'
+    KPI_NAME = 'kpi_client_name'
+    FINAL_FACINGS = 'final_facings'
+    DISTRIBUTION = 'Distribution'
+    KPI_TYPE = 'kpi_type'
+    ACTUAL_TYPE = 'KPI Type'
+    # SCENE_FK = 'scene_fk
