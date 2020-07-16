c3ae04da97617682ed74e91ca33d0cfc27191cab
limor_c <limorc@traxretail.com>
2020-07-16 12:48:30 +0300

    add sos and oos

--- Projects/CBCIL/Utils/Consts.py
+++ Projects/CBCIL/Utils/Consts.py
@@ -9,3 +9,8 @@ class Consts(object):
     LEVEL_2_FK = 'kpi_level_2_fk'
     GAPS_TOTAL_SCORE_KPI = u'פערים מובילים'
     GAP_PER_ATOMIC_KPI = u'פער לפי מדד'
+    LEADING_PRODUCTS = u'מוצרים מובילים'
+    OOS = 'OOS'
+    OOS_SKU = 'OOS - SKU'
+    KPI_NAME = 'KPI Name'
+    PARAMS_VALUE_1 = 'Param (1) Values'
